dc9fb2afd2ddad7f41cb77fa81fb4cd5f7d7fd6d
Mauricio de Oliveira <mauricio@ucsd.edu>
2016-11-16 17:29:15 -0800

    Merging with Khalid

--- eng100l/ambulances/views.py
+++ eng100l/ambulances/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 from django.views import View
-import braces
+from braces import views
 
 # Create your views here.
 
 from .models import Reporter, Ambulances
 
-class AmbulanceUpdateView(braces.views.JSONResponseMixin, View):
+class AmbulanceUpdateView(views.JSONResponseMixin, View):
 
-    def update_ambulance(pk):
+    def update_ambulance(self, pk):
         # retrieve record
         record = Ambulances.objects.get(pk=pk)
 
@@ -36,6 +38,24 @@ class AmbulanceUpdateView(braces.views.JSONResponseMixin, View):
 
     def get_ajax(self, request, pk):
         record = self.update_ambulance(pk)
-        json = { 'status': record.status; 'location': record.location }
+        json = { 'status': record.status, 'location': record.location }
         return self.render_json_response(json)
+     
+class AmbulanceInfoView(views.JSONResponseMixin, View):
+    def get_ajax(self, request, pk):
+        s = Ambulances.objects.get(pk=pk)
         
+        json = {
+            "status" : s.status,
+            "reporter" : s.reporter if s.reporter is not None else "No reporter"
+        }
+        return self.render_json_response(json)
+
+    def get(self, request, pk):
+        s = Ambulances.objects.get(pk=pk)
+
+        json = {
+            "status" : s.status,
+            "reporter" : s.reporter if s.reporter is not None else "No reporter"
+        }
+        return self.render_json_response(json)
