4580b90ed06eeb86984bf801a3e8389cd300cb34
timothylam <lamtimothy1996@gmail.com>
2017-03-01 17:42:33 -0800

    Added login app

---
+++ eng100l/crlogin/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from crlogin import views
+
+urlpatterns = [
+    #url(r'^login/', views.LoginPageView.as_view()),
+    url(r'^about/', views.AboutPageView.as_view()),
+    url(r'^home/', views.HomePageView.as_view()),
+    url(r'^login/', views.user_login, name='login_url'),
+    url(r'^signup/', views.user_signup, name='signup_url'),
+    url(r'^logout/', views.user_logout),
+    #url(r'^', views.user_login, name='login_url'),
+    #url(r'^', views.HomePageView.as_view(), name='home'),
+]
