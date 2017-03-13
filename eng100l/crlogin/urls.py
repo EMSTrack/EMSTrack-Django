4c503b9fbe902eec6f984049bdaf112634196896
Maca <marialomidze8@gmail.com>
2017-03-12 20:04:13 -0700

    Added Settings page. NOTE: Had to add Settings to the navbar in ambulance/base.html

--- eng100l/crlogin/urls.py
+++ eng100l/crlogin/urls.py
@@ -4,11 +4,9 @@ from crlogin import views
 urlpatterns = [
     #url(r'^login/', views.LoginPageView.as_view()),
     url(r'^about/', views.AboutPageView.as_view()),
-    url(r'^home/', views.HomePageView.as_view()),
     url(r'^login/', views.user_login, name='login_url'),
     url(r'^signup/', views.user_signup, name='signup_url'),
     url(r'^logout/', views.user_logout),
+    url(r'^settings/', views.user_settings, name='settings_url'),
     url(r'^$', views.user_login, name='login_url'),
-    #url(r'^', views.user_login, name='login_url'),
-    #url(r'^', views.HomePageView.as_view(), name='home'),
 ]
