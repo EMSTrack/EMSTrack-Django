db650a4fc50297ace695e9b10a7a8c6c5180fb0c
timothylam <lamtimothy1996@gmail.com>
2017-05-10 16:09:45 -0700

    Fixed forms import error

--- eng100l/ambulances/forms.py
+++ eng100l/ambulances/forms.py
@@ -1,30 +1,6 @@
 from django import forms
 
-
 from .models import Ambulances, TrackableDevice, Status, Call
-
-class LeafletPointWidget(gis_widgets.BaseGeometryWidget):
-    template_name = 'leaflet/leaflet.html'
-
-    class Media:
-        css = {
-            'all': ('leaflet/css/LeafletWidget.css',)
-        }
-        js = (
-            'http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js',
-            'leaflet/js/LeafletWidget.js'
-        )
-
-    def render(self, name, value, attrs=None):
-        # add point
-        if value:
-            attrs.update({ 'point': { 'x': value.x,
-                                      'y': value.y,
-                                      'z': value.z,
-                                      'srid': value.srid }
-                       })
-        return super().render(name, value, attrs)
-
 
 class AmbulanceCreateForm(forms.ModelForm):
     class Meta:
@@ -37,16 +13,16 @@ class AmbulanceUpdateForm(forms.ModelForm):
         model = Ambulances
         fields = ['license_plate', 'name', 'status']
 
-class TrackableDeviceCreateForm(forms.ModelForm):
-  class Meta:
-    model = TrackableDevice
-    fields = ['device_id']
+class TrackableDeviceCreateForm(forms.ModelForm): 
+    class Meta:
+        model = TrackableDevice
+        fields = ['device_id']
 
 # front end team to choose which fields to display?
 class CallCreateForm(forms.ModelForm):
-  class Meta:
-    model = Call
-    fields = '__all__'
+    class Meta:
+        model = Call
+        fields = '__all__'
 
 
 class StatusCreateForm(forms.ModelForm):
