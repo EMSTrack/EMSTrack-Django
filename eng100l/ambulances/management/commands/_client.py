c854a4fdc8a6dbf6cc2c1ed8fcdf543b9b75be30
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-05-16 17:34:16 -0700

    added some primitive error handling on the client to mqtt

--- eng100l/ambulances/management/commands/_client.py
+++ eng100l/ambulances/management/commands/_client.py
@@ -1,3 +1,4 @@
+import time
 import paho.mqtt.client as mqtt
 
 from django.core.management.base import BaseCommand
@@ -23,12 +24,11 @@ class Client():
         # default message handler
         self.client.on_message = self.on_message
         
-        # hospital message handler
-        self.client.message_callback_add('hospital/+/#',
-                                         self.on_hospital)
-        
         self.client.username_pw_set(username, password)
         self.client.connect(host, port, 60)
+
+        # populated?
+        self.populated = False
 
     # The callback for when the client receives a CONNACK
     # response from the server.
@@ -42,27 +42,40 @@ class Client():
         # success!
         self.stdout.write(self.style.SUCCESS("Connected to the mqtt brocker"))
 
+        # populate messages
+        if not self.populated:
+
+            # initialize hospitals
+            self.stdout.write("Initializing hospitals")
+            hospitals = Hospital.objects.all()
+            k = 0
+            for h in hospitals:
+                k = k + 1
+                self.stdout.write(" {:2d}. {}".format(k, h))
+                equipment = EquipmentCount.objects.filter(hospital = h)
+                for e in equipment:
+                    self.stdout.write("     {}: {}".format(e.equipment,
+                                                           e.quantity))
+                    # publish message
+                    client.publish('hospital/{}/equipment/{}'.format(h.id,
+                                                                     e.equipment),
+                                   e.quantity,
+                                   qos=2,
+                                   retain=True)
+
+            self.stdout.write(
+                self.style.SUCCESS("Done initializing hospitals"))
+
+            self.populated = True
+            
         # Subscribing in on_connect() means that if we lose the
         # connection and reconnect then subscriptions will be renewed.
         client.subscribe('#', 2)
 
-        # initialize hospitals
-        self.stdout.write("Initializing hospitals")
-        hospitals = Hospital.objects.all()
-        k = 0
-        for h in hospitals:
-            k = k + 1
-            self.stdout.write(" {:2d}. {}".format(k, h))
-            equipment = EquipmentCount.objects.filter(hospital = h)
-            for e in equipment:
-                self.stdout.write("     {}: {}".format(e.equipment,
-                                                       e.quantity))
-                # publish message
-                client.publish('hospital/{}/equipment/{}'.format(h.id,
-                                                                 e.equipment),
-                               e.quantity)
-        self.stdout.write(self.style.SUCCESS("Done initializing hospitals"))
-
+        # hospital message handler
+        self.client.message_callback_add('hospital/+/#',
+                                         self.on_hospital)
+        
         self.stdout.write(self.style.SUCCESS("Listening to messages..."))
         
     # The callback for when a PUBLISH message is received from the server.
@@ -81,20 +94,31 @@ class Client():
         topic = msg.topic.split('/')
         hospital = topic[1]
         equipment = topic[3]
+
+        if not msg.payload:
+            return
+        
         quantity = int(msg.payload)
 
-        self.stdout.write("{} {}".format(msg.topic, quantity))
-        
-        e = EquipmentCount.objects.get(hospital = hospital,
-                                       equipment__name = equipment)
+        self.stdout.write("on_hospital: {} {}".format(msg.topic, quantity))
 
-        # update quantity
-        if e.quantity != quantity:
-            e.quantity = quantity
-            e.save()
-            self.stdout.write(
-                self.style.SUCCESS("Hospital '{}' equipment '{}' updated to '{}'".format(e.hospital.name, equipment, quantity)))
+        try:
             
+            e = EquipmentCount.objects.get(hospital = hospital,
+                                           equipment__name = equipment)
+
+            # update quantity
+            if e.quantity != quantity:
+                e.quantity = quantity
+                e.save()
+                self.stdout.write(
+                    self.style.SUCCESS("Hospital '{}' equipment '{}' updated to '{}'".format(e.hospital.name, equipment, quantity)))
+
+        except:
+            
+            self.stdout.write(
+                self.style.ERROR("hospital/{}/equipment/{} is not available".format(hospital, equipment)))
+                
     # disconnect
     def disconnect(self):
         self.client.disconnect()
