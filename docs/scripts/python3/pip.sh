3ab2262fdf4fcaa6485ea9862bbaf0b3dc9427cd
Alger Vuong <algervuong@gmail.com>
2017-05-26 00:59:03 -0700

    Updated documentation; added linux guide, pip script, and migration script

---
+++ docs/scripts/python3/pip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pip3 install django
+pip3 install django-braces
+pip3 install psycopg2
+pip3 install djangorestframework
+pip3 install markdown
+pip3 install django-filter
+pip3 install django-rest-swagger
+pip3 install django-extra-fields
+pip3 install paho-mqtt
