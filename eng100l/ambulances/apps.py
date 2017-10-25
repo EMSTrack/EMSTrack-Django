f455caedca752ac0918abfe90fb2602750316f8c
Alger Vuong <algervuong@gmail.com>
2017-10-25 13:20:58 -0700

    Added signals to the project and example

--- eng100l/ambulances/apps.py
+++ eng100l/ambulances/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class AmbulancesConfig(AppConfig):
     name = 'ambulances'
+
+    def ready(self):
+        import ambulances.signals
---
+++ eng100l/ambulances/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.conf import settings
+from .models import Status, Ambulances, Region, Call, Hospital, \
+    Equipment, EquipmentCount, Base, Route, Capability, LocationPoint, User
+
+
+@receiver(post_delete, sender=Ambulances)
+@receiver(post_save, sender=Ambulances)
+def ambulances_mqtt_trigger(sender, **kwargs):
+    # do mqtt transactions here
+    print('Triggered')
