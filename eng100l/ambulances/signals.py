aa3fa16a6b268349d107fb78b7233a4d22ba10ef
Alger Vuong <algervuong@gmail.com>
2017-11-01 23:10:08 -0700

    Moved disable_for_loaddata annotation; signal disabling should work

--- eng100l/ambulances/signals.py
+++ eng100l/ambulances/signals.py
@@ -1,3 +1,4 @@
+import inspect
 from django.db.models.signals import post_save, post_delete, m2m_changed
 from django.dispatch import receiver
 
@@ -48,51 +49,51 @@ def disable_for_loaddata(signal_handler):
     def wrapper(*args, **kwargs):
         for fr in inspect.stack():
             if inspect.getmodulename(fr[1]) == 'loaddata':
-                pass
+                return
         signal_handler(*args, **kwargs)
     return wrapper
 
-@disable_for_loaddata
 @receiver(post_delete, sender=Ambulances)
 @receiver(post_save, sender=Ambulances)
+@disable_for_loaddata
 def ambulance_mqtt_trigger(sender, **kwargs):
     # Connect to mqtt
     connect_mqtt("ambulance", kwargs)
 
-@disable_for_loaddata
 @receiver(post_delete, sender=Hospital)
 @receiver(post_save, sender=Hospital)
+@disable_for_loaddata
 def hospital_mqtt_trigger(sender, **kwargs):
     # Connect to mqtt
     connect_mqtt("hospital", kwargs)
 
-@disable_for_loaddata
 @receiver(post_delete, sender=Equipment)
 @receiver(post_save, sender=Equipment)
+@disable_for_loaddata
 def hospital_equipment_mqtt_trigger(sender, **kwargs):
     connect_mqtt("equipment", kwargs)
 
-@disable_for_loaddata
 @receiver(post_delete, sender=EquipmentCount)
 @receiver(post_save, sender=EquipmentCount)
+@disable_for_loaddata
 def hospital_equipment_count_mqtt_trigger(sender, **kwargs):
     connect_mqtt("equipment_count", kwargs)
 
-@disable_for_loaddata
 @receiver(post_save, sender=User)
+@disable_for_loaddata
 def user_trigger(sender, **kwargs):
     if kwargs['created']:
         connect_mqtt("user", kwargs)
 
-@disable_for_loaddata
 @receiver(m2m_changed, sender=User.ambulances.through)
+@disable_for_loaddata
 def user_ambulances_mqtt_trigger(sender, action, instance, **kwargs):
     kwargs['instance'] = instance
     kwargs['created'] = False
     connect_mqtt("user_ambulance_list", kwargs)
 
-@disable_for_loaddata
 @receiver(m2m_changed, sender=User.hospitals.through)
+@disable_for_loaddata
 def user_hospitals_mqtt_trigger(sender, action, instance, **kwargs):
     kwargs['instance'] = instance
     kwargs['created'] = False
