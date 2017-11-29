971e0303c06c671ed519d6be898a625cad30b16a
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-11-29 13:51:48 -0800

    Overloading django flush command

--- ambulances/management/commands/flush.py
+++ ambulances/management/commands/flush.py
@@ -5,8 +5,8 @@ Location example: project/app/management/commands/flush.py
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management.commands import flush
 
-
 class Command(flush.Command):
 
     def handle(self, *args, **options):
+        print('In handle')
         super(Command, self).handle(*args, **options)
