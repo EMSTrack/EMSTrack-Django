fb3eaedc9d16781f492aa5e283be8949f61b3df4
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-08 22:38:30 -0800

    ACL permissions on login

--- login/views.py
+++ login/views.py
@@ -67,37 +67,44 @@ class MQTTAclView(CsrfExemptMixin, View):
             data = request.DATA
         allow = False
 
-        # TO DO
-        return HttpResponse('OK')
+        # Check permissions
+        username = data.get('username')
+        acc = data.get('acc') # 1 == sub, 2 == pub
+
+        # get topic and remove first '/'
+        topic = data.get('topic').split('/')
+        if len(topic) > 0 and topic[0] == '':
+            del topic[0]
+
+        try:
+            # get user
+            user = User.objects.get(username=data.get('username'),
+                                    is_active=True)
+
+            if acc == '1':
+                
+                # permission to subscribe
+                #  - all (for now)
+                
+                return HttpResponse('OK')
+            
+            elif acc == '2':
+                
+                # permission to publish on:
+                #  - user/*username*/hospital
+                #  - user/*username*/ambulance
+                #  - user/*username*/location
+                if (len(topic) >= 3 and
+                    topic[0] == 'user' and
+                    topic[1] == user.username):
+
+                    if (topic[2] == 'hospital' or
+                        topic[2] == 'ambulance' or
+                        topic[2] == 'location'):
+                        
+                        return HttpResponse('OK')
         
-        # if hasattr(settings, 'MQTT_ACL_ALLOW'):
-        #     allow = settings.MQTT_ACL_ALLOW
-
-        # topic, new_topic = Topic.objects.get_or_create(name=data.get('topic', '#'))
-        # acc = data.get('acc', None)
-        # user = None
-        # user_ = User.objects.filter(username=data.get('username'), is_active=True)
-        # if user_.count() > 0:
-        #     user = user_[0]
-
-        # acl = None
-        # if not new_topic:
-        #     try:
-        #         acl = ACL.objects.get(acc=acc, topic=topic)  # ACL only count have one or none
-        #     except ACL.DoesNotExist:
-        #         pass
-
-        # if acl is None:
-        #     allow = ACL.get_default(acc=acc, user=user)
-        #     # TODO search best candidate
-
-        # if acl:
-        #     allow = acl.has_permission(user=user)
-
-        # if allow and hasattr(settings, 'MQTT_ACL_ALLOW_ANONIMOUS'):
-        #     if user is None:
-        #         allow = settings.MQTT_ACL_ALLOW_ANONIMOUS
-
-        # if not allow:
-        #     return HttpResponseForbidden('')
-        # return HttpResponse('')
+        except User.DoesNotExist:
+            pass
+        
+        return HttpResponseForbidden()
