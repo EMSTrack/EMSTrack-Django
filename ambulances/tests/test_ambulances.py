d49565385c9f3dbe74eef5c33605372c041301bd
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-10 20:13:19 -0800

    Test login

--- ambulances/tests/test_ambulances.py
+++ ambulances/tests/test_ambulances.py
@@ -1,8 +1,8 @@
 from django.test import TestCase, RequestFactory
 
-from ..models import Ambulance, Capability, Status
+from ..models import Ambulance, AmbulanceCapability, AmbulanceStatus
 
-from ..serializers import StatusSerializer, AmbulanceSerializer
+from ..serializers import AmbulanceStatusSerializer, AmbulanceSerializer
 
 from django.test import Client
 
@@ -11,40 +11,38 @@ class CreateAmbulance(TestCase):
     def setUp(self):
 
         # Add status
-        self.s1 = Status(name='Out of service')
+        self.s1 = AmbulanceStatus(name='Out of service')
         self.s1.save()
-        self.s2 = Status(name='In service')
+        self.s2 = AmbulanceStatus(name='In service')
         self.s2.save()
-        self.s3 = Status(name='Available')
+        self.s3 = AmbulanceStatus(name='Available')
         self.s3.save()
         
         # Add capability
-        self.c1 = Capability(name='Basic')
+        self.c1 = AmbulanceCapability(name='Basic')
         self.c1.save()
-        self.c2 = Capability(name='Advanced')
+        self.c2 = AmbulanceCapability(name='Advanced')
         self.c2.save()
-        self.c3 = Capability(name='Rescue')
+        self.c3 = AmbulanceCapability(name='Rescue')
         self.c3.save()
         
         # Add ambulances
         self.a1 = Ambulance(
             identifier='BC-179',
             comment='Maintenance due',
-            capability=self.c1,
-            status=self.s1)
+            capability=self.c1)
         
         # Add ambulances
         self.a2 = Ambulance(
             identifier='BC-180',
             comment='Need painting',
-            capability=self.c2,
-            status=self.s3)
+            capability=self.c2)
 
     def test_ambulances(self):
 
-        # test StatusSerializer
+        # test AmbulanceStatusSerializer
         for s in (self.s1, self.s2, self.s3):
-            serializer = StatusSerializer(s)
+            serializer = AmbulanceStatusSerializer(s)
             result = { 'id': s.pk, 'name': s.name }
             self.assertEqual(serializer.data, result)
 
