27b73839461f04ac57370c337a896908a816d4c9
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-10 20:58:52 -0800

    Test ambulances

--- ambulances/serializers.py
+++ ambulances/serializers.py
@@ -35,7 +35,7 @@ class UserLocationSerializer(serializers.ModelSerializer):
         fields = ['user_id', 'latitude', 'longitude', 'timestamp']
         
     def get_uid(self, obj):
-        return obj.user.pk
+        return obj.user.id
     
     def get_lat(self, obj):
         return obj.location.y
