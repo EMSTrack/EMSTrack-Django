9caf1e1902b612664adda6e4b123e332d16ce4af
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-11 18:21:18 -0800

    Rolling back to basic

--- login/views.py
+++ login/views.py
@@ -4,8 +4,9 @@ from django.views.generic.base import View
 from django.views.generic.edit import FormView
 
 from django.contrib.auth.models import User
-#from django.contrib.auth import get_user_model
-#User = get_user_model()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 from braces.views import CsrfExemptMixin
 
