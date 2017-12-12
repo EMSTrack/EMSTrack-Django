a52dffc1692b518c15ede24fdae4b9ee23415bbc
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-11 20:49:22 -0800

    Rolling back to basic

--- ambulances/admin.py
+++ ambulances/admin.py
@@ -1,15 +1,27 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
 
-from .models import User, \
+from .models import Profile, \
     Ambulance, AmbulanceStatus, AmbulanceCapability, \
     AmbulanceLocation, \
     User, UserLocation, \
     Hospital, Equipment, EquipmentCount, UserLocation, \
     Call, Region
 
+# Define an inline admin descriptor for Profile model
+# which acts a bit like a singleton
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline, )
+
+# Re-register UserAdmin
 admin.site.register(User, UserAdmin)
 
 admin.site.register(Ambulance)
