61136766a2c14acc1b794fe07e9d41f604714e0c
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-12-12 16:37:09 -0800

    Back to basics

--- ambulances/signals.py
+++ ambulances/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from django.contrib.auth.models import User
+
 # Add signal to automatically extend user profile
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
