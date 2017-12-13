6905c5a6bcbd6c65e957135f2861e34aa5f09a8b
Mauricio de Oliveira <mauricio@ucsd.edu>
2017-12-13 10:16:05 -0800

    Changes in the models to streamline status and capabilities

--- login/tests/test_login.py
+++ login/tests/test_login.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 #from django.contrib.auth import get_user_model
 #User = get_user_model()
 
-from ambulances.models import Hospital
+from ambulances.models import Hospital, HospitalPermission
 
 from django.test import Client
 
