f3b1e3f2efcd7c3089aed7c1fcf56cd7d400dcd3
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-14 08:17:36 -0800

    Tests

--- ambulances/views.py
+++ ambulances/views.py
@@ -73,7 +73,7 @@ class AmbulanceViewSet(mixins.RetrieveModelMixin,
 
         # otherwise only return ambulance that the user can read
         return Ambulance.objects.filter(ambulancepermission__can_read=True,
-                                        ambulancepermission__user__id=user)
+                                        ambulancepermission__user__id=user.pk)
 
 # class AmbulanceViewSet(mixins.RetrieveModelMixin,
 #                        viewsets.GenericViewSet):
