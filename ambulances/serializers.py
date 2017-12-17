c4cee37ddc43b32e932ec6d0bf3a2e243afd0eac
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-16 17:19:46 -0800

    Streamlining ambulance updates

--- ambulances/serializers.py
+++ ambulances/serializers.py
@@ -11,25 +11,23 @@ from .models import Profile, Hospital, Ambulance, \
 
 class AmbulancePermissionSerializer(serializers.ModelSerializer):
 
-    #ambulance_id = serializers.IntegerField(source='ambulance.id')
-    #ambulance_identifier = serializers.CharField(source='ambulance.identifier')
+    ambulance_id = serializers.IntegerField(source='ambulance.id')
+    ambulance_identifier = serializers.CharField(source='ambulance.identifier')
     
     class Meta:
         model = AmbulancePermission
         fields = ('ambulance_id', 'ambulance_identifier', 'can_read', 'can_write')
         read_only_fields = ('ambulance_id', 'ambulance_identifier', 'can_read', 'can_write')
-        depth = 1
 
 class HospitalPermissionSerializer(serializers.ModelSerializer):
 
-    #hospital_id = serializers.IntegerField(source='hospital.id')
-    #hospital_name = serializers.CharField(source='hospital.name')
+    hospital_id = serializers.IntegerField(source='hospital.id')
+    hospital_name = serializers.CharField(source='hospital.name')
     
     class Meta:
         model = HospitalPermission
         fields = ('hospital_id', 'hospital_name', 'can_read', 'can_write')
         read_only_fields = ('hospital_id', 'hospital_name', 'can_read', 'can_write')
-        depth = 1
         
 class ProfileSerializer(serializers.ModelSerializer):
 
@@ -41,11 +39,12 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = ('ambulances','hospitals')
 
 # Ambulance serializers
+# Ambulance serializers
 class AmbulanceUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ambulance
-        fields = ('user', 'status', 'location', 'timestamp')
+        fields = ('ambulance_set', 'user', 'status', 'location', 'timestamp')
 
 class AmbulanceSerializer(serializers.ModelSerializer):
 
