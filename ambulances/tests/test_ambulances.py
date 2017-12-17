c1f5ad8bc34aadec1c36d725a2956ed47c0ca186
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-17 11:50:45 -0800

    Tests

--- ambulances/tests/test_ambulances.py
+++ ambulances/tests/test_ambulances.py
@@ -365,7 +365,7 @@ class CreateAmbulance(TestCase):
         serializer = AmbulanceSerializer(a,
                                          data={
                                              'status': status,
-                                             'updated_by': user
+                                             'updated_by': user.id
                                          }, partial="True")
         serializer.is_valid()
         print(serializer.errors)
