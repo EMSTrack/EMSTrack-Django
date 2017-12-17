a6f3efb614777887df5be0c0fee2ea109e4eb8eb
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-16 17:09:27 -0800

    Streamlining ambulance updates

--- ambulances/serializers.py
+++ ambulances/serializers.py
@@ -11,23 +11,25 @@ from .models import Profile, Hospital, Ambulance, \
 
 class AmbulancePermissionSerializer(serializers.ModelSerializer):
 
-    ambulance_id = serializers.IntegerField(source='ambulance.id')
-    ambulance_identifier = serializers.CharField(source='ambulance.identifier')
+    #ambulance_id = serializers.IntegerField(source='ambulance.id')
+    #ambulance_identifier = serializers.CharField(source='ambulance.identifier')
     
     class Meta:
         model = AmbulancePermission
         fields = ('ambulance_id', 'ambulance_identifier', 'can_read', 'can_write')
         read_only_fields = ('ambulance_id', 'ambulance_identifier', 'can_read', 'can_write')
+        depth = 1
 
 class HospitalPermissionSerializer(serializers.ModelSerializer):
 
-    hospital_id = serializers.IntegerField(source='hospital.id')
-    hospital_name = serializers.CharField(source='hospital.name')
+    #hospital_id = serializers.IntegerField(source='hospital.id')
+    #hospital_name = serializers.CharField(source='hospital.name')
     
     class Meta:
         model = HospitalPermission
         fields = ('hospital_id', 'hospital_name', 'can_read', 'can_write')
         read_only_fields = ('hospital_id', 'hospital_name', 'can_read', 'can_write')
+        depth = 1
         
 class ProfileSerializer(serializers.ModelSerializer):
 
