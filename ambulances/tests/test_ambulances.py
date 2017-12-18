7cf2509dde8a3d84ce83e3be2d64efe4fc8b66d4
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-18 09:38:36 -0800

    Tests

--- ambulances/tests/test_ambulances.py
+++ ambulances/tests/test_ambulances.py
@@ -123,6 +123,56 @@ class CreateAmbulance(TestCase):
         print('u2: {}\n{}'.format(self.u2, self.u2.profile))
         print('u3: {}\n{}'.format(self.u3, self.u3.profile))
 
+    def test_ambulance_get_list_viewset(self):
+
+        # instantiate client
+        client = Client()
+
+        # login as admin
+        client.login(username='admin', password='admin')
+
+        # retrieve ambulances
+        response = client.get('/ambulances/api/ambulance/',
+                              follow=True)
+        self.assertEqual(response.status_code, 200)
+        result = JSONParser().parse(BytesIO(response.content))
+        answer = [AmbulanceSerializer(self.a1).data,
+                  AmbulanceSerializer(self.a2).data,
+                  AmbulanceSerializer(self.a3).data]
+        self.assertEqual(result, answer)
+        
+        # logout
+        client.logout()
+
+        # login as testuser1
+        client.login(username='testuser1', password='top_secret')
+
+        # retrieve ambulances
+        response = client.get('/ambulances/api/ambulance/',
+                              follow=True)
+        self.assertEqual(response.status_code, 200)
+        result = JSONParser().parse(BytesIO(response.content))
+        answer = []
+        self.assertEqual(result, answer)
+        
+        # logout
+        client.logout()
+        
+        # login as testuser2
+        client.login(username='testuser2', password='very_secret')
+
+        # retrieve ambulances
+        response = client.get('/ambulances/api/ambulance/',
+                              follow=True)
+        self.assertEqual(response.status_code, 200)
+        result = JSONParser().parse(BytesIO(response.content))
+        answer = [ # AmbulanceSerializer(self.a1).data, # can't read
+                  AmbulanceSerializer(self.a3).data]
+        self.assertEqual(result, answer)
+        
+        # logout
+        client.logout()
+
 class Nothing():
         
     def test_profile_serializer(self):
@@ -309,56 +359,6 @@ class Nothing():
         # logout
         client.logout()
         
-    def test_ambulance_get_list_viewset(self):
-
-        # instantiate client
-        client = Client()
-
-        # login as admin
-        client.login(username='admin', password='admin')
-
-        # retrieve ambulances
-        response = client.get('/ambulances/api/ambulance/',
-                              follow=True)
-        self.assertEqual(response.status_code, 200)
-        result = JSONParser().parse(BytesIO(response.content))
-        answer = [AmbulanceSerializer(self.a1).data,
-                  AmbulanceSerializer(self.a2).data,
-                  AmbulanceSerializer(self.a3).data]
-        self.assertEqual(result, answer)
-        
-        # logout
-        client.logout()
-
-        # login as testuser1
-        client.login(username='testuser1', password='top_secret')
-
-        # retrieve ambulances
-        response = client.get('/ambulances/api/ambulance/',
-                              follow=True)
-        self.assertEqual(response.status_code, 200)
-        result = JSONParser().parse(BytesIO(response.content))
-        answer = []
-        self.assertEqual(result, answer)
-        
-        # logout
-        client.logout()
-        
-        # login as testuser2
-        client.login(username='testuser2', password='very_secret')
-
-        # retrieve ambulances
-        response = client.get('/ambulances/api/ambulance/',
-                              follow=True)
-        self.assertEqual(response.status_code, 200)
-        result = JSONParser().parse(BytesIO(response.content))
-        answer = [ # AmbulanceSerializer(self.a1).data, # can't read
-                  AmbulanceSerializer(self.a3).data]
-        self.assertEqual(result, answer)
-        
-        # logout
-        client.logout()
-
     def test_ambulance_create_serializer(self):
 
         serializer = AmbulanceSerializer(data={
