1e13d436ed8ff6e2064cc2435d6cf5ff734317b5
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-17 21:28:23 -0800

    Tests

--- ambulances/views.py
+++ ambulances/views.py
@@ -69,6 +69,7 @@ class AmbulanceViewSet(mixins.CreateModelMixin,
         if user.is_anonymous:
             raise PermissionDenied()
 
+        print('> METHOD = {}'.format(self.request.method))
         # otherwise only return ambulances that the user can read or write to
         if self.request.method == 'GET':
             # ambulances that the user can read
