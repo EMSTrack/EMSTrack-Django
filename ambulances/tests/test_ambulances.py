94b36abad1a28166a5905d4e56487b46838f9743
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-18 14:41:25 -0800

    Tests

--- ambulances/tests/test_ambulances.py
+++ ambulances/tests/test_ambulances.py
@@ -569,7 +569,7 @@ class CreateAmbulance(TestCase):
         self.assertEqual(response.status_code, 200)
         result = JSONParser().parse(BytesIO(response.content))
         self.assertEqual(result['status'], status)
-        self.assertEqual(result['location'], str(location))
+        self.assertEqual(result['location'], 'SRID=4326;' + str(location))
         self.assertEqual(result['location_timestamp'], date2iso(location_timestamp))
         
         # logout
