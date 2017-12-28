92848aacaccd03cbf9348c8cf2e0002d64f86628
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-28 15:52:59 -0800

    Refactor HospitalEquipment

--- ambulances/models.py
+++ ambulances/models.py
@@ -98,12 +98,19 @@ class Hospital(models.Model):
     address = models.CharField(max_length=254, default="")
     location = models.PointField(srid=4326, null=True, blank=True)
     
+    updated_by = models.ForeignKey(User,
+                                   on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return ('> Hospital {}\n' +
                 '   Address: {}\n' +
-                '  Location: {}'.format(self.name,
-                                        self.address,
-                                        self.location))
+                '  Location: {}\n' +
+                '   Updated: {} by {}').format(self.name,
+                                               self.address,
+                                               self.location,
+                                               self.updated_by,
+                                               self.updated_on)
 
 class HospitalPermission(models.Model):
     hospital = models.ForeignKey(Hospital,
