dbe47c24d83aa8168a7a3215b39cacea4d34a4b0
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-28 11:33:25 -0800

    Adding signal placeholder and ambulance_list

--- ambulances/signals.py
+++ ambulances/signals.py
@@ -10,3 +10,8 @@ from .models import Profile
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+# Add signal to log ambulance location, status and comments to route
+@receiver(pre_save, sender=Ambulance)
+def add_to_route(sender, instance, **kwargs):
+    pass
