495ba5e6e6941ad89fd120778a23f28934b36424
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2017-12-29 10:08:10 -0800

    Refactor HospitalEquipment

--- ambulances/viewsets.py
+++ ambulances/viewsets.py
@@ -137,6 +137,7 @@ class HospitalViewSet(mixins.ListModelMixin,
         print('> user = {}, can_do = {}'.format(user, can_do))
         print('> hospitals = {}'.format(Hospital.objects.all()))
         print('> filtered hospitals = {}'.format(Hospital.objects.filter(id__in=can_do)))
+        print('> filtered hospitals query = {}'.format(Hospital.objects.filter(id__in=can_do).query))
         return Hospital.objects.filter(id__in=can_do)
 
     def perform_create(self, serializer):
