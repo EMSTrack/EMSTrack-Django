7551131dbed5acd82e8e1013a1b737566c9fab2f
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-03 09:51:53 -0800

    mqttseed

--- ambulances/management/commands/mqttseed.py
+++ ambulances/management/commands/mqttseed.py
@@ -2,7 +2,7 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
-from ambulances.mqttclient import UpdateClient
+from ambulances.mqttupdate import UpdateClient
 
 from ambulances.models import Ambulance, Hospital, HospitalEquipment
 
