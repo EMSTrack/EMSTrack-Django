bfd1f49812cbb6f1ea7edf7156611e3fc6faa8d6
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-06 10:24:57 -0800

    Test with live server

--- ambulances/tests/test_mqtt.py
+++ ambulances/tests/test_mqtt.py
@@ -401,52 +401,52 @@ class TestMQTTSeed(LiveTestSetup):
         broker['PASSWORD'] = 'top_secret'
         broker['CLIENT_ID'] = 'test_mqttseed_testuser1'
 
-        client = MQTTTestClient(broker, sys.stdout, style, verbosity = 1)
-        client.test = self
+        client2 = MQTTTestClient(broker, sys.stdout, style, verbosity = 1)
+        client2.test = self
 
         # Expect user profile
         profile = Profile.objects.get(user__username='testuser1')
-        client.expect('user/testuser1/profile',
+        client2.expect('user/testuser1/profile',
                       JSONRenderer().render(ExtendedProfileSerializer(profile).data),
                       2)
 
         # Ambulances
         can_read = profile.ambulances.filter(can_read=True).values('ambulance_id')
         for ambulance in Ambulance.objects.filter(id__in=can_read):
-            client.expect('ambulance/{}/data'.format(ambulance.id),
+            client2.expect('ambulance/{}/data'.format(ambulance.id),
                           JSONRenderer().render(AmbulanceSerializer(ambulance).data),
                           2)
         
         # Hospitals
         can_read = profile.hospitals.filter(can_read=True).values('hospital_id')
         for hospital in Hospital.objects.filter(id__in=can_read):
-            client.expect('hospital/{}/data'.format(hospital.id),
+            client2.expect('hospital/{}/data'.format(hospital.id),
                           JSONRenderer().render(HospitalSerializer(hospital).data),
                           2)
             
         # Expect all hospital equipments
         for e in HospitalEquipment.objects.filter(hospital__id__in=can_read):
-            client.expect('hospital/{}/equipment/{}/data'.format(e.hospital.id,
+            client2.expect('hospital/{}/equipment/{}/data'.format(e.hospital.id,
                                                                  e.equipment.name),
                           JSONRenderer().render(HospitalEquipmentSerializer(e).data),
                           2)
             
         try:
         
-            client.loop_start()
+            client2.loop_start()
         
-            while not client.connected or not client.done():
+            while not client2.connected or not client2.done():
                 time.sleep(1)
             
-            self.assertEqual(client.connected, True)
+            self.assertEqual(client2.connected, True)
 
-            client.loop_stop()
+            client2.loop_stop()
             
         except KeyboardInterrupt:
             pass
         
         finally:
-            client.disconnect()
+            client2.disconnect()
 
     def _test(self):
             
