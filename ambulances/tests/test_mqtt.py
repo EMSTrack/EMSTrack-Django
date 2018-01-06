6203e73dd832ee3ca2eec1cb8dc41a304a77784a
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-06 08:43:48 -0800

    Test with live server

--- ambulances/tests/test_mqtt.py
+++ ambulances/tests/test_mqtt.py
@@ -330,7 +330,7 @@ class TestMQTTSeed(LiveTestSetup):
         from django.core import management
     
         management.call_command('mqttseed',
-                                verbosity=0)
+                                verbosity=2)
         
         # Start client as admin
         stdout = OutputWrapper(sys.stdout)
