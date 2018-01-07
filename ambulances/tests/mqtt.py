85f53c14acfd89234a3528b228201a80c33aadc4
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-07 14:16:00 -0800

    Test with live server

--- ambulances/tests/mqtt.py
+++ ambulances/tests/mqtt.py
@@ -1,6 +1,7 @@
 import subprocess, time, os, sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import TestCase
+from django.conf import settings
 
 from django.contrib.auth.models import User
 
