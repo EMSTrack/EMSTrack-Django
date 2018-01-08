7f8fdb0e829930d4d87e46e48575e4e0c8e6dc9f
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-07 23:31:02 -0800

    Test login

--- ambulances/tests/test_mqtt.py
+++ ambulances/tests/test_mqtt.py
@@ -89,7 +89,6 @@ class TestMQTTSeed(MQTTTestCase):
         broker['CLIENT_ID'] = 'test_mqttseed_admin'
         
         client = MQTTTestClient(broker, sys.stdout, style, verbosity = 1)
-
         self.is_connected(client)
 
         qos = 0
@@ -130,82 +129,12 @@ class TestMQTTSeed(MQTTTestCase):
         # Done?
         self.loop(client)
 
-    def _test(self):
-        
         # Repeat with same client
         
         client = MQTTTestClient(broker, sys.stdout, style, verbosity = 1)
-        client.test = self
+        self.is_connected(client)
 
-        # connected?
-        k = 0
-        while not client.connected and k < self.MAX_TRIES:
-            k += 1
-            client.loop()
-
-        self.assertEqual(client.connected, True)
-        print('>> connected')
-
-        # Expect all ambulances
-        for ambulance in Ambulance.objects.all():
-            client.expect('ambulance/{}/data'.format(ambulance.id),
-                          JSONRenderer().render(AmbulanceSerializer(ambulance).data),
-                          qos)
-
-        # Expect all hospitals
-        for hospital in Hospital.objects.all():
-            client.expect('hospital/{}/data'.format(hospital.id),
-                          JSONRenderer().render(HospitalSerializer(hospital).data),
-                          qos)
-            hospital_equipment = hospital.hospitalequipment_set.values('equipment')
-            equipment = Equipment.objects.filter(id__in=hospital_equipment)
-            client.expect('hospital/{}/metadata'.format(hospital.id),
-                          JSONRenderer().render(EquipmentSerializer(equipment, many=True).data),
-                          qos)
-
-        # Expect all hospital equipments
-        for e in HospitalEquipment.objects.all():
-            client.expect('hospital/{}/equipment/{}/data'.format(e.hospital.id,
-                                                                 e.equipment.name),
-                          JSONRenderer().render(HospitalEquipmentSerializer(e).data),
-                          qos)
-
-        # Expect all profiles
-        for obj in Profile.objects.all():
-            client.expect('user/{}/profile'.format(obj.user.username),
-                          JSONRenderer().render(ExtendedProfileSerializer(obj).data),
-                          qos)
-
-        # Subscribed?
-        k = 0
-        while len(client.subscribed) and k < self.MAX_TRIES:
-            k += 1
-            client.loop()
-
-        self.assertEqual(len(client.subscribed), 0)
-        print('>> subscribed')
-
-        # Process all messages
-        process_messages(client)
-        self.assertEqual(client.done(), True)
-        print('<< done')
-            
-        # Repeat with same client and different qos
-
-        qos = 2
-        client = MQTTTestClient(broker, sys.stdout, style, verbosity = 1)
-        client.test = self
-
-        # connected?
-        k = 0
-        while not client.connected and k < self.MAX_TRIES:
-            k += 1
-            client.loop()
-
-        self.assertEqual(client.connected, True)
-        print('>> connected')
-
-        self.assertEqual(client.connected, True)
+        qos = 0
         
         # Expect all ambulances
         for ambulance in Ambulance.objects.all():
@@ -238,19 +167,56 @@ class TestMQTTSeed(MQTTTestCase):
                           qos)
 
         # Subscribed?
-        k = 0
-        while len(client.subscribed) and k < self.MAX_TRIES:
-            k += 1
-            client.loop()
+        self.is_subscribed(client)
 
-        self.assertEqual(len(client.subscribed), 0)
-        print('>> subscribed')
+        # Done?
+        self.loop(client)
+            
+        # Repeat with same client and different qos
 
-        # Process all messages
-        process_messages(client)
-        self.assertEqual(client.done(), True)
-        print('<< done')
+        client = MQTTTestClient(broker, sys.stdout, style, verbosity = 1)
+        self.is_connected(client)
 
+        qos = 2
+        
+        # Expect all ambulances
+        for ambulance in Ambulance.objects.all():
+            client.expect('ambulance/{}/data'.format(ambulance.id),
+                          JSONRenderer().render(AmbulanceSerializer(ambulance).data),
+                          qos)
+
+        # Expect all hospitals
+        for hospital in Hospital.objects.all():
+            client.expect('hospital/{}/data'.format(hospital.id),
+                          JSONRenderer().render(HospitalSerializer(hospital).data),
+                          qos)
+            hospital_equipment = hospital.hospitalequipment_set.values('equipment')
+            equipment = Equipment.objects.filter(id__in=hospital_equipment)
+            client.expect('hospital/{}/metadata'.format(hospital.id),
+                          JSONRenderer().render(EquipmentSerializer(equipment, many=True).data),
+                          qos)
+
+        # Expect all hospital equipments
+        for e in HospitalEquipment.objects.all():
+            client.expect('hospital/{}/equipment/{}/data'.format(e.hospital.id,
+                                                                 e.equipment.name),
+                          JSONRenderer().render(HospitalEquipmentSerializer(e).data),
+                          qos)
+
+        # Expect all profiles
+        for obj in Profile.objects.all():
+            client.expect('user/{}/profile'.format(obj.user.username),
+                          JSONRenderer().render(ExtendedProfileSerializer(obj).data),
+                          qos)
+
+        # Subscribed?
+        self.is_subscribed(client)
+
+        # Done?
+        self.loop(client)
+
+    def _test(self):
+        
         # repeat with another user
 
         qos = 0
