eba133d983e811ec60ca6b7515a8b5bec8daadb0
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-08 13:36:29 -0800

    Generating pwfiles

--- ambulances/management/commands/mqttpwfile.py
+++ ambulances/management/commands/mqttpwfile.py
@@ -19,7 +19,7 @@ class Command(BaseCommand):
             # Retrieve current users
             for u in User.objects.filter(is_superuser=True):
                 file.write('{}:{}\n'.format(u.username,
-                                            u.password.replace('_','$',1))
+                                            u.password.replace('_','$',1)))
         
         if options['verbosity'] >= 1:
             self.stdout.write(
