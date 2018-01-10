632c364ce97cf445de21f9c8f9fea160a098a544
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-09 23:24:35 -0800

    Testing hospital

--- login/tests/setup.py
+++ login/tests/setup.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from django.contrib.auth.models import User
+from django.conf import settings
 
 from ambulance.models import Ambulance, \
     AmbulanceStatus, AmbulanceCapability
