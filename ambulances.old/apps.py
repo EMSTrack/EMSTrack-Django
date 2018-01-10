dcde13538599fbadfbf1c57533300ebd8108eb68
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-09 23:35:45 -0800

    Removing ambulances.old

--- ambulances.old/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class AmbulancesConfig(AppConfig):
-    name = 'ambulances'
-
-    def ready(self):
-        import ambulances.signals
--- ambulances.old/static/ambulances/js/ambulance_list.js
+++
@@ -1,5 +0,0 @@
-
-var mylist;
-$(document).ready(function() {
-
-}
