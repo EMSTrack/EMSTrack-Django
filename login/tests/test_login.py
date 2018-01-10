7178f2fd6ec2ab5baa9b68b554c33f47ad229a54
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-09 23:40:06 -0800

    More tests

--- login/tests/test_login.py
+++ login/tests/test_login.py
@@ -586,7 +586,7 @@ class TestMQTTACLPublish(MyTestCase):
                                  'acc': '2',
                                  'topic': '/user/testuser1/hospital/{}/data'.format(self.a1.id) },
                                follow=True)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 403)
         
         # can't publish
         response = self.client.post('/auth/mqtt/acl/',
