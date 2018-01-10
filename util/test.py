8ee773d10c1e590de58b5d769949920fe8b523b1
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-09 23:14:57 -0800

    Testing hospital

---
+++ util/test.py
@@ -0,0 +1,11 @@
+def date2iso(date):
+    if date is not None:
+        return date.isoformat().replace('+00:00','Z')
+    return date
+
+def point2str(point):
+    #return 'SRID=4326;' + str(point)
+    if point is not None:
+        return str(point)
+    return point
+
