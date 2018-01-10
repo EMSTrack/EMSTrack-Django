cd28462b46048d165f81b57690be00a41165db32
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-09 17:57:00 -0800

    Moved Profile and Permissions to login

--- login/viewsets.py
+++ login/viewsets.py
@@ -6,9 +6,7 @@ from rest_framework.decorators import detail_route
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from ambulances.models import Ambulance, Hospital
-
-from .models import Profile, HospitalEquipment, Equipment
+from .models import Profile
 
 from .serializers import ExtendedProfileSerializer
 
@@ -39,68 +37,3 @@ class ProfileViewSet(viewsets.GenericViewSet):
     def profile(self, request, **kwargs):
         return Response(ExtendedProfileSerializer(self.get_object()).data)
     
-# BasePermissionViewSet
-
-class BasePermissionViewSet(viewsets.GenericViewSet):
-
-    filter_field = 'id'
-    profile_field = 'ambulances'
-    profile_values = 'ambulance_id'
-    queryset = Ambulance.objects.all()
-    
-    def get_queryset(self):
-
-        #print('@get_queryset {}({})'.format(self.request.user,
-        #                                    self.request.method))
-        
-        # return all objects if superuser
-        user = self.request.user
-        if user.is_superuser:
-            return self.queryset
-
-        # return nothing if anonymous
-        if user.is_anonymous:
-            raise PermissionDenied()
-
-        # print('> METHOD = {}'.format(self.request.method))
-        # otherwise only return objects that the user can read or write to
-        if self.request.method == 'GET':
-            # objects that the user can read
-            can_do = getattr(user.profile,
-                             self.profile_field).filter(can_read=True).values(self.profile_values)
-
-        elif (self.request.method == 'PUT' or
-              self.request.method == 'PATCH' or
-              self.request.method == 'DELETE'):
-            # objects that the user can write to
-            can_do = getattr(user.profile,
-                             self.profile_field).filter(can_write=True).values(self.profile_values)
-            
-        else:
-            raise PermissionDenied()
-
-        #print('> user = {}, can_do = {}'.format(user, can_do))
-        #print('> objects = {}'.format(Object.objects.all()))
-        #print('> filtered objects = {}'.format(Object.objects.filter(id__in=can_do)))
-        filter = {}
-        filter[self.filter_field + '__in'] = can_do
-        return self.queryset.filter(**filter)
-    
-# AmbulancePermission
-
-class AmbulancePermissionViewSet(BasePermissionViewSet):
-
-    filter_field = 'id'
-    profile_field = 'ambulances'
-    profile_values = 'ambulance_id'
-    queryset = Ambulance.objects.all()
-    
-# HospitalPermission
-
-class HospitalPermissionViewSet(BasePermissionViewSet):
-    
-    filter_field = 'id'
-    profile_field = 'hospitals'
-    profile_values = 'hospital_id'
-    queryset = Hospital.objects.all()
-
