015bd484e0fbcb4553b89e29a219e618d8954361
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-13 17:47:27 -0800

    Hospital webapp

--- emstrack/models.py
+++ emstrack/models.py
@@ -2,6 +2,13 @@ from django.contrib.auth.models import User
 
 from django.contrib.gis.db import models
 
+defaults = {
+    'location': Point(-117.0382, 32.5149, srid=4326),
+    'state': 'BC',
+    'city': 'Tijuana',
+    'country': 'MX',
+}
+
 class AddressModel(models.Model):
     """
     An abstract base class model that provides address fields.
@@ -10,12 +17,12 @@ class AddressModel(models.Model):
     number = models.CharField(max_length=30, default = "")
     street = models.CharField(max_length=254, default = "")
     unit = models.CharField(max_length=30, null=True, blank=True)
-    city = models.CharField(max_length=100, default = "")
-    state = models.CharField(max_length=2, default = "")
+    city = models.CharField(max_length=100, default = defaults.city)
+    state = models.CharField(max_length=2, default = defaults.state)
     zipcode = models.CharField(max_length=12, default = "")
-    country = models.CharField(max_length=2, default = "")
+    country = models.CharField(max_length=2, default = defaults.country)
 
-    location = models.PointField(srid=4326, null=True, blank=True)
+    location = models.PointField(srid=4326, default = defaults.location)
     
     class Meta:
         abstract = True
