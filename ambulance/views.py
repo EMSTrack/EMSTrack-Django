a8c9b1efc3247783b036fa58c4513bf423dc4bdf
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-13 18:37:10 -0800

    Restructuring templates and static

--- ambulance/views.py
+++ ambulance/views.py
@@ -56,7 +56,7 @@ class AmbulanceUpdateView(LoginRequiredMixin,
         return self.object.get_absolute_url()
 
 class AmbulanceMap(TemplateView):
-     template_name = reverse('ambulance:map')
+     template_name = 'ambulance/map.html')
 
      def get_context_data(self, **kwargs):
          context = super().get_context_data(**kwargs)
