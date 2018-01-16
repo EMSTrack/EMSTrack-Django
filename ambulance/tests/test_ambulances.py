6ea9d0ed65f5515d021b50ac217c2bdfc321dde7
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-15 17:02:35 -0800

    New serializer for point

--- ambulance/tests/test_ambulances.py
+++ ambulance/tests/test_ambulances.py
@@ -232,7 +232,7 @@ class TestAmbulanceUpdate(TestSetup):
             'capability': a.capability,
             'status': a.status,
             'orientation': None,
-            'location': location,
+            'location': point2str(location),
             'location_timestamp': date2iso(location_timestamp),
             'updated_by': user.id,
             'updated_on': date2iso(a.updated_on)
