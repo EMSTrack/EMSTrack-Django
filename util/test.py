73b56c003169d29fad5781d85b3f8fdc66c5cec8
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-16 07:07:57 -0800

    Addind defaults for location and orientation in ambulance

--- util/test.py
+++ util/test.py
@@ -1,3 +1,5 @@
+from django.contrib.gis.geos import GEOSGeometry
+
 def date2iso(date):
     if date is not None:
         return date.isoformat().replace('+00:00','Z')
@@ -11,8 +13,14 @@ def date2iso(date):
 
 def point2str(point):
     #return 'SRID=4326;' + str(point)
-    if point is not None:
+    if point is None:
+        return None
+    if isinstance(point, GEOSGeometry):
+        return {'latitude': str(value.y),
+                'longitude': str(value.x)}
+    elif isinstance(point, dict):
         return {'latitude': str(point['latitude']),
                 'longitude': str(point['longitude'])}
-    return point
+    else:
+        raise Exception("Unkonw point type")
 
