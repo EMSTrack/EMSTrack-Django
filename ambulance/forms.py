230ad24be39b1b80ec1bc4d00a2e2ff51775554d
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-01-16 16:49:07 -0800

    Plug in map widget

--- ambulance/forms.py
+++ ambulance/forms.py
@@ -10,8 +10,7 @@ class LeafletPointWidget(gis_widgets.BaseGeometryWidget):
 
     class Media:
         css = {
-            'all': ('http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css',
-                    'leaflet/css/LeafletWidget.css')
+            'all': ('leaflet/css/LeafletWidget.css')
         }
         js = (
             'http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js',
