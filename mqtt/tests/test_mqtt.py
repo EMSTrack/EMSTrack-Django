05fcd9dc9e40f84a4b50b337bda609263994f41f
Mauricio de Oliveira <mauricio@ucsd.edu>
2018-02-01 12:22:18 -0800

    Added user/*username*/client/*client-id*/status message to mqtt

--- mqtt/tests/test_mqtt.py
+++ mqtt/tests/test_mqtt.py
@@ -413,6 +413,7 @@ class TestMQTTPublish(TestMQTT, MQTTTestCase):
                                             equipment_id = self.e1.id)
         self.assertEqual(obj.value, 'False')
 
+        
         # Start client as testuser1
         broker = {
             'HOST': 'localhost',
@@ -471,6 +472,83 @@ class TestMQTTPublish(TestMQTT, MQTTTestCase):
                                             equipment_id = self.e1.id)
         self.assertEqual(obj.value, 'True')
 
+
+        # Start client as testuser2
+        broker = {
+            'HOST': 'localhost',
+            'PORT': 1883,
+            'KEEPALIVE': 60,
+            'CLEAN_SESSION': True
+        }
+        
+        # Start test client
+        
+        broker.update(settings.MQTT)
+        broker['CLIENT_ID'] = 'test_mqtt_publish_admin'
+        broker['USERNAME'] = 'testuser2'
+        broker['PASSWORD'] = 'very_secret'
+        
+        client = MQTTTestClient(broker,
+                                check_payload = False,
+                                debug=False)
+        self.is_connected(client)
+
+        # subscribe to ambulance/+/data
+        topics = ('ambulance/{}/data'.format(self.a3.id),
+                  'hospital/{}/data'.format(self.h1.id),
+                  'hospital/{}/equipment/{}/data'.format(self.h1.id,
+                                                         self.e1.name))
+        [client.expect(t) for t in topics]
+        self.is_subscribed(client)
+
+        # process messages
+        self.loop(client)
+
+        # expect more ambulance
+        client.expect(topics[0])
+
+        # modify data in ambulance and save should trigger message
+        obj = Ambulance.objects.get(id = self.a3.id)
+        self.assertEqual(obj.status, AmbulanceStatus.R.name)
+        obj.status = AmbulanceStatus.OS.name
+        obj.save()
+        
+        # process messages
+        self.loop(client)
+
+        # assert change
+        obj = Ambulance.objects.get(id=self.a3.id)
+        self.assertEqual(obj.status, AmbulanceStatus.OS.name)
+
+        # expect more hospital and equipment
+        [client.expect(t) for t in topics[1:]]
+
+        # modify data in hospital and save should trigger message
+        obj = Hospital.objects.get(id = self.h1.id)
+        self.assertEqual(obj.comment, 'no comments')
+        obj.comment = 'yet no comments'
+        obj.save()
+        
+        # modify data in hospital_equipment and save should trigger message
+        obj = HospitalEquipment.objects.get(hospital_id = self.h1.id,
+                                            equipment_id = self.e1.id)
+        self.assertEqual(obj.value, 'True')
+        obj.value = 'False'
+        obj.save()
+        
+        # process messages
+        self.loop(client)
+        client.wait()
+        
+        # assert changes
+        obj = Hospital.objects.get(id=self.h1.id)
+        self.assertEqual(obj.comment, 'yet no comments')
+        
+        obj = HospitalEquipment.objects.get(hospital_id = self.h1.id,
+                                            equipment_id = self.e1.id)
+        self.assertEqual(obj.value, 'False')
+
+        
 class TestMQTTSubscribe(TestMQTT, MQTTTestCase):
 
     def test(self):
