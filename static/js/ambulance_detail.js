c095ac162b1d413a39b26c95df4d0c3e2645f94c
thl024 <lamtimothy1996@gmail.com>
2018-02-15 14:35:14 -0800

    AmbulanceDetailMap

---
+++ static/js/ambulance_detail.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	// Set map view
+    mymap = L.map('live-map').setView([32.5149, -117.0382], 12);
+
+    // Add layer to map.
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieWFuZ2Y5NiIsImEiOiJjaXltYTNmbTcwMDJzMzNwZnpzM3Z6ZW9kIn0.gjEwLiCIbYhVFUGud9B56w', {
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'pk.eyJ1IjoieWFuZ2Y5NiIsImEiOiJjaXltYTNmbTcwMDJzMzNwZnpzM3Z6ZW9kIn0.gjEwLiCIbYhVFUGud9B56w'
+    }).addTo(mymap);
+}
