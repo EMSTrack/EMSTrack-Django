1b3dfbd4216cf302a2997b2f25ee898c145036c1
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-02-16 16:29:03 -0800

    Testing.

--- ambulance/tests/test_ambulances.py
+++ ambulance/tests/test_ambulances.py
@@ -660,7 +660,7 @@ class TestAmbulanceUpdates(TestSetup):
             serializer = AmbulanceUpdateSerializer(u)
             result = {
                 'id': u.id,
-                'ambulance_identifier': a.identifier,
+                'ambulance_identifier': u.identifier,
                 'comment': u.comment,
                 'status': u.status,
                 'orientation': u.orientation,
