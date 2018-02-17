112722ca4da15bc5ebce4f24791c1ba997c91959
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-02-16 16:10:21 -0800

    Testing.

--- ambulance/tests/test_ambulances.py
+++ ambulance/tests/test_ambulances.py
@@ -646,6 +646,13 @@ class TestAmbulanceUpdates(TestSetup):
         serializer.is_valid()
         serializer.save(updated_by=user)
 
+        serializer = AmbulanceSerializer(a,
+                                         data={
+                                             'identifier': 'someid',
+                                         }, partial=True)
+        serializer.is_valid()
+        serializer.save(updated_by=user)
+
         # test AmbulanceUpdateSerializer
         queryset = AmbulanceUpdate.objects.filter(ambulance=a)
         answer1 = []
@@ -729,7 +736,7 @@ class TestAmbulanceUpdates(TestSetup):
         self.assertEqual(response.status_code, 200)
         result = JSONParser().parse(BytesIO(response.content))
         self.assertCountEqual(result['results'], answer1)
-        self.assertEqual(len(result['results']), 3)
+        self.assertEqual(len(result['results']), 4)
 
         # retrieve ambulances updates
         response = client.get('/api/ambulance/{}/updates/'.format(self.a3.id),
@@ -737,7 +744,7 @@ class TestAmbulanceUpdates(TestSetup):
         self.assertEqual(response.status_code, 200)
         result = JSONParser().parse(BytesIO(response.content))
         self.assertCountEqual(result['results'], answer3)
-        self.assertEqual(len(result['results']), 3)
+        self.assertEqual(len(result['results']), 4)
 
         # logout
         client.logout()
@@ -772,7 +779,7 @@ class TestAmbulanceUpdates(TestSetup):
         self.assertEqual(response.status_code, 200)
         result = JSONParser().parse(BytesIO(response.content))
         self.assertCountEqual(result['results'], answer3)
-        self.assertEqual(len(result['results']), 3)
+        self.assertEqual(len(result['results']), 4)
 
         # logout
         client.logout()
