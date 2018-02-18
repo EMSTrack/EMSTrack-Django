5c249116ef9839d272a595e2a9334006c9ccac40
thl024 <lamtimothy1996@gmail.com>
2018-02-18 10:07:46 -0800

    AmbDetail Map Widget

--- static/js/ambulance_detail.js
+++ static/js/ambulance_detail.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
-	// Set map view
-    map = L.map('live-map').setView([32.5149, -117.0382], 12);
+	// // Set map view
+ //    map = L.map('live-map').setView([32.5149, -117.0382], 12);
 
-    // Add layer to map.
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieWFuZ2Y5NiIsImEiOiJjaXltYTNmbTcwMDJzMzNwZnpzM3Z6ZW9kIn0.gjEwLiCIbYhVFUGud9B56w', {
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	maxZoom: 22,
-	id: 'mapbox.streets',
-	accessToken: 'pk.eyJ1IjoieWFuZ2Y5NiIsImEiOiJjaXltYTNmbTcwMDJzMzNwZnpzM3Z6ZW9kIn0.gjEwLiCIbYhVFUGud9B56w'
-    }).addTo(map);
+ //    // Add layer to map.
+ //    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieWFuZ2Y5NiIsImEiOiJjaXltYTNmbTcwMDJzMzNwZnpzM3Z6ZW9kIn0.gjEwLiCIbYhVFUGud9B56w', {
+	// attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	// maxZoom: 22,
+	// id: 'mapbox.streets',
+	// accessToken: 'pk.eyJ1IjoieWFuZ2Y5NiIsImEiOiJjaXltYTNmbTcwMDJzMzNwZnpzM3Z6ZW9kIn0.gjEwLiCIbYhVFUGud9B56w'
+ //    }).addTo(map);
 
-    console.log(ambulance_id);
-    retrieveAmbulances(ambulance_id)
+ //    console.log(ambulance_id);
+ //    retrieveAmbulances(ambulance_id)
+
+ 	options = {
+ 		map_id: "map"
+ 	}
+ 	LeafletWidget(options)
+
 });
 
 function retrieveAmbulances(ambulance_id) {
