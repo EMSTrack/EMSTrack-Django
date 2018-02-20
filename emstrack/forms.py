68f38b71f776dec7bf54a173514cb116fdb32916
Mauricio de Oliveira <mauricio@ucsd.edu>
2018-02-20 13:25:05 -0800

    Recovering PointWidget template.

--- emstrack/forms.py
+++ emstrack/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.gis.forms import widgets
 
 class LeafletPointWidget(widgets.BaseGeometryWidget):
-    template_name = 'leaflet/leaflet.html'
+    template_name = 'leaflet_point_widged.html'
 
     class Media:
         css = {
