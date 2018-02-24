82cc1cdd8c749d0a966e07403ee6a9e33e12834e
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-02-24 12:04:01 -0800

    Adding group profiles.

--- login/admin.py
+++ login/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 
 # Register your models here.
 
@@ -23,4 +23,3 @@ class UserAdmin(BaseUserAdmin):
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
-admin.site.register(Group)
