66201c0b81a5c2e21217dbba0de2a5295d5d0156
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-02-26 09:33:44 -0800

    Adding group permissions.

--- login/tests/test_permissions.py
+++ login/tests/test_permissions.py
@@ -9,3 +9,24 @@ class TestPermissions(TestSetup):
         u = self.u1
         perms = Permissions(u)
         self.assertEqual(len(perms.ambulances), 1)
+        self.assertEqual(len(perms.hospitals), 2)
+
+        u = self.u2
+        perms = Permissions(u)
+        self.assertEqual(len(perms.ambulances), 0)
+        self.assertEqual(len(perms.hospitals), 2)
+
+        u = self.u3
+        perms = Permissions(u)
+        self.assertEqual(len(perms.ambulances), 2)
+        self.assertEqual(len(perms.hospitals), 0)
+
+        u = self.u4
+        perms = Permissions(u)
+        self.assertEqual(len(perms.ambulances), 0)
+        self.assertEqual(len(perms.hospitals), 2)
+
+        u = self.u5
+        perms = Permissions(u)
+        self.assertEqual(len(perms.ambulances), 3)
+        self.assertEqual(len(perms.hospitals), 2)
