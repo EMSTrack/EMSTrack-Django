4bcbe123de4e17829686163bf8e762d1f1da259a
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-02-25 21:49:59 -0800

    Fetching groups.all() == None bug.

--- login/viewsets.py
+++ login/viewsets.py
@@ -20,7 +20,7 @@ class IsUserOrAdminOrSuper(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.user.is_superuser or
                 request.user.is_staff or
-                obj.user == request.user)
+                obj == request.user)
 
 
 # Profile viewset
