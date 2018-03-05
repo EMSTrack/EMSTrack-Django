82fe653bcc67292a3060b31c0f5bfcab9af09cb9
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-03-04 17:15:20 -0800

    Separating dispatch from map.js

--- static/js/map.js
+++ static/js/map.js
@@ -87,8 +87,9 @@ $(document).ready(function () {
 
     // Set map view
     mymap = L.map('live-map').setView([32.5149, -117.0382], 12);
-    L.mapbox.accessToken = accessToken;
-    geocoder = L.mapbox.geocoder('mapbox.places');
+
+    //L.mapbox.accessToken = accessToken;
+    //geocoder = L.mapbox.geocoder('mapbox.places');
 
     // Add layer to map.
     L.tileLayer(
