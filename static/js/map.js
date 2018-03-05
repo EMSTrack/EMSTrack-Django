33977acd266421368c44b7976ae4f89941f5fe6e
Mauricio de Oliveira <mauricio@ucsd.edu>
2018-03-05 15:30:17 -0800

    Geocoding on the dispatch application.

--- static/js/map.js
+++ static/js/map.js
@@ -656,7 +656,7 @@ function createCategoryFilter() {
     //Generate HTML code for checkboxes for each of the statuses.
     var filterHtml = "";
 
-    filterHtml += '<div style="border-style: solid; border-radius: 5px; padding: 0px 5px 0px 5px; margin: 0px 0px 0px 0px">';
+    filterHtml += '<div style="border-style: solid; border-width: thin; border-radius: 5px; padding: 0px 5px 0px 5px; margin: 0px 0px 0px 0px">';
     Object.keys(ambulance_status).forEach(function (status) {
 
         categoryGroupLayers[status] = L.layerGroup(markersByCategory[status]);
@@ -671,7 +671,7 @@ function createCategoryFilter() {
     filterHtml += "</div>";
 
     //Generate HTML code for checkboxes for hospital
-    filterHtml += '<div style="border-style: solid; border-radius: 5px; padding: 0px 5px 0px 5px; margin: 0px 0px 0px 0px">';
+    filterHtml += '<div style="border-style: solid; border-width: thin; border-radius: 5px; padding: 0px 5px 0px 5px; margin: 0px 0px 0px 0px">';
     let category = 'Hospital'
     categoryGroupLayers[category] = L.layerGroup(markersByCategory[category]);
     categoryGroupLayers[category].addTo(mymap);
@@ -682,7 +682,7 @@ function createCategoryFilter() {
     filterHtml += "</div>";
 
     //Generate HTML code for checkboxes for locations
-    filterHtml += '<div style="border-style: solid; border-radius: 5px; padding: 0px 5px 0px 5px; margin: 0px 0px 0px 0px">';
+    filterHtml += '<div style="border-style: solid; border-width: thin; border-radius: 5px; padding: 0px 5px 0px 5px; margin: 0px 0px 0px 0px">';
     Object.keys(location_type).forEach(function (type) {
 
         categoryGroupLayers[type] = L.layerGroup(markersByCategory[type]);
