faa484d2213c9871e9118aa68f0f8a1a5da9bc72
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-03-07 08:40:48 -0800

    Bootstrap 4.0

--- login/forms.py
+++ login/forms.py
@@ -17,6 +17,60 @@ from .models import TemporaryPassword, GroupProfile, UserAmbulancePermission, Us
 
 
 class SignupForm(auth_forms.UserCreationForm):
+    username = auth_forms.UsernameField(
+        label=_("Username"),
+        max_length=150,
+        required=True,
+        widget=forms.TextInput(
+            attrs={'autofocus': True,
+                   'placeholder': 'Username',
+                   'class': 'form-control input-lg'}
+        ),
+    )
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Password',
+                   'class': 'form-control input-lg'}
+        ),
+    )
+    password2 = forms.CharField(
+        label=_("Confirm password"),
+        strip=False,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Confirm password',
+                   'class': 'form-control input-lg'}
+        ),
+    )
+    first_name = forms.CharField(
+        label=_("First name"),
+        max_length=30,
+        required=False,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'First name',
+                   'class': 'form-control input-lg'}
+        )
+    )
+    last_name = forms.CharField(
+        label=_("Last name"),
+        max_length=30,
+        required=False,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Last name',
+                   'class': 'form-control input-lg'}
+        )
+    )
+    email = forms.EmailField(
+        label=_("Email"),
+        required=True,
+        widget=forms.EmailInput(
+            attrs={'placeholder': 'Email',
+                   'class': 'form-control input-lg'}
+        )
+    )
 
     class Meta:
         model = User
