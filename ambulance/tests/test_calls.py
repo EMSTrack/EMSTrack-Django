4fd1dea9dea1cd7aacf0a0eb8c7525bc26e102a4
Den <den@Dens-MacBook-Pro.local>
2018-03-07 13:34:24 -0800

    newest test call

--- ambulance/tests/test_calls.py
+++ ambulance/tests/test_calls.py
@@ -13,10 +13,7 @@ from login.tests.setup_data import TestSetup
 class TestCall(TestSetup):
     def test_call_serializer(self):
 
-        c1 = Call.objects.create(
-            number="123",
-            street="dunno",
-            updated_by=self.u1)
+        c1 = Call.objects.create( number="123", street="dunno", updated_by=self.u1 )
 
         serializer = CallSerializer(c1)
         result = {
