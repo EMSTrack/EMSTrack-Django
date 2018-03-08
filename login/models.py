4cbe22add4e0973cdb595e52bd4a54c843b99dc1
thl024 <lamtimothy1996@gmail.com>
2018-03-08 14:24:42 -0800

    Group ordering

--- login/models.py
+++ login/models.py
@@ -40,7 +40,7 @@ class GroupProfile(models.Model):
         return '{}: description = {}'.format(self.group, self.description)
 
     class Meta:
-        order_with_respect_to = 'level'
+        ordering = ['level']
 
 
 # Group Ambulance and Hospital Permissions
