5e4f7f41f79a34118161741c197bffd49e9df5b2
thl024 <lamtimothy1996@gmail.com>
2018-03-08 14:53:12 -0800

    Order by

--- login/models.py
+++ login/models.py
@@ -39,8 +39,8 @@ class GroupProfile(models.Model):
     def __str__(self):
         return '{}: description = {}'.format(self.group, self.description)
 
-    # class Meta:
-    #     ordering = ['level']
+    class Meta:
+        ordering = ['level']
 
 
 # Group Ambulance and Hospital Permissions
--- login/permissions.py
+++ login/permissions.py
@@ -68,13 +68,14 @@ class Permissions:
 
             else:
 
+                print(user.groups.all())
+
                 # regular users, loop through groups
                 for group in user.groups.all():
                     for (profile_field, object_field) in zip(self.profile_fields, self.object_fields):
-                        # e.g.: objs = group.groupambulancepermission_set.all()
-                        objs = getattr(group, 'group' + object_field + 'permission_set').all().order_by('level')
 
-                        print(objs)
+                        # e.g.: objs = group.groupambulancepermission_set.all()
+                        objs = getattr(group, 'group' + object_field + 'permission_set').all()
 
                         # e.g.: self.ambulances.update({e.ambulance_id: {...} for e in objs})
                         getattr(self, profile_field).update({
