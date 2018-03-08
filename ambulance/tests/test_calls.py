c2e912076a4f3ea93d3816de7ff37e5d4790ab4d
Den <den@Dens-MacBook-Pro.local>
2018-03-08 00:52:02 -0800

    fixed syntax

--- ambulance/tests/test_calls.py
+++ ambulance/tests/test_calls.py
@@ -33,7 +33,7 @@ class TestCall(TestSetup):
             'ended_at': date2iso(c1.ended_at),
             'comment': c1.comment,
             'updated_by': c1.updated_by.id,
-            'updated_on': date2iso(c1.updated_on)
+            'updated_on': date2iso(c1.updated_on),
             'ambulances': c1.ambulances
         }
         self.assertDictEqual(serializer.data, result)
