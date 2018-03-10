e19920e3ae1c48e95b9c003d8ef54f72436627dc
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-03-10 06:49:31 -0800

    Modifying HospitalSerializer to include equipment.

--- hospital/serializers.py
+++ hospital/serializers.py
@@ -53,7 +53,7 @@ class HospitalSerializer(serializers.ModelSerializer):
                   'city', 'state', 'zipcode', 'country',
                   'location',
                   'name',
-                  'equipment',
+                  'hospitalequipments',
                   'comment', 'updated_by', 'updated_on']
         read_only_fields = ('hospitalequipments', 'updated_by',)
 
