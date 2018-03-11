61750e17eeae6074cc7929460601d4218cd3fc3a
Den <den@Dens-MacBook-Pro.local>
2018-03-10 21:52:56 -0800

    field ambulance_set

--- ambulance/tests/test_calls.py
+++ ambulance/tests/test_calls.py
@@ -35,7 +35,7 @@ class TestCall(TestSetup):
             'comment': c1.comment,
             'updated_by': c1.updated_by.id,
             'updated_on': date2iso(c1.updated_on),
-            'ambulance_set': c1.ambulances_set
+            'ambulance_set': c1.ambulances
         }
         self.assertDictEqual(serializer.data, result)
 
