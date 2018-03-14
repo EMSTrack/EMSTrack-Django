4cd02f0b0b72c4bf49534b1dcbd128d70a9c0f9e
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-03-13 21:22:29 -0700

    Page size links in ambulance detail.

--- emstrack/views.py
+++ emstrack/views.py
@@ -21,34 +21,28 @@ def get_page_links(request, page, page_query_param='page'):
     return _get_page_links(page_numbers, current, page_number_to_url)
 
 
-def get_page_size_links(request, paginator, page, page_sizes, page_query_param='page', page_size_query_param='page_size'):
+def get_page_size_links(request, page, page_sizes, page_query_param='page', page_size_query_param='page_size'):
 
     base_url = request.build_absolute_uri()
 
     def page_size_to_url(page_size, page_number):
+
+        # remove query params
+        url = remove_query_param(base_url, page_query_param)
+        url = remove_query_param(url, page_size_query_param)
+
         # process page number
-        if page_number == 1:
-            url = remove_query_param(base_url, page_query_param)
-        else:
+        if page_number > 1:
             url = replace_query_param(base_url, page_query_param, page_number)
 
         # process page size
-        return replace_query_param(base_url, page_size_query_param, page_size)
+        return replace_query_param(url, page_size_query_param, page_size)
 
-    def get_page_sizes(item_count, start_index, page_sizes):
-        _page_sizes = []
-        _page_numbers = []
-        for size in page_sizes:
-            if item_count > size:
-                _page_sizes.append(size)
-                _page_numbers.append(math.floor(start_index / size))
-
-        return zip(_page_sizes, _page_numbers)
-
-    item_count = paginator.count
     start_index = page.start_index()
-    return [{ 'url': page_size_to_url(page_size, page_number), 'size': page_size}
-            for page_size, page_number in get_page_sizes(item_count, start_index, page_sizes)]
+
+    return [{'url': page_size_to_url(page_size, page_number), 'size': page_size}
+            for page_size, page_number in
+            zip(*[(size, math.floor(start_index / size)) for size in page_sizes])]
 
 
 class IndexView(TemplateView):
