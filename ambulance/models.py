0b1127d41f46a001b173d47e4d2a228e607d589e
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-03-30 17:19:59 -0700

    Adding a location_client to Ambulance.

--- ambulance/models.py
+++ ambulance/models.py
@@ -122,6 +122,8 @@ class Ambulance(UpdatedByModel):
                                                  self.location,
                                                  self.orientation))
 
+        logger.debug('self._loaded_values = {}'.format(self._loaded_values))
+
         # if location_client changed
         if (self._loaded_values is not None) and self._loaded_values['location_client'] == self.location_client:
             model_changed = False
