b9a46e28ab9f6051d181a38d03d96d255d1b4969
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-03-30 18:21:11 -0700

    Adding a location_client to Ambulance.

--- ambulance/tests/test_ambulances.py
+++ ambulance/tests/test_ambulances.py
@@ -316,7 +316,7 @@ class TestAmbulanceUpdate(TestSetup):
 
         serializer = AmbulanceSerializer(a,
                                          data={
-                                             'location_client': client1
+                                             'location_client': client1.id
                                          }, partial=True)
         serializer.is_valid()
         logger.debug('id = "{}"'.format(client1.id))
