0483907cca19c3c0a349a02d4ce9dadf5ba5da0e
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-04-01 11:27:01 -0700

    Adding a location_client to Ambulance.

--- mqtt/subscribe.py
+++ mqtt/subscribe.py
@@ -263,8 +263,14 @@ class SubscribeClient(BaseClient):
                     # retrieve location client
                     new_location_client = Ambulance.objects.get(id=ambulance_id).location_client
 
+                    logger.debug("old_location_client: {}".format(old_location_client))
+                    logger.debug("new_location_client: {}".format(new_location_client))
+
                     # change in location client?
                     if old_location_client != new_location_client:
+
+                        logger.debug("location_client changed from '{}' to {}".format(old_location_client,
+                                                                                      new_location_client))
 
                         # log in old client
                         if old_location_client:
