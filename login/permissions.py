22805c7f4038dc515289682b8edea6cf8a81ae34
thl024 <lamtimothy1996@gmail.com>
2018-04-02 07:24:49 -0700

    Group priority tests

--- login/permissions.py
+++ login/permissions.py
@@ -107,8 +107,11 @@ class Permissions:
                     if obj['can_write']:
                         # e.g.: self.can_write['ambulances'].append(obj['id'])
                         self.can_write[profile_field].append(id)
-                logger.debug('can_read[{}] = {}'.format(profile_field, self.can_read[profile_field]))
-                logger.debug('can_write[{}] = {}'.format(profile_field, self.can_write[profile_field]))
+                # logger.debug('can_read[{}] = {}'.format(profile_field, self.can_read[profile_field]))
+                # logger.debug('can_write[{}] = {}'.format(profile_field, self.can_write[profile_field]))
+
+                print('can_read[{}] = {}'.format(profile_field, self.can_read[profile_field]))
+                print('can_write[{}] = {}'.format(profile_field, self.can_write[profile_field]))
 
     def check_can_read(self, **kwargs):
         assert len(kwargs) == 1
