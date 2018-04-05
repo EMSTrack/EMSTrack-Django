3aa9112048b2fc25cb53a447b6a41a39c6cbe0c4
Den <den@Dens-MacBook-Pro.local>
2018-04-05 15:01:58 -0700

    syntax error fixed

--- ambulance/models.py
+++ ambulance/models.py
@@ -284,7 +284,7 @@ class Call(AddressModel, UpdatedByModel):
     CALL_STATUS_CHOICES = \
         [(m.name, m.value) for m in CallStatus]
     status = models.CharField(max_length=1,
-                              choices=CALL_STATUS_CHOICES
+                              choices=CALL_STATUS_CHOICES,
                               default=CallStatus.P.name)
 
     # details
