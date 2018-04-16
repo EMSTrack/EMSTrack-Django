10814bf760ff5a1f7da807b7ea394c477805f050
Mauricio de Oliveira <mauricio@ucsd.edu>
2018-04-16 15:48:58 -0700

    Improved call tests.

--- ambulance/tests/test_calls.py
+++ ambulance/tests/test_calls.py
@@ -1,7 +1,7 @@
 from django.db import IntegrityError
 from rest_framework.parsers import JSONParser
 
-from ambulance.models import Call, Patient, AmbulanceCallTime, CallStatus
+from ambulance.models import Call, Patient, AmbulanceCallTime, CallStatus, CallPriority
 from ambulance.serializers import CallSerializer, AmbulanceCallTimeSerializer, PatientSerializer
 from emstrack.tests.util import date2iso, point2str
 
@@ -11,11 +11,92 @@ from login.tests.setup_data import TestSetup
 class TestCall(TestSetup):
 
     def test_call_serializer(self):
-        # test CallSerializer
+
         c1 = Call.objects.create(number="123", street="dunno", updated_by=self.u1)
+
+        ambCallTime1 = AmbulanceCallTime.objects.create(call=c1, ambulance=self.a1)
+
+        ambCallTime = ambCallTime1
+        serializer = AmbulanceCallTimeSerializer(ambCallTime)
+        result = {
+            'id': ambCallTime.id,
+            'call_id': ambCallTime.call.id,
+            'ambulance_id': ambCallTime.ambulance.id,
+            'dispatch_time': date2iso(ambCallTime.dispatch_time),
+            'departure_time': date2iso(ambCallTime.departure_time),
+            'patient_time': date2iso(ambCallTime.patient_time),
+            'hospital_time': date2iso(ambCallTime.hospital_time),
+            'end_time': date2iso(ambCallTime.end_time)
+        }
+        self.assertDictEqual(serializer.data, result)
+
+        ambCallTime2 = AmbulanceCallTime.objects.create(call=c1, ambulance=self.a3)
+
+        ambCallTime = ambCallTime2
+        serializer = AmbulanceCallTimeSerializer(ambCallTime)
+        result = {
+            'id': ambCallTime.id,
+            'call_id': ambCallTime.call.id,
+            'ambulance_id': ambCallTime.ambulance.id,
+            'dispatch_time': date2iso(ambCallTime.dispatch_time),
+            'departure_time': date2iso(ambCallTime.departure_time),
+            'patient_time': date2iso(ambCallTime.patient_time),
+            'hospital_time': date2iso(ambCallTime.hospital_time),
+            'end_time': date2iso(ambCallTime.end_time)
+        }
+        self.assertDictEqual(serializer.data, result)
+
         serializer = CallSerializer(c1)
-        ambulancecalltimeserializer = AmbulanceCallTimeSerializer(many=True)
-        patientserializer = PatientSerializer(many=True)
+        ambCallTimeSerializer1 = AmbulanceCallTimeSerializer(ambCallTime1)
+        ambCallTimeSerializer2 = AmbulanceCallTimeSerializer(ambCallTime2)
+
+        expected = {
+            'id': c1.id,
+            'status': c1.status,
+            'details': c1.details,
+            'priority': c1.priority,
+            'number': c1.number,
+            'street': c1.street,
+            'unit': c1.unit,
+            'neighborhood': c1.neighborhood,
+            'city': c1.city,
+            'state': c1.state,
+            'zipcode': c1.zipcode,
+            'country': c1.country,
+            'location': point2str(c1.location),
+            'created_at': date2iso(c1.created_at),
+            'ended_at': date2iso(c1.ended_at),
+            'comment': c1.comment,
+            'updated_by': c1.updated_by.id,
+            'updated_on': date2iso(c1.updated_on),
+            'ambulancecalltime_set': [],
+            'patient_set': []
+        }
+        self.assertCountEqual(serializer.data['ambulancecalltime_set'],
+                              [ambCallTimeSerializer2.data, ambCallTimeSerializer1.data])
+        result = serializer.data
+        result['ambulancecalltime_set'] = []
+        self.assertDictEqual(result, expected)
+
+        with self.assertRaises(IntegrityError) as context:
+            AmbulanceCallTime.objects.create(call=c1, ambulance = self.a1)
+
+    def test_call_serializer_create(self):
+
+        call = {
+            'status': CallStatus.O,
+            'priority': CallPriority.B,
+            'number': '123',
+            'street': 'asdasdasd asd asd asdas',
+        }
+        serializer = CallSerializer(data=call)
+        serializer.is_valid()
+        serializer.save(updated_by=self.u1)
+
+        # test CallSerializer
+        c1 = Call.objects.get(number='123',street='asdasdasd asd asd asdas')
+        serializer = CallSerializer(c1)
+
         result = {
             'id': c1.id,
             'status': c1.status,
@@ -35,10 +116,11 @@ class TestCall(TestSetup):
             'comment': c1.comment,
             'updated_by': c1.updated_by.id,
             'updated_on': date2iso(c1.updated_on),
-            'ambulancecalltime_set': ambulancecalltimeserializer.data,
-            'patient_set': patientserializer.data
+            'ambulancecalltime_set': [],
+            'patient_set': []
         }
         self.assertDictEqual(serializer.data, result)
+
 
     def test_call_update_serializer(self):
         
@@ -171,76 +253,3 @@ class TestCall(TestSetup):
         }
         self.assertDictEqual(serializer.data, result)
 
-    def test_ambulance_call_time_serializer(self):
-
-        c1 = Call.objects.create(number="123", street="dunno", updated_by =
-                self.u1)
-
-        ambCallTime1 = AmbulanceCallTime.objects.create(call=c1, ambulance =
-                self.a1)
-
-        ambCallTime = ambCallTime1
-        serializer = AmbulanceCallTimeSerializer(ambCallTime)
-        result = {
-            'id': ambCallTime.id,
-            'call_id': ambCallTime.call.id,
-            'ambulance_id': ambCallTime.ambulance.id,
-            'dispatch_time': date2iso(ambCallTime.dispatch_time),
-            'departure_time': date2iso(ambCallTime.departure_time),
-            'patient_time': date2iso(ambCallTime.patient_time),
-            'hospital_time': date2iso(ambCallTime.hospital_time),
-            'end_time': date2iso(ambCallTime.end_time)
-        }
-        self.assertDictEqual(serializer.data, result)
-
-        ambCallTime2 = AmbulanceCallTime.objects.create(call=c1, ambulance =
-                self.a3)
-
-        ambCallTime = ambCallTime2
-        serializer = AmbulanceCallTimeSerializer(ambCallTime)
-        result = {
-            'id': ambCallTime.id,
-            'call_id': ambCallTime.call.id,
-            'ambulance_id': ambCallTime.ambulance.id,
-            'dispatch_time': date2iso(ambCallTime.dispatch_time),
-            'departure_time': date2iso(ambCallTime.departure_time),
-            'patient_time': date2iso(ambCallTime.patient_time),
-            'hospital_time': date2iso(ambCallTime.hospital_time),
-            'end_time': date2iso(ambCallTime.end_time)
-        }
-        self.assertDictEqual(serializer.data, result)
-
-        serializer = CallSerializer(c1)
-        ambCallTimeSerializer1 = AmbulanceCallTimeSerializer(ambCallTime1)
-        ambCallTimeSerializer2 = AmbulanceCallTimeSerializer(ambCallTime2)
-
-        expected = {
-            'id': c1.id,
-            'status': c1.status,
-            'details': c1.details,
-            'priority': c1.priority,
-            'number': c1.number,
-            'street': c1.street,
-            'unit': c1.unit,
-            'neighborhood': c1.neighborhood,
-            'city': c1.city,
-            'state': c1.state,
-            'zipcode': c1.zipcode,
-            'country': c1.country,
-            'location': point2str(c1.location),
-            'created_at': date2iso(c1.created_at),
-            'ended_at': date2iso(c1.ended_at),
-            'comment': c1.comment,
-            'updated_by': c1.updated_by.id,
-            'updated_on': date2iso(c1.updated_on),
-            'ambulancecalltime_set': [],
-            'patient_set': []
-        }
-        self.assertCountEqual(serializer.data['ambulancecalltime_set'],
-                              [ambCallTimeSerializer2.data, ambCallTimeSerializer1.data])
-        result = serializer.data
-        result['ambulancecalltime_set'] = []
-        self.assertDictEqual(result, expected)
-
-        with self.assertRaises(IntegrityError) as context:
-            AmbulanceCallTime.objects.create(call=c1, ambulance = self.a1)
