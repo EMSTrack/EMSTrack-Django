f71aca99522c33041e9cf06bb0f5861341f88ddf
Den <den@Dens-MacBook-Pro.local>
2018-04-18 10:11:39 -0700

    improving Call tests

--- ambulance/serializers.py
+++ ambulance/serializers.py
@@ -288,7 +288,7 @@ class CallSerializer(serializers.ModelSerializer):
             for ambulancecalltime_data in ambulancecalltime_set_data:
                 AmbulanceCallTime.objects.create(
                     call=Call.objects.get(number=data['number'],street=data['street']),
-                    ambulance=ambulancecalltime_data['ambulance'],
+                    ambulance=ambulancecalltime_data.pop('ambulance'),
                     **ambulancecalltime_data)
 
         return super().create(data)
