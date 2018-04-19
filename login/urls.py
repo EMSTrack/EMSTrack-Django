24eb3c4cefb13b7afd6df2854f50fd713fc6710f
Mauricio de Oliveira <mauricio@ucsd.edu>
2018-04-19 14:50:00 -0700

    Adding a change password page.

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib.admin.views.decorators import staff_member_required
-from django.contrib.auth import views as auth_views
+
 from login import views
 
 app_name = 'login'
