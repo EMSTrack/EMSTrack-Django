ae7f6a387687abac255f944cd0a073b69854c447
Mauricio de Oliveira <mauricio@ucsd.edu>
2018-04-19 14:45:33 -0700

    Adding a change password page.

--- emstrack/urls.py
+++ emstrack/urls.py
@@ -93,6 +93,28 @@ urlpatterns = [
     # admin
     url(r'^admin/', admin.site.urls),
     
+    # password change
+    url(r'^password_change/$',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change'),
+    url(r'^password_change/done/$',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'),
+
+    # password reset
+    url(r'^password_reset/$',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'),
+    url(r'^password_reset/done/$',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+    url(r'^reset/done/$',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'),
+
     url(r'^$',
         IndexView.as_view(),
         name='index'),
--- login/urls.py
+++ login/urls.py
@@ -19,28 +19,6 @@ urlpatterns = [
         views.SignupView.as_view(),
         name='signup'),
     
-    # password change
-    url(r'^password_change/$',
-        auth_views.PasswordChangeView.as_view(),
-        name='password_change'),
-    url(r'^password_change/done/$',
-        auth_views.PasswordChangeDoneView.as_view(),
-        name='password_change_done'),
-    
-    # password reset
-    url(r'^password_reset/$',
-        auth_views.PasswordResetView.as_view(),
-        name='password_reset'),
-    url(r'^password_reset/done/$',
-        auth_views.PasswordResetDoneView.as_view(),
-        name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm'),
-    url(r'^reset/done/$',
-        auth_views.PasswordResetCompleteView.as_view(),
-        name='password_reset_complete'),
-
     # User Admin
     
     url(r'^user/$',
