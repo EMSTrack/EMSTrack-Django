5746bd00b829ce92fc07a59cf8cf02e8b299cb6c
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-04-28 18:36:32 -0700

    Changed from AmbulanceCallTime to AmbulanceCall in preparation for normalization of call events.

--- ambulance/tests/test_calls.py
+++ ambulance/tests/test_calls.py
@@ -5,9 +5,6 @@ from django.conf import settings
 from django.urls import reverse
 
 from django.db import IntegrityError
-from django.db.utils import IntegrityError as utilsIntegrityError
-from django.utils import timezone
-from rest_framework.parsers import JSONParser
 
 from ambulance.models import Call, Patient, AmbulanceCall, CallStatus, CallPriority
 from ambulance.serializers import CallSerializer, AmbulanceCallSerializer, PatientSerializer
@@ -20,36 +17,111 @@ logger = logging.getLogger(__name__)
 
 class TestCall(TestSetup):
 
+    def test_patient_serializer(self):
+
+        # test PatientSerializer
+        c1 = Call.objects.create(number="123", street="dunno", updated_by=self.u1)
+        p1 = Patient.objects.create(call=c1)
+        serializer = PatientSerializer(p1)
+        result = {
+            'id': p1.id,
+            'name': p1.name,
+            'age': p1.age
+        }
+        self.assertDictEqual(serializer.data, result)
+
     def test_call_serializer(self):
 
+        # create call
         c1 = Call.objects.create(number="123", street="dunno", updated_by=self.u1)
 
+        # it is fine to have no ambulances because it is pending
+        serializer = CallSerializer(c1)
+        expected = {
+            'id': c1.id,
+            'status': c1.status,
+            'details': c1.details,
+            'priority': c1.priority,
+            'number': c1.number,
+            'street': c1.street,
+            'unit': c1.unit,
+            'neighborhood': c1.neighborhood,
+            'city': c1.city,
+            'state': c1.state,
+            'zipcode': c1.zipcode,
+            'country': c1.country,
+            'location': point2str(c1.location),
+            'created_at': date2iso(c1.created_at),
+            'pending_at': date2iso(c1.pending_at),
+            'started_at': date2iso(c1.started_at),
+            'ended_at': date2iso(c1.ended_at),
+            'comment': c1.comment,
+            'updated_by': c1.updated_by.id,
+            'updated_on': date2iso(c1.updated_on),
+            'ambulancecall_set': [],
+            'patient_set': []
+        }
+        self.assertDictEqual(serializer.data, expected)
+
+        # create first ambulance call
         ambulance_call_1 = AmbulanceCall.objects.create(call=c1, ambulance=self.a1)
 
         ambulance_call = ambulance_call_1
         serializer = AmbulanceCallSerializer(ambulance_call)
-        result = {
+        expected = {
             'id': ambulance_call.id,
             'ambulance_id': ambulance_call.ambulance.id,
             'created_at': date2iso(ambulance_call.created_at),
             'ambulancecallevent_set': []
         }
-        self.assertDictEqual(serializer.data, result)
+        self.assertDictEqual(serializer.data, expected)
 
+        serializer = CallSerializer(c1)
+        ambulance_call_serializer_1 = AmbulanceCallSerializer(ambulance_call_1)
+
+        expected = {
+            'id': c1.id,
+            'status': c1.status,
+            'details': c1.details,
+            'priority': c1.priority,
+            'number': c1.number,
+            'street': c1.street,
+            'unit': c1.unit,
+            'neighborhood': c1.neighborhood,
+            'city': c1.city,
+            'state': c1.state,
+            'zipcode': c1.zipcode,
+            'country': c1.country,
+            'location': point2str(c1.location),
+            'created_at': date2iso(c1.created_at),
+            'pending_at': date2iso(c1.pending_at),
+            'started_at': date2iso(c1.started_at),
+            'ended_at': date2iso(c1.ended_at),
+            'comment': c1.comment,
+            'updated_by': c1.updated_by.id,
+            'updated_on': date2iso(c1.updated_on),
+            'ambulancecall_set': [],
+            'patient_set': []
+        }
+        self.assertCountEqual(serializer.data['ambulancecall_set'], [ambulance_call_serializer_1.data])
+        expected = serializer.data
+        expected['ambulancecall_set'] = []
+        self.assertDictEqual(expected, expected)
+
+        # create second ambulance call
         ambulance_call_2 = AmbulanceCall.objects.create(call=c1, ambulance=self.a3)
 
         ambulance_call = ambulance_call_2
         serializer = AmbulanceCallSerializer(ambulance_call)
-        result = {
+        expected = {
             'id': ambulance_call.id,
             'ambulance_id': ambulance_call.ambulance.id,
             'created_at': date2iso(ambulance_call.created_at),
             'ambulancecallevent_set': []
         }
-        self.assertDictEqual(serializer.data, result)
+        self.assertDictEqual(serializer.data, expected)
 
         serializer = CallSerializer(c1)
-        ambulance_call_serializer_1 = AmbulanceCallSerializer(ambulance_call_1)
         ambulance_call_serializer_2 = AmbulanceCallSerializer(ambulance_call_2)
 
         expected = {
@@ -78,12 +150,12 @@ class TestCall(TestSetup):
         }
         self.assertCountEqual(serializer.data['ambulancecall_set'],
                               [ambulance_call_serializer_2.data, ambulance_call_serializer_1.data])
-        result = serializer.data
-        result['ambulancecall_set'] = []
-        self.assertDictEqual(result, expected)
+        expected = serializer.data
+        expected['ambulancecall_set'] = []
+        self.assertDictEqual(expected, expected)
 
-        with self.assertRaises(IntegrityError) as context:
-            AmbulanceCall.objects.create(call=c1, ambulance=self.a1)
+        # cannot have duplicate
+        self.assertRaises(IntegrityError, AmbulanceCall.objects.create, call=c1, ambulance=self.a1)
 
     def test_call_serializer_create(self):
 
@@ -207,7 +279,7 @@ class TestCall(TestSetup):
         }
         serializer = CallSerializer(data=call)
         serializer.is_valid()
-        self.assertRaises(utilsIntegrityError, serializer.save, updated_by=self.u1)
+        self.assertRaises(IntegrityError, serializer.save, updated_by=self.u1)
 
         # make sure no call was created
         self.assertRaises(Call.DoesNotExist, Call.objects.get, street='will fail')
@@ -334,19 +406,6 @@ class TestCall(TestSetup):
         self.assertDictEqual(serializer.data, result)
 
         # Need more tests for updates by regular authorized user
-
-    def test_patient_serializer(self):
-
-        # test PatientSerializer
-        c1 = Call.objects.create(number="123", street="dunno", updated_by=self.u1)
-        p1 = Patient.objects.create(call=c1)
-        serializer = PatientSerializer(p1)
-        result = {
-            'id': p1.id,
-            'name': p1.name,
-            'age': p1.age
-        }
-        self.assertDictEqual(serializer.data, result)
 
     def test_call_list_view_loads(self):
         client = Client()
