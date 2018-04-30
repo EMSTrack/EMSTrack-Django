2515074abc781ba5e362dbd246b77d508c259163
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-04-29 20:59:26 -0700

    Adding patient set to call serializer.

--- mqtt/tests/test_mqtt.py
+++ mqtt/tests/test_mqtt.py
@@ -812,7 +812,7 @@ class TestMQTTSubscribe(TestMQTT, MQTTTestCase):
                             qos=0)
 
         # process messages
-        self.loop(test_client, subscribe_client)
+        self.loop(test_client)
         subscribe_client.loop()
 
         # generate ERROR: JSON formated incorrectly
@@ -828,7 +828,7 @@ class TestMQTTSubscribe(TestMQTT, MQTTTestCase):
                             qos=0)
 
         # process messages
-        self.loop(test_client, subscribe_client)
+        self.loop(test_client)
         subscribe_client.loop()
 
         test_invalid_serializer = False
