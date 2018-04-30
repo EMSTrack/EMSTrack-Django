12fb24ed105850af84154b2657dfad6c9b82eaa1
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-04-29 20:04:32 -0700

    Adding patient set to call serializer.

--- ambulance/tests/test_ambulances.py
+++ ambulance/tests/test_ambulances.py
@@ -199,7 +199,7 @@ class TestAmbulanceUpdate(TestSetup):
             'status': status,
             'orientation': a.orientation,
             'location': point2str(a.location),
-            'location_client': None,
+            'location_client_id': None,
             'timestamp': date2iso(a.timestamp),
             'updated_by': user.id,
             'updated_on': date2iso(a.updated_on)
