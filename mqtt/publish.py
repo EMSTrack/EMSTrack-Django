d6a5da2ae60c628382453c1b1482f662fb7968ad
Mauricio de Oliveira <mauricio@ucsd.edu>
2018-05-01 16:59:21 -0700

    Publish calls on mqtt.

--- mqtt/publish.py
+++ mqtt/publish.py
@@ -148,7 +148,7 @@ class PublishClient(BaseClient):
     def publish_call_status(self, ambulancecall, qos=2, retain=True):
         self.publish_topic('ambulance/{}/call/{}/status'.format(ambulancecall.ambulance_id,
                                                                 ambulancecall.call_id),
-                           AmbulanceCallStatus(ambulancecall.status).value,
+                           ambulancecall.status,
                            qos=qos,
                            retain=retain)
 
