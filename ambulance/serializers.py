ef80af5d3f6260791ba4aa12b35cf52714361299
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-05-04 21:45:46 -0700

    Making sure ambulance updates are from authorized clients.

--- ambulance/serializers.py
+++ ambulance/serializers.py
@@ -79,13 +79,13 @@ class AmbulanceSerializer(serializers.ModelSerializer):
         # stored in validated_data as {'location_client': {'client_id': client_id}}
         if 'location_client' in validated_data:
 
-            client_id = validated_data.pop('location_client')['client_id']
-
+            # retrieve new location_client and remove it from validated_data
             location_client = None
+            client_id = validated_data.pop('location_client')['client_id']
             if client_id:
                 location_client = Client.objects.get(client_id=client_id)
 
-            # reset location_client if either is None or ignore
+            # reset location_client if either new or old is None, otherwise ignore
             if instance.location_client is None or location_client is None:
 
                 # fine, clear or update location client
