8bb823872d8ab70aa44af1c4ac99ea4f7e4e3694
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-11-11 17:09:52 -0800

    Add settings for priority.

--- static/js/map.js
+++ static/js/map.js
@@ -676,7 +676,7 @@ function addCallToGrid(call) {
             '<div class="form-group form-check mt-0 mb-1 status-' + status + '" id="call-item-' + call.id + '">\n' +
             '     <input type="checkbox" class="form-check-input" id="call-' + call.id + '">\n' +
             '     <label class="form-check-label" for="call-' + call.id + '">\n' +
-            '       <strong>' + date + '</strong>\n' +
+            '       <strong class="txt-' + call_priority_css[call.priority].class + '">' + date + '</strong>\n' +
             '     </label>\n' +
             '     <div id="call-item-grid-' + call.id + '">' +
             '     </div>' +
