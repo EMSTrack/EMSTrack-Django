567282bda42fae1d7082a070fdbc0e7440910bec
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-11-15 13:28:47 -0800

    Filtering ambulance updates by call.

--- ambulance/tests/test_calls.py
+++ ambulance/tests/test_calls.py
@@ -321,7 +321,7 @@ class TestCall(TestSetup):
             }
             answer1.append(serializer.data)
         logger.debug(answer1)
-        self.assertEqual(len(answer1), 3)
+        self.assertEqual(len(answer1), 4)
 
         # instantiate client
         client = Client()
