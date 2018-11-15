523194f0000ca0d07b337ca372efa84f91a6e5b7
zhl483 <zhl483@ucsd.edu>
2018-11-14 16:59:32 -0800

    testing abort function

--- mqtt/tests/test_calls.py
+++ mqtt/tests/test_calls.py
@@ -343,16 +343,16 @@ class TestMQTTCallsAbort(TestMQTT, MQTTTestCase):
         ambulancecall = call.ambulancecall_set.get(ambulance_id=ambulance_id)
         self.assertEqual(ambulancecall.status, AmbulanceCallStatus.C.name)
 
+        # expect blank ambulancecall
+        test_client.expect('ambulance/{}/call/+/status'.format(ambulance_id))
+        self.is_subscribed(test_client)
+
         # Check if call status is Ended
         call = Call.objects.get(id=call.id)
         self.assertEqual(call.status, CallStatus.E.name)
 
         # expect blank call
         test_client.expect('call/{}/data'.format(call.id))
-        self.is_subscribed(test_client)
-
-        # expect blank ambulancecall
-        test_client.expect('ambulance/{}/call/+/status'.format(ambulance_id))
         self.is_subscribed(test_client)
 
         # Client handshake
