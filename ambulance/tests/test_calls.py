1da1e8df21dc9d65a187ebf81f82adf5d7822422
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-11-25 12:05:49 -0800

    Testing waypoints and serializers.

--- ambulance/tests/test_calls.py
+++ ambulance/tests/test_calls.py
@@ -758,48 +758,49 @@ class TestCall(TestSetup):
         client = Client()
         client.login(username=settings.MQTT['USERNAME'], password=settings.MQTT['PASSWORD'])
 
+        # 'ambulancecall_set': [
+        #     {
+        #         'ambulance_id': self.a1.id,
+        #         'waypoint_set': [
+        #             {
+        #                 'order': 0,
+        #                 'type': WaypointType.IA.name,
+        #                 'waypoint_address': {
+        #                     'number': '123',
+        #                     'street': 'some street'
+        #                 }
+        #             },
+        #             {
+        #                 'order': 1,
+        #                 'type': WaypointType.WL.name,
+        #                 'visited': True,
+        #                 'waypoint_location': {
+        #                     'logitude': -110.54,
+        #                     'latitude': 35.75
+        #                 }
+        #             }
+        #         ]
+        #     },
+        #     {
+        #         'ambulance_id': self.a2.id,
+        #         'waypoint_set': [
+        #             {
+        #                 'order': 0,
+        #                 'type': WaypointType.WA.name,
+        #                 'waypoint_address': {
+        #                     'number': '321',
+        #                     'street': 'another street'
+        #                 }
+        #             }
+        #         ]
+        #     }
+        # ],
+
         response = client.post('/api/call/',
                                {
                                    'status': CallStatus.P.name,
                                    'priority': CallPriority.B.name,
                                    'ambulancecall_set': [{'ambulance_id': self.a1.id}, {'ambulance_id': self.a2.id}],
-                                   # 'ambulancecall_set': [
-                                   #     {
-                                   #         'ambulance_id': self.a1.id,
-                                   #         'waypoint_set': [
-                                   #             {
-                                   #                 'order': 0,
-                                   #                 'type': WaypointType.IA.name,
-                                   #                 'waypoint_address': {
-                                   #                     'number': '123',
-                                   #                     'street': 'some street'
-                                   #                 }
-                                   #             },
-                                   #             {
-                                   #                 'order': 1,
-                                   #                 'type': WaypointType.WL.name,
-                                   #                 'visited': True,
-                                   #                 'waypoint_location': {
-                                   #                     'logitude': -110.54,
-                                   #                     'latitude': 35.75
-                                   #                 }
-                                   #             }
-                                   #         ]
-                                   #     },
-                                   #     {
-                                   #         'ambulance_id': self.a2.id,
-                                   #         'waypoint_set': [
-                                   #             {
-                                   #                 'order': 0,
-                                   #                 'type': WaypointType.WA.name,
-                                   #                 'waypoint_address': {
-                                   #                     'number': '321',
-                                   #                     'street': 'another street'
-                                   #                 }
-                                   #             }
-                                   #         ]
-                                   #     }
-                                   # ],
                                    'patient_set': [{'name': 'Jose', 'age': 3}, {'name': 'Maria', 'age': 10}]
                                })
         self.assertEqual(response.status_code, 201)
