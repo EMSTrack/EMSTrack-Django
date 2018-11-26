e36edd82596a13c3bb054a41cb51409de917fbc5
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-11-26 09:17:09 -0800

    Reusing Location for waypoints.

--- ambulance/models.py
+++ ambulance/models.py
@@ -655,7 +655,7 @@ class Location(AddressModel,
 
     def get_absolute_url(self):
         return reverse('ambulance:location_detail', kwargs={'pk': self.id})
-e
+
     def __str__(self):
         return "{} @{} ({})".format(self.name, self.location, self.comment)
 
