62596bf90204a6cfacca5997267ec196921e0539
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-11-29 07:43:36 -0800

    Hospital derives from Location.

--- ambulance/tests/test_calls.py
+++ ambulance/tests/test_calls.py
@@ -410,7 +410,7 @@ class TestCall(TestSetup):
             'updated_by': self.h1.updated_by.id,
             'updated_on': date2iso(self.h1.updated_on)
         }
-        logger.debug(wp_3_serializer.data)
+        logger.debug(wp_3_serializer.data['location'])
         logger.debug(result)
         self.assertDictEqual(wp_3_serializer.data['location'], result)
 
