dc99b3f599a92cb1c49b9e88580e9f97c5f1f9f3
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-12-09 07:53:34 -0800

    Made hospital an equipment holder.

--- login/tests/setup_data.py
+++ login/tests/setup_data.py
@@ -10,9 +10,8 @@ from login.models import \
 from ambulance.models import Ambulance, \
     AmbulanceCapability, Location, LocationType
 
-from hospital.models import Hospital, \
-    HospitalEquipment
-from emstrack.models import EquipmentType, Equipment
+from hospital.models import Hospital
+from emstrack.models import EquipmentType, Equipment, EquipmentItem
 
 
 class TestSetupData:
@@ -108,33 +107,33 @@ class TestSetupData:
             name='MRI - Ressonance',  # name with space!
             type=EquipmentType.B.name)
 
-        # add hospital equipment
-        cls.he1 = HospitalEquipment.objects.create(
-            hospital=cls.h1,
+        # add equipment
+        cls.he1 = EquipmentItem.objects.create(
+            equipment_holder=cls.h1,
             equipment=cls.e1,
             value='True',
             updated_by=cls.u1)
 
-        cls.he2 = HospitalEquipment.objects.create(
-            hospital=cls.h1,
+        cls.he2 = EquipmentItem.objects.create(
+            equipment_holder=cls.h1,
             equipment=cls.e2,
             value='45',
             updated_by=cls.u1)
 
-        cls.he3 = HospitalEquipment.objects.create(
-            hospital=cls.h2,
+        cls.he3 = EquipmentItem.objects.create(
+            equipment_holder=cls.h2,
             equipment=cls.e1,
             value='False',
             updated_by=cls.u1)
 
-        cls.he4 = HospitalEquipment.objects.create(
-            hospital=cls.h2,
+        cls.he4 = EquipmentItem.objects.create(
+            equipment_holder=cls.h2,
             equipment=cls.e3,
             value='True',
             updated_by=cls.u1)
 
-        cls.he5 = HospitalEquipment.objects.create(
-            hospital=cls.h3,
+        cls.he5 = EquipmentItem.objects.create(
+            equipment_holder=cls.h3,
             equipment=cls.e1,
             value='True',
             updated_by=cls.u1)
