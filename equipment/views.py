1f4eeee62eb0d8055586cdc4d618367217d820ab
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-12-11 18:10:05 -0800

    Adding EquipmentHolderUpdateView.

--- equipment/views.py
+++ equipment/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-from extra_views import InlineFormSet
+from extra_views import InlineFormSet, UpdateWithInlinesView
 
-from .models import EquipmentItem, Equipment
+from emstrack.mixins import SuccessMessageWithInlinesMixin, UpdatedByWithInlinesMixin
+from .models import EquipmentItem, Equipment, EquipmentHolder
 
 
 class EquipmentItemInline(InlineFormSet):
@@ -40,6 +42,26 @@ class EquipmentAdminUpdateView(SuccessMessageMixin,
 
     def get_success_message(self, cleaned_data):
         return "Successfully updated equipment '{}'".format(self.object.name)
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+
+class EquipmentPermissionMixin(object):
+    pass
+
+
+class EquipmentHolderUpdateView(LoginRequiredMixin,
+                                SuccessMessageWithInlinesMixin,
+                                UpdatedByWithInlinesMixin,
+                                EquipmentPermissionMixin,
+                                UpdateWithInlinesView):
+    model = EquipmentHolder
+    inlines = [EquipmentItemInline]
+    form_class = EquipmentHolderUpdateForm
+
+    def get_success_message(self, cleaned_data):
+        return "Successfully updated equipments"
 
     def get_success_url(self):
         return self.object.get_absolute_url()
