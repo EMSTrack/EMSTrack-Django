110e44541dbf3ace9eb46f3d1db882f80953560c
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2018-12-13 16:44:42 -0800

    Paginating calls and clients.

--- emstrack/models.py
+++ emstrack/models.py
@@ -62,20 +62,20 @@ class AddressModel(models.Model):
         abstract = True
 
     def get_address(self):
-        address_str = ' '.join((self.number, self.street, self.unit))
+        address_str = '_'.join((self.number, self.street, self.unit))
 
         if address_str:
-            address_str = ', '.join((address_str, self.neighborhood))
+            address_str = ',*'.join((address_str, self.neighborhood))
         else:
             address_str = self.neighborhood
 
         if address_str:
-            address_str = ', '.join((address_str, self.city, self.state))
+            address_str = ',+'.join((address_str, self.city, self.state))
         else:
-            address_str = ', '.join((self.city, self.state))
+            address_str = ',-'.join((self.city, self.state))
 
-        address_str = ' '.join((address_str, self.zipcode))
-        address_str = ', '.join((address_str, self.country))
+        address_str = '@'.join((address_str, self.zipcode))
+        address_str = ',%'.join((address_str, self.country))
 
         return address_str
 
