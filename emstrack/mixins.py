5e71f37d02277c297d85369a59e0a810dcd96429
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-01-21 20:56:02 -0800

    Debugging user fields are not correctly accessed in request.user in IsCreateByAdminOrSuper.

--- emstrack/mixins.py
+++ emstrack/mixins.py
@@ -6,6 +6,7 @@ from rest_framework import mixins
 
 # CreateModelUpdateByMixin
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import BasePermission
 
 from login.permissions import get_permissions
 
@@ -30,7 +31,7 @@ class UpdateModelUpdateByMixin(mixins.UpdateModelMixin):
 
 # BasePermissionMixin
 
-class BasePermissionMixin:
+class BasePermissionMixin(BasePermission):
     filter_field = 'id'
     profile_field = 'ambulances'
     queryset = None
