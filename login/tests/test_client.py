b75dfded6c0b1928f41e3cbec9b0f1fa050c6c4a
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-02-23 06:59:46 -0800

    Removing location_client from ambulance.

--- login/tests/test_client.py
+++ login/tests/test_client.py
@@ -43,7 +43,7 @@ class TestClient(TestSetup):
         self.assertEqual(log.activity, ClientActivity.AI.name)
         self.assertEqual(log.details, self.a1.identifier)
 
-        log = ClientLog.objects.filter(client=client1).order_by('updated_on')[-2]
+        log = ClientLog.objects.filter(client=client1).order_by('-updated_on')[1]
         self.assertEqual(log.client, client1)
         self.assertEqual(log.status, ClientStatus.F.name)
         self.assertEqual(log.activity, ClientActivity.HS.name)
