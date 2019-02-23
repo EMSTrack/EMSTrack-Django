c4f027d5cd91178f85d3c0ebefbc443558acb31e
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-02-22 23:30:25 -0800

    Removing location_client from ambulance.

--- login/tests/test_client.py
+++ login/tests/test_client.py
@@ -14,15 +14,18 @@ class TestClient(TestSetup):
         # client online
         client1 = Client.objects.create(client_id='client_id_1', user=self.u1,
                                         status=ClientStatus.O.name)
+
+        self.assertEqual(client1.status, ClientStatus.O.name)
         self.assertEqual(client1.ambulance, None)
         self.assertEqual(client1.hospital, None)
 
-        # login ambulance
+        # login ambulance a1
         client1.ambulance = self.a1
         client1.save()
 
         a = Ambulance.objects.get(id=self.a1.id)
 
+        self.assertEqual(client1.status, ClientStatus.O.name)
         self.assertEqual(client1.ambulance, self.a1)
         self.assertEqual(a.client, client1)
         self.assertEqual(client1.hospital, None)
@@ -33,7 +36,29 @@ class TestClient(TestSetup):
 
         a = Ambulance.objects.get(id=self.a1.id)
 
+        self.assertEqual(client1.status, ClientStatus.O.name)
         self.assertEqual(client1.ambulance, None)
         self.assertFalse(hasattr(a, 'client'))
         self.assertEqual(client1.hospital, None)
 
+        # login ambulance a2
+        client1.ambulance = self.a2
+        client1.save()
+
+        a = Ambulance.objects.get(id=self.a2.id)
+
+        self.assertEqual(client1.status, ClientStatus.O.name)
+        self.assertEqual(client1.ambulance, self.a2)
+        self.assertEqual(a.client, client1)
+        self.assertEqual(client1.hospital, None)
+
+        # go offline
+        client1.status = ClientStatus.F.name
+        client1.save()
+
+        a = Ambulance.objects.get(id=self.a2.id)
+
+        self.assertEqual(client1.status, ClientStatus.F.name)
+        self.assertEqual(client1.ambulance, None)
+        self.assertFalse(hasattr(a, 'client'))
+        self.assertEqual(client1.hospital, None)
