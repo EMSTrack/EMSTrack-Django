44f55aef91a0557e02d8af321959716eeb482d83
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-02-22 23:33:36 -0800

    Removing location_client from ambulance.

--- login/tests/test_client.py
+++ login/tests/test_client.py
@@ -1,7 +1,7 @@
 import logging
 
 from ambulance.models import Ambulance
-from login.models import Client, ClientStatus, ClientLog
+from login.models import Client, ClientStatus, ClientLog, ClientActivity
 from login.tests.setup_data import TestSetup
 
 logger = logging.getLogger(__name__)
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 class TestClient(TestSetup):
 
-    def testSave(self):
+    def testAmbulanceLogin(self):
 
         # client online
         client1 = Client.objects.create(client_id='client_id_1', user=self.u1,
@@ -18,6 +18,12 @@ class TestClient(TestSetup):
         self.assertEqual(client1.status, ClientStatus.O.name)
         self.assertEqual(client1.ambulance, None)
         self.assertEqual(client1.hospital, None)
+
+        log = ClientLog.objects.filter(id=client1.id).latest('updated_on')
+        self.assertEqual(log.client, client1)
+        self.assertEqual(log.status, ClientStatus.O.name)
+        self.assertEqual(log.activity, ClientActivity.HS.name)
+        self.assertEqual(log.details, None)
 
         # login ambulance a1
         client1.ambulance = self.a1
