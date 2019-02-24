4c851c75d356597a5fd49a5bbef128b7e56d7367
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-02-23 17:01:02 -0800

    Removing location_client from ambulance.

--- login/models.py
+++ login/models.py
@@ -210,8 +210,8 @@ class Client(models.Model):
     _loaded_values = None
 
     def __str__(self):
-        return self.client_id + '[' + self.status + ', user = ' + self.user + ']' + \
-               '(ambulance=' + self.ambulance + ', hospital=' + self.hospital + ')'
+        return '{}[{},{}](ambulance={},hospital={}'.format(self.client_id, self.status,
+                                                           self.user, self.ambulance, self.hospital)
 
     def get_absolute_url(self):
         return reverse('login:detail-client', kwargs={'pk': self.id})
