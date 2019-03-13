1f781ed6df5decd0fcf992b927964ef0aedce996
Mauricio C de Oliveira <mcdeoliveira@earthlink.net>
2019-03-13 08:01:16 +0000

    node packages

---
+++ webpack-ambulance-config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+  context: __dirname,
+  entry: './static/js/ambulance',
+  mode: 'development',
+  output: {
+    path: path.resolve('./static/bundles/ambulance'),
+    filename: "[name]-[hash].js"
+  },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-ambulance-stats.json'})
+  ],
+  module: {
+    rules: [
+      {
+         test: /\.css$/,
+         use: ['style-loader', 'css-loader'],
+      },
+      {
+         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+         loader: 'url-loader',
+         options: {
+           limit: 10000,
+         },
+      },
+    ],
+  },
+}
