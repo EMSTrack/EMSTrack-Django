c5522f05e9dc91d6e3eccafaa33b06b487119bd1
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-13 00:36:27 -0700

    Bundling ambulance detail frontend.

--- static/js/leaflet/LeafletWidget.js
+++ static/js/leaflet/LeafletWidget.js
@@ -18,9 +18,9 @@ L.Marker.prototype.options.icon = DefaultIcon;
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'leaflet/dist/images/marker-icon-2x.png',
-  iconUrl: 'leaflet/dist/images/marker-icon.png',
-  shadowUrl: 'leaflet/dist/images/marker-shadow.png',
+  iconRetinaUrl: '/static/leaflet/dist/images/marker-icon-2x.png',
+  iconUrl: '/static/leaflet/dist/images/marker-icon.png',
+  shadowUrl: '/static/leaflet/dist/images/marker-shadow.png',
 });
 
 // LeafletWidget
