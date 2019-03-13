4637fbe0fa5a8dfbb1cd3ee6338a98923808f39e
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-12 23:40:05 -0700

    Bundling ambulance detail frontend.

--- static/js/ambulance.js
+++ static/js/ambulance.js
@@ -1,17 +1,17 @@
-import $ from 'jquery'
+import $ from "jquery";
 
-import L from 'leaflet'
-import 'leaflet-rotatedmarker'
+import L from "leaflet";
+import "leaflet-rotatedmarker";
 
 import { LeafletPolylineWidget } from "./leaflet/LeafletWidget";
 
-var map;
-var page;
+let map;
+let page;
 
 $(function() {
 
  	// Set up map widget
- 	options = {
+ 	let options = {
  		map_id: "map",
  		zoom: 12,
         access_token: access_token
@@ -19,8 +19,8 @@ $(function() {
  	map = new LeafletPolylineWidget(options);
 
  	// get page and page_size parameters
-	var searchParams = new URLSearchParams(window.location.search)
-	if (searchParams.has('page'))
+    const searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has('page'))
         page = searchParams.get('page');
 	if (searchParams.has('page_size'))
         page_size = searchParams.get('page_size');
@@ -33,8 +33,8 @@ $(function() {
 function retrieveAmbulances(ambulance_id) {
 
 	// Build url
-	var url = APIBaseUrl + 'ambulance/' + ambulance_id + '/updates';
-	if (page != null) {
+    let url = APIBaseUrl + 'ambulance/' + ambulance_id + '/updates';
+    if (page != null) {
         url += "?page=" + page;
         if (page_size != null)
             url += "&page_size=" + page_size;
@@ -61,7 +61,7 @@ function retrieveAmbulances(ambulance_id) {
         }
 
     })
-        .done(function (data) {
+        .done(function () {
             if (console && console.log) {
                 console.log("Done retrieving ambulance data from API");
             }
@@ -76,10 +76,10 @@ function addMarker(map, update, layer) {
 		.bindPopup('<strong>' + ambulance_status[update.status] + '</strong><br/>@'
             + (new Date(Date.parse(update.timestamp))).toLocaleString())
 		.on('mouseover',
-			function(e){
+			function(){
 				// open popup bubble
 				this.openPopup().on('mouseout',
-					function(e){
+					function(){
 						this.closePopup();
 					});
 			});
@@ -91,13 +91,13 @@ function calculateDistanceHaversine(location1, location2, radius) {
 	radius = radius || 6371e3;
 
 	// convert latitude and longitude to radians first
-    var lat1 = Math.PI * location1.latitude / 180;
-    var lat2 = Math.PI * location2.latitude / 180;
-    var d_phi = lat2 - lat1;
-    var d_lambda = Math.PI * (location2.longitude - location1.longitude) / 180;
+    const lat1 = Math.PI * location1.latitude / 180;
+    const lat2 = Math.PI * location2.latitude / 180;
+    const d_phi = lat2 - lat1;
+    const d_lambda = Math.PI * (location2.longitude - location1.longitude) / 180;
 
-    var a = Math.sin(d_phi / 2) * Math.sin(d_phi / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(d_lambda / 2) * Math.sin(d_lambda / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const a = Math.sin(d_phi / 2) * Math.sin(d_phi / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(d_lambda / 2) * Math.sin(d_lambda / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     // console.log('|| {} - {} ||= {}'.format(location2, location1, earth_radius * c))
 
@@ -109,21 +109,21 @@ function breakSegments(data, separationRadius, timeInterval) {
 	separationRadius = separationRadius || [100, 10000]; // 10m, 10km
 	timeInterval = timeInterval || [2 * 60 * 1000, 60 * 60 * 1000]; // 2 minutes, 1 hour
 
-	var segments = [];
+    const segments = [];
 
-	var currentSegment = [];
-	var lastPosition = null;
-	var n = data.length;
-	for (var i = 0; i < n; i++) {
+    let currentSegment = [];
+    let lastPosition = null;
+    const n = data.length;
+    for (let i = 0; i < n; i++) {
 
 		// current position
-		var currentPosition = data[i];
+        const currentPosition = data[i];
 
-		// distance?
+        // distance?
 		if (lastPosition != null) {
-			var distance = calculateDistanceHaversine(lastPosition.location, currentPosition.location);
-			var interval = Math.abs(Date.parse(lastPosition.timestamp) - Date.parse(currentPosition.timestamp));
-			if (distance > separationRadius[1] || interval > timeInterval[1] ||
+            const distance = calculateDistanceHaversine(lastPosition.location, currentPosition.location);
+            const interval = Math.abs(Date.parse(lastPosition.timestamp) - Date.parse(currentPosition.timestamp));
+            if (distance > separationRadius[1] || interval > timeInterval[1] ||
                 (interval > timeInterval[0] && distance > separationRadius[0])) {
                 // terminate current segment
                 segments.push(currentSegment);
@@ -157,27 +157,28 @@ function addSegment(updates, layer) {
     map.createLayer(layer);
 
 	// First entry
-	var lastStatus;
-	if (updates.length >= 2) {
+    let lastStatus;
+    let entry;
+    if (updates.length >= 2) {
 
-		// Retrieve last entry (first position)
-		entry = updates[updates.length - 1];
+        // Retrieve last entry (first position)
+        entry = updates[updates.length - 1];
 
-		// add marker
-		addMarker(map, entry, layer);
+        // add marker
+        addMarker(map, entry, layer);
 
-		// entry status
-		lastStatus = entry.status;
+        // entry status
+        lastStatus = entry.status;
 
-	}
+    }
 
 	// Mid updates
-	for (var i = updates.length - 2; i > 0; i--) {
+	for (let i = updates.length - 2; i > 0; i--) {
 
 		// Retrieve next entry
 		entry = updates[i];
 
-		if (entry.status != lastStatus) {
+		if (entry.status !== lastStatus) {
 
             // add marker
             addMarker(map, entry, layer);
@@ -201,12 +202,12 @@ function addSegment(updates, layer) {
 	}
 
 	// Store data in an array
-	var latlngs = [];
-	updates.forEach(function(update) {
+    const latlngs = [];
+    updates.forEach(function(update) {
 
 		// push location
-		var loc = update.location;
-		latlngs.push([loc.latitude, loc.longitude]);
+        const loc = update.location;
+        latlngs.push([loc.latitude, loc.longitude]);
 
     });
 
@@ -220,11 +221,11 @@ function addSegment(updates, layer) {
 function addAmbulanceRoute(data) {
 
     // short return
-    if (data.results.length == 0)
+    if (data.results.length === 0)
         return;
 
     // break segments
-    var segments = breakSegments(data.results);
+    const segments = breakSegments(data.results);
 
     // loop on segments
     segments.forEach( function(segment, index) {
@@ -245,15 +246,15 @@ function addAmbulanceRoute(data) {
 function createRouteFilter(segments) {
 
     // Add the checkbox on the top right corner for filtering.
-    var container = L.DomUtil.create('div', 'filter-options bg-light');
+    const container = L.DomUtil.create('div', 'filter-options bg-light');
 
     //Generate HTML code for checkboxes for each of the statuses.
-    var filterHtml = "";
+    let filterHtml = "";
 
     filterHtml += '<div class="border border-dark rounded px-1 pt-1 pb-0">';
     segments.forEach(function (segment, index) {
 
-        var date = new Date(Date.parse(segment[0].timestamp));
+        const date = new Date(Date.parse(segment[0].timestamp));
         filterHtml += '<div class="checkbox">'
             + '<label><input class="chk" data-status="layer_' + index + '" type="checkbox" value="" checked >'
             + date.toLocaleString()
@@ -267,7 +268,7 @@ function createRouteFilter(segments) {
     container.innerHTML = filterHtml;
 
     // Add the checkboxes.
-    var customControl = L.Control.extend({
+    const customControl = L.Control.extend({
 
         options: {
             position: 'topright'
@@ -284,7 +285,7 @@ function createRouteFilter(segments) {
     $('.chk').change(function () {
 
         // Which layer?
-        var layer = map.getLayerPane(this.getAttribute('data-status'));
+        const layer = map.getLayerPane(this.getAttribute('data-status'));
 
         if (this.checked) {
             layer.style.display = 'block';
