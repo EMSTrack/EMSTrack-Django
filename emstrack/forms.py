21e88457d71ca293550bd56511699e5a34d1c25d
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-13 07:59:49 -0700

    Bundling point widget.

--- emstrack/forms.py
+++ emstrack/forms.py
@@ -6,18 +6,6 @@ from django.contrib.gis.forms import widgets
 class LeafletPointWidget(widgets.BaseGeometryWidget):
     template_name = 'leaflet/point_widget.html'
 
-    # class Media:
-    #     css = {
-    #         'all': ('https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-    #                 # 'leaflet/css/location_form.css',
-    #                 # 'leaflet/css/LeafletWidget.css'
-    #                 )
-    #     }
-    #     js = (
-    #         'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-    #         'leaflet/js/LeafletWidget.js'
-    #     )
-
     def render(self, name, value, attrs=None, renderer=None):
         # add point
         if value:
@@ -32,8 +20,5 @@ class LeafletPointWidget(widgets.BaseGeometryWidget):
         attrs.update({
             'access_token': settings.ACCESS_TOKEN
         })
-
-        # use TemplatesSetting as default rendering
-        # Otherwise we get in trouble with finding point_widget.html
 
         return super().render(name, value, attrs, TemplatesSetting())
