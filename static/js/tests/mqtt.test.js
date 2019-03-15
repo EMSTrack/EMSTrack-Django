74a8683eac90bb363557dc91e898ce3768b60638
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-14 21:13:06 -0700

    More javascript and tests.

---
+++ static/js/tests/mqtt.test.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+
+import { MqttClient } from "../mqtt";
+
+describe('mqtt', () => {
+
+    const mqttClient = new MqttClient('localhost', '8884', 'test-client', 1);
+
+    context('connect', function() {
+        it('should subscribe', function(done) {
+            mqttClient.connect({
+                onSuccess: () => { done(); },
+                onFailure: (cntxt, errorCode, errorMessage) => { done(errorMessage); }
+            });
+        })
+    })
+
+});
--- static/js/tests/observer.test.js
+++ static/js/tests/observer.test.js
@@ -2,38 +2,34 @@ const expect = require('chai').expect;
 
 import { Observer } from "../observer";
 
-describe('observer subscribe', () => {
+describe('observer', () => {
 
     const observer = new Observer();
     const fn1 = () => {};
 
-    context('subscribe',
-        () => { it('should subscribe',
-            () => {
-                observer.subscribe(fn1);
-                expect(observer.observers.length).to.equal(1)
-            })
+    context('subscribe', function() {
+        it('should subscribe', function() {
+            observer.subscribe(fn1);
+            expect(observer.observers.length).to.equal(1)
         })
+    })
 
-    context('unsubscribe',
-        () => { it('should unsubscribe',
-                () => {
-                    observer.unsubscribe(fn1);
-                    expect(observer.observers.length).to.equal(0);
-                })
-        });
+    context('unsubscribe', function() {
+        it('should unsubscribe', function() {
+            observer.unsubscribe(fn1);
+            expect(observer.observers.length).to.equal(0);
+        })
+    });
 
     let subscriberHasBeenCalled = false;
     const fn2 = (data) => subscriberHasBeenCalled = data;
 
-    context('broadcast',
-        () => {
-            it('should receive',
-                () => {
-                    observer.subscribe(fn2);
-                    observer.broadcast(true);
-                    expect(subscriberHasBeenCalled).to.equal(true);
-                })
-        });
+    context('broadcast', function() {
+        it('should receive', function () {
+            observer.subscribe(fn2);
+            observer.broadcast(true);
+            expect(subscriberHasBeenCalled).to.equal(true);
+        })
+    });
 
 });
