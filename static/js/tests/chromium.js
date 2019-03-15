0e7d35c3ecc2dabbbffe93bac29989e1fd105baa
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-14 21:57:46 -0700

    More javascript and tests.

--- static/js/tests/chromium.js
+++ static/js/tests/chromium.js
@@ -1,14 +1,13 @@
 const {runner} = require('mocha-headless-chrome');
 
 const options = {
-    file: 'test.html',                           // test page path
+    file: 'mqtt.test.html',                      // test page path
     reporter: 'dot',                             // mocha reporter name
     width: 800,                                  // viewport width
     height: 600,                                 // viewport height
     timeout: 120000,                             // timeout in ms
-    executablePath: '/usr/bin/chrome-unstable',  // chrome executable path
     visible: true,                               // show chrome window
-    args: ['no-sandbox']                         // chrome arguments
+    args: ['--no-sandbox']                       // chrome arguments
 };
 
 runner(options)
