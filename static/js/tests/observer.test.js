cea76c95ec8d52ce187b0cd75135e9191e7274fa
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-14 18:01:41 -0700

    More javascript and tests.

--- static/js/tests/observer.test.js
+++ static/js/tests/observer.test.js
@@ -1,8 +1,8 @@
-const observer = require('../observer');
+import { Observer} from "../observer";
 
 test('observer subscribe', () => {
 
-    const observer = new observer.Observer();
+    const observer = new Observer();
     const fn = () => {};
     observer.subscribe(fn);
     expect(observer.observers.length).toBe(1);
