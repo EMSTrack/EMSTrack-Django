5f0fad46c3124f77982c5e99d9546e7189d4289e
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-14 21:16:11 -0700

    More javascript and tests.

--- static/js/tests/mqtt.test.js
+++ static/js/tests/mqtt.test.js
@@ -4,11 +4,16 @@ import { MqttClient } from "../mqtt";
 
 describe('mqtt', () => {
 
-    const mqttClient = new MqttClient('localhost', '8884', 'test-client', 1);
+    const mqttClient = new MqttClient('localhost', 8884, 'test-client', 1);
+
+    const userName = 'admin';
+    const password = 'cruzrojaadmin';
 
     context('connect', function() {
         it('should subscribe', function(done) {
             mqttClient.connect({
+                userName: userName,
+                password: password,
                 onSuccess: () => { done(); },
                 onFailure: (cntxt, errorCode, errorMessage) => { done(errorMessage); }
             });
