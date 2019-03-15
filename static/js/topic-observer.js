25682e05d66ddb54cd07e67f529bde12fb62edbe
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-15 12:45:18 -0700

    Put back changes in observer after unsuccessful commit.

--- static/js/topic-observer.js
+++ static/js/topic-observer.js
@@ -7,10 +7,37 @@ export class TopicObserver {
         this.observers = { __all__: [] };
     }
 
+    static topicToRegex(topic) {
+
+        // Parse topic
+        let regex = topic;
+        if (regex.indexOf('+') >= 0) {
+            regex = regex.replace(/\+/g, "[^/]+");
+        }
+        if (regex.indexOf('#') === regex.length() - 1) {
+            regex = regex.replace("#", "[a-zA-Z0-9_/ ]+");
+        }
+        // Invalid topic
+        if (regex.indexOf('#') >= 0) {
+            throw new Error("Invalid topic '" + topic + "'");
+        }
+
+        // compile regex?
+        if (topic !== regex)
+            return new RegExp(regex);
+        else
+            return topic;
+    }
+
     observe(fn, topic) {
 
         // default is all
         topic = topic || '__all__';
+
+        // compile topic as regex if needed
+        if (topic !== '__all__') {
+            topic = TopicObserver.topicToRegex(topic);
+        }
 
         // create topic if needed
         if (!this.observers.hasOwnProperty(topic))
@@ -24,11 +51,20 @@ export class TopicObserver {
         // default is all
         topic = topic || '__all__';
 
+        // compile topic as regex if needed
+        if (topic !== '__all__') {
+            topic = TopicObserver.topicToRegex(topic);
+        }
+
         this.observers[topic] = this.observers[topic].filter((subscriber) => subscriber !== fn);
     }
 
     hasTopic(topic) {
         return this.observers.hasOwnProperty(topic);
+    }
+
+    getTopics(pattern) {
+        const keys = this.observers.keys();
     }
 
     broadcast(data, topic) {
