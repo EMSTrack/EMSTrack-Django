166f92019f4a1ab0cecdf46951ce3fec380128d0
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-15 08:01:52 -0700

    More javascript and tests.

--- static/js/tests/mqtt.test.js
+++ static/js/tests/mqtt.test.js
@@ -8,12 +8,6 @@ describe('mqtt', () => {
 
     console.log(settings);
 
-    context('subscribe', function() {
-        it('should subscribe', function() {
-            expect(1).to.equal(1)
-        })
-    })
-
     const mqttClient = new MqttClient('localhost', 8884, 'test-client', 1);
 
     const userName = 'admin';
