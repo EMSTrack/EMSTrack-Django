38ab27b232ef96306a1d4e7d7e4000fd28342f43
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-15 17:56:14 -0700

    Put back changes in observer after unsuccessful commit.

--- static/js/topic-observer.js
+++ static/js/topic-observer.js
@@ -1,3 +1,85 @@
+/*
+ * Auxiliary functions
+ */
+
+/**
+ * Convert topic to regex
+ *
+ * @param topic
+ * @returns {*}
+ */
+export function topicToRegex(topic) {
+
+    // Parse topic
+    let regex = topic;
+    if (regex.indexOf('+') >= 0) {
+        regex = regex.replace(/\+/g, "[^/]+");
+    }
+    if (regex.indexOf('#') === regex.length - 1) {
+        regex = regex.replace("#", "[a-zA-Z0-9_/ ]+");
+    }
+    // Invalid topic
+    if (regex.indexOf('#') >= 0) {
+        throw new Error("Invalid topic '" + topic + "'");
+    }
+
+    // compile regex?
+    if (topic !== regex)
+        return new RegExp(regex);
+    else
+        return topic;
+
+}
+
+/**
+ * Matches topic to all possible entries in array of regexps.
+ *
+ * @param array the array of regexps
+ * @param topic the topic to be matched
+ * @returns {Array}
+ */
+export function matchAllTopics(array, topic) {
+
+    const topics = [];
+    for(let i = 0; i < array.length; i++) {
+        const regex = array[i];
+        let match = false;
+        if (regex instanceof RegExp) {
+            match = regex.exec(topic)
+        } else {
+            match = regex === topic;
+        }
+        if (match)
+            topics.add(regex);
+    }
+    return topics;
+
+}
+
+/**
+ * Matches topic to first possible entry in array of regexps.
+ *
+ * @param array the array of regexps
+ * @param topic the topic to be matched
+ * @returns {*}
+ */
+export function matchFirstTopic(array, topic) {
+
+    for(let i = 0; i < array.length; i++) {
+        const regex = array[i];
+        let match = false;
+        if (regex instanceof RegExp) {
+            match = regex.exec(topic)
+        } else {
+            match = regex === topic;
+        }
+        if (match)
+            return regex;
+    }
+    return null;
+
+}
+
 /**
  * Topic observer pattern base class
  */
@@ -5,65 +87,11 @@ export class TopicObserver {
 
     constructor() {
         this.observers = { __all__: [] };
+        this.keys = Object.keys(this.observers);
     }
 
-    static topicToRegex(topic) {
-
-        // Parse topic
-        let regex = topic;
-        if (regex.indexOf('+') >= 0) {
-            regex = regex.replace(/\+/g, "[^/]+");
-        }
-        if (regex.indexOf('#') === regex.length - 1) {
-            regex = regex.replace("#", "[a-zA-Z0-9_/ ]+");
-        }
-        // Invalid topic
-        if (regex.indexOf('#') >= 0) {
-            throw new Error("Invalid topic '" + topic + "'");
-        }
-
-        // compile regex?
-        if (topic !== regex)
-            return new RegExp(regex);
-        else
-            return topic;
-    }
-
-    matchAllTopics(topic) {
-        const topics = [];
-        const array = Object.keys(this.observers);
-        for(let i = 0; i < array.length; i++) {
-            const regex = array[i];
-            let match = false;
-            if (regex instanceof RegExp) {
-                match = regex.exec(topic)
-            } else {
-                match = regex === topic;
-            }
-            if (match)
-                topics.add(regex);
-        }
-        return topics;
-    }
-
-    matchTopic(topic) {
-        const array = Object.keys(this.observers);
-        for(let i = 0; i < array.length; i++) {
-            const regex = array[i];
-            let match = false;
-            if (regex instanceof RegExp) {
-                match = regex.exec(topic)
-            } else {
-                match = regex === topic;
-            }
-            if (match)
-                return regex;
-        }
-        return null;
-    }
-
-    hasTopic(topic) {
-        return this.matchTopic(TopicObserver.topicToRegex(topic)) !== null;
+     hasTopic(topic) {
+        return matchFirstTopic(this.keys, topic) !== null;
     }
 
     observe(fn, topic) {
@@ -73,12 +101,14 @@ export class TopicObserver {
 
         // compile topic as regex if needed
         if (topic !== '__all__') {
-            topic = TopicObserver.topicToRegex(topic);
+            topic = topicToRegex(topic);
         }
 
         // create topic if needed
-        if (!this.observers.hasOwnProperty(topic))
+        if (!this.observers.hasOwnProperty(topic)) {
             this.observers[topic] = [];
+            this.keys = Object.keys(this.observers);
+        }
 
         this.observers[topic].push(fn);
     }
@@ -90,7 +120,7 @@ export class TopicObserver {
 
         // compile topic as regex if needed
         if (topic !== '__all__') {
-            topic = TopicObserver.topicToRegex(topic);
+            topic = topicToRegex(topic);
         }
 
         this.observers[topic] = this.observers[topic].filter((subscriber) => subscriber !== fn);
@@ -102,8 +132,18 @@ export class TopicObserver {
         topic = topic || '__all__';
 
         // is that a particular topic?
-        if (topic !== '__all__')
-            this.observers[topic].forEach((subscriber) => subscriber(data));
+        if (topic !== '__all__') {
+
+            // match topics and broadcast
+            const topics = matchAllTopics(this.keys, topic);
+            if (topics.length > 0)
+                topics.forEach(
+                    (topic) => this.observers[topic].forEach((subscriber) => subscriber(data))
+                );
+            else
+                console.warn('No topics matched!');
+
+        }
 
         // broadcast for all
         this.observers['__all__'].forEach((subscriber) => subscriber(data));
