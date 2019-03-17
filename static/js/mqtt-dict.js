51b9aa244d257c066310ca03f1281578fc85f82a
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-16 18:16:02 -0700

    TopicObserver pattern and testing.

---
+++ static/js/mqtt-dict.js
@@ -0,0 +1,106 @@
+
+export class MqttDict {
+
+    constructor() {
+
+        this.dict = {};
+        this.keys = [];
+
+    }
+
+    static matchStringOrRegExp(regex, topic) {
+        if (regex instanceof RegExp) {
+            return regex.test(topic);
+        } else {
+            return topic === regex;
+        }
+    }
+
+    static topicToRegex(topic) {
+
+        // deep copy
+        let regex = (' ' + topic).slice(1);
+
+        // Parse topic
+        if (regex.indexOf('+') >= 0) {
+            regex = regex.replace(/\+/g, "[^/]+");
+        }
+        if (regex.indexOf('#') === regex.length - 1) {
+            regex = regex.replace("#", "[a-zA-Z0-9_/ ]+");
+        }
+        // Invalid topic
+        if (regex.indexOf('#') >= 0) {
+            throw new Error("Invalid topic '" + topic + "'");
+        }
+
+        // compile regex?
+        if (topic !== regex)
+            return new RegExp('^' + regex + '$');
+        else
+            return topic;
+
+    }
+
+    push(topic, obj) {
+
+        const key = MqttDict.topicToRegex(topic);
+
+        // create topic if needed
+        if (!this.dict.hasOwnProperty(topic)) {
+            this.dict[key] = {key: key, array: []};
+            this.keys = Object.keys(this.dict);
+        }
+
+        this.dict[key].array.push(obj);
+
+    }
+
+    remove(topic, obj) {
+
+        const key = MqttDict.topicToRegex(topic);
+
+        this.dict[key].array = this.dict[key].array.filter((subscriber) => subscriber !== obj);
+
+    }
+
+    matchAll(topic) {
+
+        const topics = [];
+        for (let i = 0; i < this.keys.length; i++) {
+            const key = this.keys[i];
+            const regexp = this.dict[key].key;
+            if (MqttDict.matchStringOrRegExp(regexp, topic))
+                topics.push(key);
+        }
+
+        return topics;
+
+    }
+
+    matchFirst(topic) {
+
+        for (let i = 0; i < this.keys.length; i++) {
+            const key = this.keys[i];
+            const regexp = this.dict[key].key;
+            if (MqttDict.matchStringOrRegExp(regexp, topic))
+                return key;
+        }
+
+        return null;
+
+    }
+
+    get(topic) {
+
+        const arrays = [];
+        const keys = this.matchAll(topic);
+        if (keys.length > 0)
+            keys.forEach(
+                (topic) => arrays.push(this.dict[topic].array)
+            );
+        else
+            throw new Error("Unknown topic '" + topic + "'");
+
+    }
+
+}
---
+++ static/js/tests/mqtt-dict.test.js
@@ -0,0 +1,29 @@
+const expect = require('chai').expect;
+
+import { MqttDict } from "../mqtt-dict";
+
+describe('mqtt-dict strings', () => {
+
+    const dict = MqttDict();
+
+    it('has topic', function () {
+
+        const obj1 = "object1";
+        const key1 = 'string1';
+        dict.push(key1, obj1);
+        expect(Object.keys(dict.dict).length).to.equal(1);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(1);
+
+        const obj2 = "object2";
+        dict.push(key1, obj2);
+        expect(Object.keys(dict.dict).length).to.equal(1);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(2);
+
+        const key2 = 'string2';
+        dict.push(key2, obj1);
+        expect(Object.keys(dict.dict).length).to.equal(2);
+        expect(Object.keys(dict.dict[key2].array).length).to.equal(1);
+
+    });
+
+});
