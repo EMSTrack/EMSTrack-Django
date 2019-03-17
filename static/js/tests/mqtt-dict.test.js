a28bed35c76344982b2ab0e7963792783c2e3efc
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-16 18:19:28 -0700

    TopicObserver pattern and testing.

--- static/js/tests/mqtt-dict.test.js
+++ static/js/tests/mqtt-dict.test.js
@@ -6,12 +6,13 @@ describe('mqtt-dict strings', () => {
 
     const dict = new MqttDict();
 
-    it('has topic', function () {
+    it('push and remove', function () {
 
         const obj1 = "object1";
         const key1 = 'string1';
         dict.push(key1, obj1);
         expect(Object.keys(dict.dict).length).to.equal(1);
+        expect(dict.dict[key1].key).to.equal(key1);
         expect(Object.keys(dict.dict[key1].array).length).to.equal(1);
 
         const obj2 = "object2";
@@ -22,7 +23,19 @@ describe('mqtt-dict strings', () => {
         const key2 = 'string2';
         dict.push(key2, obj1);
         expect(Object.keys(dict.dict).length).to.equal(2);
+        expect(dict.dict[key2].key).to.equal(key2);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(2);
         expect(Object.keys(dict.dict[key2].array).length).to.equal(1);
+
+        dict.remove(key1, obj2);
+        expect(Object.keys(dict.dict).length).to.equal(2);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(1);
+        expect(Object.keys(dict.dict[key2].array).length).to.equal(1);
+
+        dict.remove(key2, obj1);
+        expect(Object.keys(dict.dict).length).to.equal(2);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(1);
+        expect(Object.keys(dict.dict[key2].array).length).to.equal(0);
 
     });
 
