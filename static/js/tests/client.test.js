4dd83e462d887c80ef4aa87228f2bb3fb81ef996
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-17 18:10:44 -0700

    Javascript Client testing.

--- static/js/tests/client.test.js
+++ static/js/tests/client.test.js
@@ -49,6 +49,7 @@ describe('client connection', () => {
 
         httpClient.get('user/' + userName + '/password/')
             .then( (response) => {
+                console.log(response);
                 mqttPassword = response.data.password;
                 expect(mqttPassword !== null).to.equal(true);
                 done();
