2668f3072e7d3511b50f8114695e9205b4179b4a
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-19 09:11:22 -0700

    Adapting map.js to base.js.

--- static/js/tests/mqtt-dict.test.js
+++ static/js/tests/mqtt-dict.test.js
@@ -187,13 +187,13 @@ describe('mqtt-dict topics', () => {
 
     });
 
-    it('push and remove pattern', function () {
+    it('push and remove pattern 2', function () {
 
         const dict = new MqttDict();
 
         const obj1 = "object1";
         const pattern1 = 'topic/+/call/+/data';
-        const regexp1 = new RegExp('^topic/[^/]+/call/[^/]/data$');
+        const regexp1 = new RegExp('^topic/[^/]+/call/[^/]+/data$');
         const key1 = regexp1.toString();
 
         dict.push(pattern1, obj1);
@@ -238,5 +238,110 @@ describe('mqtt-dict topics', () => {
         expect(objects[key2]).to.eql([]);
 
     });
+
+    it('push and remove pattern 3', function () {
+
+        const dict = new MqttDict();
+
+        const obj1 = "object1";
+        const pattern1 = 'topic/#';
+        const regexp1 = new RegExp('^topic/[a-zA-Z0-9_/ ]+$');
+        const key1 = regexp1.toString();
+
+        dict.push(pattern1, obj1);
+        expect(Object.keys(dict.dict).length).to.equal(1);
+        expect(Object.keys(dict.dict)).to.eql([key1]);
+
+        expect(dict.dict[key1].regexp).to.eql(regexp1);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(1);
+
+        const obj2 = "object2";
+        dict.push(pattern1, obj2);
+        expect(Object.keys(dict.dict).length).to.equal(1);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(2);
+
+        const key2 = 'topic/2/call/3/data';
+        dict.push(key2, obj1);
+        expect(Object.keys(dict.dict).length).to.equal(2);
+        expect(dict.dict[key2].regexp).to.equal(key2);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(2);
+        expect(Object.keys(dict.dict[key2].array).length).to.equal(1);
+
+        dict.remove(pattern1, obj2);
+        expect(Object.keys(dict.dict).length).to.equal(2);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(1);
+        expect(Object.keys(dict.dict[key2].array).length).to.equal(1);
+
+        dict.remove(key2, obj1);
+        expect(Object.keys(dict.dict).length).to.equal(2);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(1);
+        expect(Object.keys(dict.dict[key2].array).length).to.equal(0);
+
+        let objects = dict.get(pattern1);
+        expect(Object.keys(objects).length).to.equal(1);
+        expect(objects[key1].length).to.equal(1);
+        expect(objects[key1]).to.eql([obj1]);
+
+        objects = dict.get(key2);
+        expect(Object.keys(objects).length).to.equal(2);
+        expect(objects[key1].length).to.equal(1);
+        expect(objects[key1]).to.eql([obj1]);
+        expect(objects[key2].length).to.equal(0);
+        expect(objects[key2]).to.eql([]);
+
+    });
+
+    it('push and remove pattern 3', function () {
+
+        const dict = new MqttDict();
+
+        const obj1 = "object1";
+        const pattern1 = 'topic/+/call/#';
+        const regexp1 = new RegExp('^topic/[^/]+/call/[a-zA-Z0-9_/ ]+$');
+        const key1 = regexp1.toString();
+
+        dict.push(pattern1, obj1);
+        expect(Object.keys(dict.dict).length).to.equal(1);
+        expect(Object.keys(dict.dict)).to.eql([key1]);
+
+        expect(dict.dict[key1].regexp).to.eql(regexp1);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(1);
+
+        const obj2 = "object2";
+        dict.push(pattern1, obj2);
+        expect(Object.keys(dict.dict).length).to.equal(1);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(2);
+
+        const key2 = 'topic/2/call/3/data';
+        dict.push(key2, obj1);
+        expect(Object.keys(dict.dict).length).to.equal(2);
+        expect(dict.dict[key2].regexp).to.equal(key2);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(2);
+        expect(Object.keys(dict.dict[key2].array).length).to.equal(1);
+
+        dict.remove(pattern1, obj2);
+        expect(Object.keys(dict.dict).length).to.equal(2);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(1);
+        expect(Object.keys(dict.dict[key2].array).length).to.equal(1);
+
+        dict.remove(key2, obj1);
+        expect(Object.keys(dict.dict).length).to.equal(2);
+        expect(Object.keys(dict.dict[key1].array).length).to.equal(1);
+        expect(Object.keys(dict.dict[key2].array).length).to.equal(0);
+
+        let objects = dict.get(pattern1);
+        expect(Object.keys(objects).length).to.equal(1);
+        expect(objects[key1].length).to.equal(1);
+        expect(objects[key1]).to.eql([obj1]);
+
+        objects = dict.get(key2);
+        expect(Object.keys(objects).length).to.equal(2);
+        expect(objects[key1].length).to.equal(1);
+        expect(objects[key1]).to.eql([obj1]);
+        expect(objects[key2].length).to.equal(0);
+        expect(objects[key2]).to.eql([]);
+
+    });
+
 
 });
