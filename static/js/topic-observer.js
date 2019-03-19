57c0a1fcbc52fa20303379ffcf6397953b1e136d
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-18 21:33:50 -0700

    Adapting map.js to base.js.

--- static/js/topic-observer.js
+++ static/js/topic-observer.js
@@ -19,15 +19,20 @@ export class TopicObserver {
 
     broadcast(topic, data) {
 
-         // match topics and broadcast
-         const objects = this.observers.get(topic);
-         if (Object.keys(objects).length > 0)
-             Object.values(objects).forEach(
-                 (value) => value.forEach((subscriber) => subscriber(data))
-             );
-         else
-             console.warn('No topics matched!');
+        // match topics and broadcast
+        const objects = this.observers.get(topic);
+        if (Object.keys(objects).length > 0)
+            Object.values(objects).forEach( (value) => {
+                try {
+                    value.forEach((subscriber) => subscriber(data))
+                } catch (error) {
+                    console.log('Broadcast error:');
+                    console.log(error);
+                }
+            })
+        else
+            console.warn('No topics matched!');
 
     }
-
+    
 }
