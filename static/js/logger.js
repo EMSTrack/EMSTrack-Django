900f2a50327fff6543003e92eb765044427407ca
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-19 11:42:41 -0700

    Adapting map.js to base.js.

--- static/js/logger.js
+++ static/js/logger.js
@@ -3,7 +3,7 @@ const { createLogger, format, transports } = require('winston');
 export const logger = createLogger({
     format: format.combine(
         format.colorize(),
-        format.json()
+        format.simple()
     ),
     transports: [
         new transports.Console({ level: 'silly' }),
