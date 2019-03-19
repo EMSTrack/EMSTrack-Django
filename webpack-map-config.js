24cdb42ce7c563f127d06acee062224cba6c7a74
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-19 12:13:48 -0700

    Adapting map.js to base.js.

--- webpack-map-config.js
+++ webpack-map-config.js
@@ -3,30 +3,34 @@ var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  context: __dirname,
-  entry: './static/js/map',
-  mode: 'development',
-  output: {
-    path: path.resolve('./static/bundles/map'),
-    filename: "[name]-[hash].js"
-  },
+    context: __dirname,
+    entry: './static/js/map',
+    mode: 'development',
+    output: {
+        path: path.resolve('./static/bundles/map'),
+        filename: "[name]-[hash].js"
+    },
+    node: {
+        fs: "empty"
+    },
 
-  plugins: [
-    new BundleTracker({filename: './webpack-map-stats.json'})
-  ],
-  module: {
-    rules: [
-      {
-         test: /\.css$/,
-         use: ['style-loader', 'css-loader'],
-      },
-      {
-         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-         loader: 'url-loader',
-         options: {
-           limit: 10000,
-         },
-      },
+    plugins: [
+        new BundleTracker({filename: './webpack-map-stats.json'})
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                },
+            },
+        ],
+    },
 }
+
