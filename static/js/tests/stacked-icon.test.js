dde7ea5d69b41572b0877bb4fef77ffdd4e8395b
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-19 22:46:12 -0700

    Stacked icon testing.

--- static/js/tests/stacked-icon.test.js
+++ static/js/tests/stacked-icon.test.js
@@ -11,14 +11,14 @@ describe('stacked icon', () => {
         let json = iconFactory.createIcon();
         expect(json).to.eql({
             className: "LeafletDivIcon",
-            html: "<span class=\"fa-stack fa-2x fa-stack-marker-xs\"><i class=\"fas fa-map-marker fa-stack-2x\"></i><i class=\"fas fa-plus fa-stack-1x fa-inverse\" style=\"margin-top:0.2em\"></i></span>"
+            html: "<span class=\"fa-stack fa-2x fa-stack-marker-xs\"><i class=\"fas fa-map-marker fa-stack-2x\"></i><i class=\"fas fa-plus fa-stack-1x fa-inverse\" style=\"margin-top:0.2em\"></i></span>",
             popupAnchor: [0, -15]
         });
 
         json = iconFactory.createSimpleIcon('hospital');
         expect(json).to.eql({
             className: "LeafletDivIcon",
-            html: "<span class=\"fa-stack fa-2x fa-stack-marker-xs\"><i class=\"fas fa-map-marker fa-stack-2x\"></i><i class=\"fas fa-hospital fa-stack-1x fa-inverse\" style=\"margin-top:0.2em\"></i></span>"
+            html: "<span class=\"fa-stack fa-2x fa-stack-marker-xs\"><i class=\"fas fa-map-marker fa-stack-2x\"></i><i class=\"fas fa-hospital fa-stack-1x fa-inverse\" style=\"margin-top:0.2em\"></i></span>",
             popupAnchor: [0, -15]
         });
 
