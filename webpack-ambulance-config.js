7f7d98d55c0e7726d23641a24c908b44700ace07
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-03-20 22:34:12 -0700

    Bug on call detail page.

--- webpack-ambulance-config.js
+++ webpack-ambulance-config.js
@@ -3,30 +3,33 @@ var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  context: __dirname,
-  entry: './static/js/ambulance',
-  mode: 'development',
-  output: {
-    path: path.resolve('./static/bundles/ambulance'),
-    filename: "[name]-[hash].js"
-  },
+    context: __dirname,
+    entry: './static/js/ambulance',
+    mode: 'development',
+    output: {
+        path: path.resolve('./static/bundles/ambulance'),
+        filename: "[name]-[hash].js"
+    },
+    node: {
+        fs: "empty"
+    },
 
-  plugins: [
-    new BundleTracker({filename: './webpack-ambulance-stats.json'})
-  ],
-  module: {
-    rules: [
-      {
-         test: /\.css$/,
-         use: ['style-loader', 'css-loader'],
-      },
-      {
-         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-         loader: 'url-loader',
-         options: {
-           limit: 10000,
-         },
-      },
+    plugins: [
+        new BundleTracker({filename: './webpack-ambulance-stats.json'})
     ],
-  },
-}
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                },
+            },
+        ],
+    },
+};
--- webpack-point-widget-config.js
+++ webpack-point-widget-config.js
@@ -3,30 +3,33 @@ var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  context: __dirname,
-  entry: './static/js/point-widget',
-  mode: 'development',
-  output: {
-    path: path.resolve('./static/bundles/point-widget'),
-    filename: "[name]-[hash].js"
-  },
+    context: __dirname,
+    entry: './static/js/point-widget',
+    mode: 'development',
+    output: {
+        path: path.resolve('./static/bundles/point-widget'),
+        filename: "[name]-[hash].js"
+    },
+    node: {
+        fs: "empty"
+    },
 
-  plugins: [
-    new BundleTracker({filename: './webpack-point-widget-stats.json'})
-  ],
-  module: {
-    rules: [
-      {
-         test: /\.css$/,
-         use: ['style-loader', 'css-loader'],
-      },
-      {
-         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-         loader: 'url-loader',
-         options: {
-           limit: 10000,
-         },
-      },
+    plugins: [
+        new BundleTracker({filename: './webpack-point-widget-stats.json'})
     ],
-  },
-}
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                },
+            },
+        ],
+    },
+};
