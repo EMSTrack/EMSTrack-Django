af31452a05e10f09d494f67c153028ccff1f2745
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-06-20 08:02:10 -0700

    Adding support for MPDS codes.

--- static/js/map.js
+++ static/js/map.js
@@ -1736,7 +1736,7 @@ function dispatchCall() {
     form['details'] = $('#comment').val().trim();
     form['priority'] = $('input:radio[name=priority]:checked').val();
 
-    const radio_code = $('radio-code-list').val();
+    const radio_code = $('#radio-code-list').val();
     const radio_code_id = $('option[value="'+radio_code+'"]').attr('id');
     logger.log('debug', radio_code);
     logger.log('debug', radio_code_id);
