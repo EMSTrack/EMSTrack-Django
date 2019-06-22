29d3a2d14c8a39afab52bcfb83599cfda973921c
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-06-22 13:36:16 -0700

    Improved call visualization in map.

--- static/js/alert.js
+++ static/js/alert.js
@@ -1,10 +1,20 @@
-export function alert(message, type = 'warning', placeholder = '#alert_placeholder') {
+// internal counter for uniqueness
+let counter = 0;
+
+export function alert(message, type = 'warning', timeout = 3000, placeholder = '#alert_placeholder') {
+
+    // increment counter
+    counter += 1;
+
+    // language=HTML
     $(placeholder).append(
-        '<div class="alert alert-' + type + ' alert-dismissible fade show" role="alert">' +
-        '  <button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-        '    <span aria-hidden="true">&times;</span>' +
-        '  </button>' +
-        message +
-        '</div>'
-    );
-};
+`<div class="alert alert-${ type } alert-dismissible fade show" id="alert-${ counter }" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
+  </button>
+  ${ message }
+</div>`);
+
+    if (timeout > 0)
+        setTimeout( () => { $(placeholder).remove('#alert-' + counter); }, timeout);
+}
