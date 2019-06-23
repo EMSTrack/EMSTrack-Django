fc28a3e418b12f6f229b65e3af827629059512ca
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-06-22 17:14:03 -0700

    Working on javascript translation table.

--- ambulance/views.py
+++ ambulance/views.py
@@ -276,14 +276,13 @@ class AmbulanceMap(TemplateView):
             "Radio Code": _("Radio Code"),
             "Priority Code": _("Priority Code"),
             "Priority Classification": _("Priority Classification"),
-
             "Alert": _("Alert"),
             "Attention": _("Attention"),
-            "Invalid radio code.": _("Invalid radio code."),
-            "Invalid priority code.": _("Invalid priority code."),
-            "Please select the priority.": _("Please select the priority."),
-            "Please dispatch at least one ambulance.": _("Please dispatch at least one ambulance."),
-            "Empty name.": _("Empty name."),
+            "Invalid radio code": _("Invalid radio code"),
+            "Invalid priority code": _("Invalid priority code"),
+            "Please select the priority": _("Please select the priority"),
+            "Please dispatch at least one ambulance": _("Please dispatch at least one ambulance"),
+            "Blank name": _("Blank name"),
             "Can only dispatch available ambulances!": _("Can only dispatch available ambulances!"),
             "Could not update ambulance status": _("Could not update ambulance status")
         }
