ecc03d689583425f2cb657536854d2eb49ff39e3
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-06-23 21:06:10 -0700

    Formatting call in map.

--- static/js/map.js
+++ static/js/map.js
@@ -850,7 +850,7 @@ function addCallToGrid(call) {
                 '    </div>' +
                 '  </div>' +
                 '  <div class="col-sm-1">' +
-                '    <span id="call-' + call.id + '-' + ambulance.id +'-waypoints" class="fas fa-map-marker"></span>' +
+                '    <span id="call-' + call.id + '-' + ambulance.id +'-waypoints" class="fas fa-map-marker text-center"></span>' +
                 '  </div>' +
                 '  <div class="col-sm-1">' +
                 '    <button type="button" class="close" aria-label="Close">\n' +
