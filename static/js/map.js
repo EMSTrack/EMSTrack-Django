f6f8909deb6c6e61d382e71195087696d030f1e3
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-06-23 20:35:02 -0700

    Formatting call in map.

--- static/js/map.js
+++ static/js/map.js
@@ -832,7 +832,7 @@ function addCallToGrid(call) {
         // Add ambulance button to call item grid
         $('#call-item-grid-' + call.id)
             .append(
-                '<div class="row" style="display:flex; align-items:center;">' +
+                '<div class="row no-gutters" style="display:flex; align-items:center;">' +
                 '  <div class="col-sm-4">' +
                 '    <button type="button"' +
                 '            id="call-grid-button-' + call.id + '-' + ambulance.id + '"' +
