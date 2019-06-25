e739d55bbb53ca87a555073f3600638684861e7b
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-06-25 13:03:56 -0700

    Adding dispatcher_override to CallPermissionMixin.

--- static/js/patients.js
+++ static/js/patients.js
@@ -47,7 +47,7 @@ export class Patients {
 
     addBlankPatientForm(index) {
 
-        this.addPatientForm(index, { id: None, name: '', age: '' }, 'fa-plus');
+        this.addPatientForm(index, { id: undefined, name: '', age: '' }, 'fa-plus');
 
         // bind addBlankPatientForm to click
         this.placeholder.find('#patient-' + this.label + '-' + index + '-button')
