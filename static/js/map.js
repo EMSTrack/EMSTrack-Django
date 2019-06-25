1a75b52fbb5806a90bffa3ac307fdf45118c0ffd
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-06-25 16:31:10 -0700

    Setting csrf token in axios.

--- static/js/map.js
+++ static/js/map.js
@@ -814,30 +814,35 @@ function addCallToGrid(call) {
     $('#call-grid-' + status)
         .append(
             '<div class="card status-' + status + '" id="call-item-' + call.id + '">\n' +
-            '  <div class="card-header px-1 py-1 d-flex"' +
+            '  <div class="card-header px-1 py-1"' +
             '       id="call-' + call.id + '">\n' +
-            '    <input class="filter-checkbox" value="call" data-status="' + status + '|call_' + call.id + '"\n' +
-            '            type="checkbox" id="call-checkbox-' + call.id + '" ' +
+            '    <div class="d-flex">' +
+            '      <div class="flex-grow-1">' +
+            '        <input class="filter-checkbox" value="call" data-status="' + status + '|call_' + call.id + '"\n' +
+            '               type="checkbox" id="call-checkbox-' + call.id + '" ' +
             (visibleCategory[status + "|" + 'call_' + call.id] ? 'checked' : '') + '>\n' +
-            '    <span data-toggle="collapse"\n' +
-            '         data-target="#call-' + call.id + '-body"\n' +
-            '         aria-expanded="true" aria-controls="call-' + call.id+ '-body">' +
-            '      <span>' + call.id + ':</span>' +
-            '      <span class="font-weight-bold">' + priority_prefix + '</span>' +
-            '      <button id="call-' + call.id + '-button"\n' +
-            '             class="btn btn-'+ call_priority_css[call.priority].class + ' btn-sm">' +
-            '         ' + call_priority_css[call.priority].html + '\n' +
-            '      </button>\n' +
-            '      <span class="font-weight-bold">' + priority_suffix + '</span>' +
-            '      <span class="ml-auto" id="call-date-' + call.id + '">' + date + '</span>' +
-            '      <button id="call-' + call.id + '-patients-button" ' +
-            '              type="button" class="btn btn-outline-dark btn-sm" aria-label="Patients">' +
-            '        <span class="fas fa-user fa-sm"></span>' +
-            '      </button>\n' +
-            '    </span>\n' +
-            '    <button id="call-' + call.id + '-abort" type="button" class="close ml-1" aria-label="Close">\n' +
-            '      <span aria-hidden="true">&times;</span>\n' +
-            '    </button>\n' +
+            '        <span data-toggle="collapse"\n' +
+            '              data-target="#call-' + call.id + '-body"\n' +
+            '              aria-expanded="true" aria-controls="call-' + call.id+ '-body"></span>' +
+            '        <span>' + call.id + ':</span>' +
+            '        <span class="font-weight-bold">' + priority_prefix + '</span>' +
+            '        <button id="call-' + call.id + '-button"\n' +
+            '               class="btn btn-'+ call_priority_css[call.priority].class + ' btn-sm">' +
+            '           ' + call_priority_css[call.priority].html + '\n' +
+            '        </button>\n' +
+            '        <span class="font-weight-bold">' + priority_suffix + '</span>' +
+            '      </div>' +
+            '      <div>' +
+            '        <span id="call-date-' + call.id + '">' + date + '</span>' +
+            '        <button id="call-' + call.id + '-patients-button" ' +
+            '                type="button" class="btn btn-outline-dark btn-sm" aria-label="Patients">' +
+            '          <span class="fas fa-user fa-sm"></span>' +
+            '        </button>\n' +
+            '        <button id="call-' + call.id + '-abort" type="button" class="close ml-1" aria-label="Close">\n' +
+            '          <span aria-hidden="true">&times;</span>\n' +
+            '        </button>\n' +
+            '      </div>' +
+            '    </div>' +
             '  </div>\n' +
             '  <div class="collapse"\n' +
             '       id="call-' + call.id + '-body"\n' +
