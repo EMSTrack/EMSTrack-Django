a7f0b344535aad1ddde7c1e63b9a0fc3b68608cb
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-06-25 14:06:54 -0700

    Test call update serializer.

--- ambulance/tests/test_calls.py
+++ ambulance/tests/test_calls.py
@@ -1114,13 +1114,26 @@ class TestCall(TestSetup):
         }
         serializer = CallSerializer(call, data=data)
         serializer.is_valid()
-        logger.debug(serializer.errors)
         call = serializer.save(updated_by=self.u1)
         self.assertEqual(call.status, CallStatus.S.name)
         self.assertEqual(call.priority, CallPriority.D.name)
         self.assertNotEqual(call.pending_at, None)
         self.assertNotEqual(call.started_at, None)
         self.assertEqual(call.ended_at, None)
+
+        patient_set = PatientSerializer(call.patient_set.all(), many=True).data
+        patient_set[0]['age'] = 5
+        patient_set[1]['age'] = 12
+
+        data = {
+            'patient_set': patient_set
+        }
+        serializer = CallSerializer(call, data=data)
+        serializer.is_valid()
+        call = serializer.save(updated_by=self.u1)
+        self.assertEqual(call.status, CallStatus.S.name)
+        self.assertEqual(call.priority, CallPriority.D.name)
+        self.assertCountEqual(call.patient_set, patient_set)
 
     def test_call_create_viewset(self):
 
