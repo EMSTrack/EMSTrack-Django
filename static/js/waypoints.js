2e225a5b878194afef7dcc6aefa3344d59ce250b
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-06-27 15:07:22 -0700

    Waypoint editor.

--- static/js/waypoints.js
+++ static/js/waypoints.js
@@ -215,11 +215,13 @@ export class Waypoints {
                 const previousWaypoint = this.waypoints[this.activeIndex - 1];
 
                 // swap items
-                const items = $(`#call-${this.label}-carousel-items`);
-                items.removeClass('active');
+                $(`#call-${this.label}-carousel-items`)
+                    .removeClass('active');
                 swap(`#call-${this.label}-carousel-waypoint-${activeWaypoint.order}-item`,
                     `#call-${this.label}-carousel-waypoint-${previousWaypoint.order}-item`);
-                items.eq(this.activeIndex - 1).addClass('active');
+                $(`#call-${this.label}-carousel-items`)
+                    .eq(this.activeIndex - 1)
+                    .addClass('active');
 
                 // reset active indicator
                 const indicators = $(`#call-${this.label}-carousel-indicators li`);
@@ -244,11 +246,13 @@ export class Waypoints {
                 const nextWaypoint = this.waypoints[this.activeIndex + 1];
 
                 // swap items
-                const items = $(`#call-${this.label}-carousel-items`);
-                items.removeClass('active');
+                $(`#call-${this.label}-carousel-items`)
+                    .removeClass('active');
                 swap(`#call-${this.label}-carousel-waypoint-${activeWaypoint.order}-item`,
                     `#call-${this.label}-carousel-waypoint-${nextWaypoint.order}-item`);
-                items.eq(this.activeIndex + 1).addClass('active');
+                $(`#call-${this.label}-carousel-items`)
+                    .eq(this.activeIndex + 1)
+                    .addClass('active');
 
                 // reset active indicator
                 const indicators = $(`#call-${this.label}-carousel-indicators li`);
