2e862f5718d4324536365758ddde480920bc7bed
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-06-30 13:58:45 -0700

    Replacing bsalert by dialog.alert

--- static/js/map.js
+++ static/js/map.js
@@ -72,7 +72,7 @@ LocationSettings.translation_table = translation_table;
 LocationSettings.map_provider = mapProvider;
 
 // Initialize settings
-settings = new Settings({
+const settings = new Settings({
     locations: locations,
     location_type: location_type,
     translation_table: translation_table,
