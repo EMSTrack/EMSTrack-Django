0fa027daac4abe343ad44b473a83276338924235
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-10-10 20:15:25 -0700

    Turning mqtt publication off for initialization.

--- mqtt/cache_clear.py
+++ mqtt/cache_clear.py
@@ -1,3 +1,4 @@
+import os
 from login.permissions import cache_clear
 
 
@@ -6,6 +7,7 @@ def mqtt_cache_clear():
     # call cache_clear locally
     cache_clear()
 
-    # and signal through mqtt
-    from mqtt.publish import SingletonPublishClient
-    SingletonPublishClient().publish_message('cache_clear')
+    if os.environ.get("DJANGO_ENABLE_MQTT_PUBLISH", "True"):
+        # and signal through mqtt
+        from mqtt.publish import SingletonPublishClient
+        SingletonPublishClient().publish_message('cache_clear')
