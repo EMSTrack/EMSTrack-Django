ba57aeb7227ea9b92b5a42912b860eeb4c34ef66
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-10-12 12:20:39 -0700

    Testing mqtt.

--- mqtt/tests/test_mqtt_3.py
+++ mqtt/tests/test_mqtt_3.py
@@ -235,7 +235,7 @@ class TestMQTTHandshakeReconnect(TestMQTT, MQTTTestCase):
 
         # check record log
         obj = ClientLog.objects.filter(client=clnt).order_by('-updated_on')
-        self.assertEqual(len(obj), nrecords + 5)
+        self.assertEqual(len(obj), nrecords + 6)
         self.assertEqual(obj[0].status, ClientStatus.O.name)
         self.assertEqual(obj[1].status, ClientStatus.O.name)
         self.assertEqual(obj[2].status, ClientStatus.D.name)
