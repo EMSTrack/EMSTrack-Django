4d45da9a72b9fd370806607315fb54ea238318df
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-10-27 21:28:23 -0700

    Vehicle report.

--- static/js/report-vehicle.js
+++ static/js/report-vehicle.js
@@ -4,6 +4,8 @@ import {addAmbulanceRoute, addCallWaypoints } from "./map-tools";
 
 import { logger } from './logger';
 
+const axios = require('axios');
+
 let map;
 let apiClient;
 const vehicles = {};
@@ -51,7 +53,7 @@ function init (client) {
             });
 
             logger.log('debug', "requests = %s", requests);
-            return apiClient.httpClient.all(requests);
+            return axios.all(requests);
 
         })
         .then( responses => responses.forEach(
