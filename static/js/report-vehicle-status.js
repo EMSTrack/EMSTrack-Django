7ef716b8f374ae7dc3045d8386a9c03de64c600c
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-10-29 16:13:08 -0700

    Adding nouislider dependency.

--- static/js/report-vehicle-status.js
+++ static/js/report-vehicle-status.js
@@ -275,7 +275,8 @@ $(function () {
             'max': [24]
         },
         pips: {
-            mode: 'steps',
+            mode: 'count',
+            values: 25,
             density: 0.96,
         }
     });
