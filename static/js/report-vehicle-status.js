dde7a8b26ddd5f5db639c270e104295b56f8ae6f
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-10-29 20:53:47 -0700

    Structuring vehicle status report.

--- static/js/report-vehicle-status.js
+++ static/js/report-vehicle-status.js
@@ -229,18 +229,22 @@ function renderRuler(beginDate, endDate, offsetMillis = 0) {
     let progress = '<div class="progress" style="height: 20px;">\n';
     const totalTime = endDate.getTime() - beginDate.getTime();
 
-    const [hours, minutes, seconds, milliseconds] = millisToSplitTime(offsetMillis);
+    // const [hours, minutes, seconds, milliseconds] = millisToSplitTime(offsetMillis);
     const nextHourMillis = splitTimeToMillis(Math.ceil(offsetMillis / 1000 / 60 / 60), 0, 0, 0);
+    const [hours, minutes, seconds, milliseconds] = millisToSplitTime(nextHourMillis);
 
-    const offsetHours = hours;
+    logger.log('debug', 'offsetHour = %s:%s:%s.%s, nextHourMillis = %s', hours, minutes, seconds, milliseconds, nextHourMillis);
+
+    const labels = ['secondary', 'light'];
+
+    const offsetHour = hours;
     const offsetDelta = 100 * ((nextHourMillis - offsetMillis) / totalTime);
-    progress += `<div class="progress-bar" role="progressbar" style="width: ${offsetDelta}%" aria-valuenow="${offsetDelta}" aria-valuemin="0" aria-valuemax="100"></div>\n`;
+    progress += `<div class="progress-bar bg-${labels[(offsetHour - 1)% 2]}" role="progressbar" style="width: ${offsetDelta}%" aria-valuenow="${offsetDelta}" aria-valuemin="0" aria-valuemax="100"></div>\n`;
 
     const numberOfHours = Math.floor(totalTime / 1000 / 60 / 60);
     const delta = 100 * (1000 * 60 * 60 / totalTime);
-    const labels = ['secondary', 'light'];
-    for (let i = offsetHours; i < offsetHours + numberOfHours; i++) {
-        progress += `<div class="progress-bar bg-${labels[i % 2]} text-${labels[(i + 1) % 2]}" role="progressbar" style="width: ${delta}%" aria-valuenow="${delta}" aria-valuemin="0" aria-valuemax="100">${i + offsetHours}</div>\n`;
+    for (let i = offsetHour; i < offsetHour + numberOfHours; i++) {
+        progress += `<div class="progress-bar bg-${labels[i % 2]} text-${labels[(i + 1) % 2]}" role="progressbar" style="width: ${delta}%" aria-valuenow="${delta}" aria-valuemin="0" aria-valuemax="100">${i + offsetHour}</div>\n`;
     }
     progress += '</div>';
     logger.log('debug', 'progress = %s', progress);
