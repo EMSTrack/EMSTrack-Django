8c582c892e1f9c5a95848020eb763ae014560405
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-11-05 08:19:34 -0800

    Export users.

--- login/resources.py
+++ login/resources.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.models import Group
 from django.contrib.auth.models import User
 
-from import_export import resources, fields
+from import_export import resources, fields, widgets
 
 
 class UserResource(resources.ModelResource):
-    is_dispatcher = fields.Field(attribute='userprofile__is_dispatcher')
+    is_dispatcher = fields.Field(attribute='userprofile__is_dispatcher',
+                                 widget=widgets.BooleanWidget)
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email',
                   'is_staff', 'is_dispatcher', 'is_active')
+        export_order = ('username', 'first_name', 'last_name', 'email',
+                        'is_staff', 'is_dispatcher', 'is_active')
