53b7f3405c19bdf31a9ef0b8849acd2f021f35b8
Mauricio de Oliveira <mauricio@emstrack.org>
2020-04-20 10:28:56 -0700

    call editing on map

--- ambulance/signals.py
+++ ambulance/signals.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 from emstrack.sms import client
 
