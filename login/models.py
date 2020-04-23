dc17660998ea11f43791447956c511e31d3dc4ea
Mauricio de Oliveira <mauricio@emstrack.org>
2020-04-23 11:45:48 -0700

    Fixing ordering in can_sms_notifications when there are not registered groups.

--- login/models.py
+++ login/models.py
@@ -63,10 +63,10 @@ def can_sms_notifications():
     n = len(users)
     if n > 1:
         # union of all groups
-        users = users[0].union(users[1:])
+        users = users[0].union(users[1:]).order_by('username')
     elif n == 1:
         # just one group
-        users = users[0]
+        users = users[0].order_by('username')
     return users
 
 
