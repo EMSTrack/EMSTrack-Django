9cbe74c251c6c1658e3daadca83fd649ec0e4595
Mauricio de Oliveira <mauricio@emstrack.org>
2020-05-02 17:09:18 -0700

    Fixing tests

--- ambulance/tests/test_ambulances.py
+++ ambulance/tests/test_ambulances.py
@@ -15,7 +15,7 @@ import json
 from ambulance.models import Ambulance, \
     AmbulanceStatus, AmbulanceCapability, AmbulanceUpdate
 from emstrack.latlon import calculate_orientation
-from ambulance.serializers import AmbulanceSerializer, AmbulanceUpdateSerializer
+from ambulance.serializers import AmbulanceSerializer, AmbulanceUpdateSerializer, AmbulanceUpdateCompactSerializer
 
 from login.models import Client as loginClient
 
@@ -997,13 +997,13 @@ class TestAmbulanceBulkUpdates(TestSetup):
         queryset = AmbulanceUpdate.objects.filter(ambulance=a)
         answer1 = []
         for u in queryset:
-            serializer = AmbulanceUpdateSerializer(u)
+            serializer = AmbulanceUpdateCompactSerializer(u)
             result = {
-                'id': u.id,
-                'ambulance_id': u.ambulance.id,
-                'ambulance_identifier': u.ambulance.identifier,
-                'comment': u.comment,
-                'capability': u.capability,
+                # 'id': u.id,
+                # 'ambulance_id': u.ambulance.id,
+                # 'ambulance_identifier': u.ambulance.identifier,
+                # 'comment': u.comment,
+                # 'capability': u.capability,
                 'status': u.status,
                 'orientation': u.orientation,
                 'location': point2str(u.location),
@@ -1059,13 +1059,13 @@ class TestAmbulanceBulkUpdates(TestSetup):
         queryset = AmbulanceUpdate.objects.filter(ambulance=a)
         answer3 = []
         for u in queryset:
-            serializer = AmbulanceUpdateSerializer(u)
+            serializer = AmbulanceUpdateCompactSerializer(u)
             result = {
-                'id': u.id,
-                'ambulance_id': a.id,
-                'ambulance_identifier': a.identifier,
-                'comment': u.comment,
-                'capability': u.capability,
+                # 'id': u.id,
+                # 'ambulance_id': a.id,
+                # 'ambulance_identifier': a.identifier,
+                # 'comment': u.comment,
+                # 'capability': u.capability,
                 'status': u.status,
                 'orientation': u.orientation,
                 'location': point2str(u.location),
