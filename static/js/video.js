8a21d46f10c8d533c61e19b124c39d85b806235e
Mauricio de Oliveira <mauricio@emstrack.org>
2020-05-07 15:06:47 -0700

    Add javascript support for video page

--- static/js/video.js
+++ static/js/video.js
@@ -15,6 +15,53 @@ function init (client) {
 
 }
 
+let yourConn;
+let stream;
+
 $(function () {
+
+    const localVideo = $('#localVideo');
+    const remoteVideo = $('#remoteVideo');
+
+    //**********************
+    //Starting a peer connection
+    //**********************
+    // https://www.tutorialspoint.com/webrtc/webrtc_video_demo.htm
+
+    //getting local video stream
+    navigator.webkitGetUserMedia({ video: true, audio: true }, function (myStream) {
+        stream = myStream;
+
+        //displaying local video stream on the page
+        localVideo.src = window.URL.createObjectURL(stream);
+
+        //using Google public stun server
+        var configuration = {
+            "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
+        };
+
+        yourConn = new webkitRTCPeerConnection(configuration);
+
+        // setup stream listening
+        yourConn.addStream(stream);
+
+        //when a remote user adds stream to the peer connection, we display it
+        yourConn.onaddstream = function (e) {
+            remoteVideo.src = window.URL.createObjectURL(e.stream);
+        };
+
+        // Setup ice handling
+        yourConn.onicecandidate = function (event) {
+            if (event.candidate) {
+                send({
+                    type: "candidate",
+                    candidate: event.candidate
+                });
+            }
+        };
+
+    }, function (error) {
+        console.log(error);
+    });
 
 })
