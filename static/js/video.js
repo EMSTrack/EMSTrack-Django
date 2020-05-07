da1f919c8d4f601558bba485038c9fe779b73944
Mauricio de Oliveira <mauricio@emstrack.org>
2020-05-07 15:17:09 -0700

    Add javascript support for video page

--- static/js/video.js
+++ static/js/video.js
@@ -15,53 +15,69 @@ function init (client) {
 
 }
 
-let yourConn;
-let stream;
+let RTCConnection;
+let localStream;
+
+// Video element where stream will be placed.
+const localVideo = document.querySelector('localVideo');
+const remoteVideo = document.querySelector('remoteVideo');
+
+// Handles success by adding the MediaStream to the video element.
+function gotLocalMediaStream(mediaStream) {
+    localStream = mediaStream;
+    localVideo.srcObject = mediaStream;
+}
+
+function setupRTC() {
+
+    //using Google public stun server
+    const configuration = {
+        "iceServers": [{"url": "stun:stun2.1.google.com:19302"}]
+    };
+    RTCConnection = new webkitRTCPeerConnection(configuration);
+
+    // setup stream listening
+    RTCConnection.addStream(localStream);
+
+    //when a remote user adds stream to the peer connection, we display it
+    RTCConnection.onaddstream = function (e) {
+        remoteVideo.srcObject = e.stream;
+    };
+
+    // Setup ice handling
+    RTCConnection.onicecandidate = function (event) {
+        if (event.candidate) {
+            send({
+                type: "candidate",
+                candidate: event.candidate
+            });
+        }
+    };
+
+}
+
+// Handles error by logging a message to the console with the error message.
+function handleLocalMediaStreamError(error) {
+    console.log('navigator.getUserMedia error: ', error);
+}
 
 $(function () {
-
-    const localVideo = $('#localVideo');
-    const remoteVideo = $('#remoteVideo');
 
     //**********************
     //Starting a peer connection
     //**********************
     // https://www.tutorialspoint.com/webrtc/webrtc_video_demo.htm
 
-    //getting local video stream
-    navigator.webkitGetUserMedia({ video: true, audio: true }, function (myStream) {
-        stream = myStream;
+    // On this codelab, you will be streaming only video (video: true).
+    const mediaStreamConstraints = {
+        video: true,
+        audio: true,
+    };
 
-        //displaying local video stream on the page
-        localVideo.src = window.URL.createObjectURL(stream);
-
-        //using Google public stun server
-        var configuration = {
-            "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
-        };
-
-        yourConn = new webkitRTCPeerConnection(configuration);
-
-        // setup stream listening
-        yourConn.addStream(stream);
-
-        //when a remote user adds stream to the peer connection, we display it
-        yourConn.onaddstream = function (e) {
-            remoteVideo.src = window.URL.createObjectURL(e.stream);
-        };
-
-        // Setup ice handling
-        yourConn.onicecandidate = function (event) {
-            if (event.candidate) {
-                send({
-                    type: "candidate",
-                    candidate: event.candidate
-                });
-            }
-        };
-
-    }, function (error) {
-        console.log(error);
-    });
+    // Initializes media stream.
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+        .then(gotLocalMediaStream)
+        .then(setupRTC)
+        .catch(handleLocalMediaStreamError);
 
 })
