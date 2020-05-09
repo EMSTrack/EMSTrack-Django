e9fcdf4e051249bbc7444cb355dc446d2dadda24
Mauricio de Oliveira <mauricio@emstrack.org>
2020-05-09 16:40:22 -0700

    Handshake for javascript clients

--- static/js/video.js
+++ static/js/video.js
@@ -242,6 +242,7 @@ function sendMessage(peer, message) {
     }
 
     message.client = localClient;
+    console.log(message);
     logger.log('info', 'Client sending message: %j to %j', message, peer);
     //socket.emit('message', message);
     apiClient.publish(`user/${peer.username}/client/${peer.client_id}/webrtc/message`, JSON.stringify(message), 0, false);
