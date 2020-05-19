a548dccb107197d84f56393f8cf4a950a5236543
sungwookson <sungwookson@gmail.com>
2020-05-19 18:45:42 +0900

    Trying new View

--- guest/urls.py
+++ guest/urls.py
@@ -7,6 +7,12 @@ from . import views
 app_name = 'guest'
 urlpatterns = [
 
+    url(r'^video/(?P<receiver_info>)$',
+        views.VideoView.as_view(),
+        name="video"
+    ),
+
+
     url(r'^$',
         guest_login_required(views.IndexView.as_view()),
         name="index"),
--- guest/views.py
+++ guest/views.py
@@ -3,9 +3,35 @@ import logging
 from django.conf import settings
 from django.views.generic.base import TemplateView
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from django.shortcuts import redirect
+
+from login.models import UserProfile
 logger = logging.getLogger(__name__)
 
 
 class IndexView(TemplateView):
     template_name = 'video/index.html'
+
+
+
+class VideoView(APIView):
+
+    def get(self, request, receiver_info=None):
+        temp_user = User.objects.create_user(
+                username=receiver_info,
+                email='',
+                password='top_secret')
+        UserProfile.objects.create(is_guest=True, 
+                mobile_number=receiver_info,
+                user=temp_user)
+        return Response(UserProfile)
+
+        #response = redirect('/redirect-success/')
+        #return response
+
+
+
