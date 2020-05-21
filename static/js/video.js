be0d2366a939b00458c778613564e7955d5230ee
Mauricio de Oliveira <mauricio@emstrack.org>
2020-05-20 18:50:58 -0700

    Adding turn server to video.js

--- static/js/video.js
+++ static/js/video.js
@@ -495,7 +495,7 @@ window.onbeforeunload = function() {
 
 function createPeerConnection() {
     try {
-        pc = new RTCPeerConnection(null);
+        pc = new RTCPeerConnection(pcConfig);
         pc.onicecandidate = handleIceCandidate;
         pc.onaddstream = handleRemoteStreamAdded;
         pc.onremovestream = handleRemoteStreamRemoved;
