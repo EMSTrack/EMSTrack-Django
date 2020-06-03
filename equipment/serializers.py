5f1ed85b8bdd42cd35c255a88afde59b98f0f682
xannem <41248956+xannem@users.noreply.github.com>
2020-06-03 15:17:29 -0700

    updates

--- equipment/serializers.py
+++ equipment/serializers.py
@@ -17,52 +17,6 @@ class EquipmentItemSerializer(serializers.ModelSerializer):
                             'equipment_id', 'equipment_name', 'equipment_type',
                             'updated_by',)
 
-    def validate(self, data):
-        validated_data = super().validate(data)
-        # Get what type of value is trying to be updated
-        # print("Checking if value is integer value")
-        status_of_val = ""
-        str_search = True
-        try:
-            (validated_data['value'])
-        except KeyError:
-            return validated_data
-
-        try:
-            int(validated_data['value'])
-            status_of_val = EquipmentType.I.name
-            str_search = False
-        except ValueError:
-            str_search = True
-
-        # ("Checking if value is string value")
-        if (isinstance(validated_data['value'], str) and str_search and
-                validated_data['value'] != 'False' and validated_data['value'] != 'True'):
-            status_of_val = EquipmentType.S.name
-
-        # ("Checking if value is boolean value False")
-        if (validated_data['value'] == 'False') or (validated_data['value'] == 'True'):
-            status_of_val = EquipmentType.B.name
-
-        # printing info
-        # print("Value of Updated Data in status_of_val either I S or B. " +
-        #       validated_data['value'] + " is " + status_of_val)
-
-        type_of_data = self.instance.equipment.type
-        # print("Type of data itself: " + type_of_data)
-        if status_of_val == EquipmentType.I.name and type_of_data != EquipmentType.I.name:
-            # print("Trying to set non integer value to integer")
-            raise serializers.ValidationError('Trying to set non integer value to integer')
-
-        if status_of_val == EquipmentType.B.name and type_of_data != EquipmentType.B.name:
-            # print("Trying to set non bool value to bool")
-            raise serializers.ValidationError('Trying to set non bool value to bool')
-
-        if status_of_val == EquipmentType.S.name and type_of_data != EquipmentType.S.name:
-            # print("Trying to set non string value to string")
-            raise serializers.ValidationError('Trying to set non string value to string')
-
-        return validated_data
 
 
 class EquipmentSerializer(serializers.ModelSerializer):
