a8540826aa8bfbc43a60226c146b1cc9ebe2111d
Mauricio de Oliveira <mauricio@emstrack.org>
2020-06-08 14:01:24 -0700

    TokenLogin model and serializer

--- login/serializers.py
+++ login/serializers.py
@@ -1,5 +1,7 @@
 import logging
 
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
 
 from .models import UserAmbulancePermission, UserHospitalPermission, Client, TokenLogin
