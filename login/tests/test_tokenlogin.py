3092b44f2d1921ddf2aeb11d50f164e770d6aabc
Mauricio de Oliveira <mauricio@emstrack.org>
2020-06-09 10:30:44 -0700

    TokenLogin model and serializer

--- login/tests/test_tokenlogin.py
+++ login/tests/test_tokenlogin.py
@@ -96,7 +96,7 @@ class TestTokenLogin(TestSetup):
         client = Client()
 
         # login with token
-        response = client.get('/en/auth/login/{}'.format(str(token['token'])), follow=True)
+        response = client.get('/en/auth/login/{}/'.format(str(token['token'])), follow=True)
         logger.debug(response)
         self.assertEqual(response.status_code, 200)
 
@@ -128,7 +128,7 @@ class TestTokenLogin(TestSetup):
         client = Client()
 
         # login with token
-        response = client.get('/en/auth/login/{}'.format(str(token['token'])), follow=True)
+        response = client.get('/en/auth/login/{}/'.format(str(token['token'])), follow=True)
         logger.debug(response)
         self.assertEqual(response.status_code, 200)
 
