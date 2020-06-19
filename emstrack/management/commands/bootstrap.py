5aae8449ae31fad2ae0d78ec12098e5591768bd2
Mauricio de Oliveira <mauricio@emstrack.org>
2020-06-18 22:30:42 -0700

    Guest pages and video styling

--- emstrack/management/commands/bootstrap.py
+++ emstrack/management/commands/bootstrap.py
@@ -4,6 +4,7 @@ from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
 from django.db import DEFAULT_DB_ALIAS
+from django.db import IntegrityError
 
 
 class Command(BaseCommand):
