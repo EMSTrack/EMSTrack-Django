19e6ffa3e8d5a91dc015361c8ef4b2ed1466db90
Mauricio de Oliveira <mauricio@emstrack.org>
2020-07-09 14:54:49 -0700

    Video controls

--- static/js/components/drag.js
+++ static/js/components/drag.js
@@ -1,5 +1,5 @@
 // Make an element draggable:
-export function dragElement(elmnt) {
+export function dragElement(elmnt, parent = null) {
 
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     // otherwise, move the DIV from anywhere inside the DIV:
@@ -24,9 +24,13 @@ export function dragElement(elmnt) {
         pos2 = pos4 - e.clientY;
         pos3 = e.clientX;
         pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+        const boun=parent.offsetWidth-elmnt.offsetWidth;
+        if (parent === null || ((pos1>0)&&(pos1<boun)&&(pos2>0)&&(pos2<boun))) {
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
     }
 
     function closeDragElement() {
