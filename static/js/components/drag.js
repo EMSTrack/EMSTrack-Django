7427d55c527e66ade7f693549f4a40817f899754
Mauricio de Oliveira <mauricio@emstrack.org>
2020-07-12 11:44:27 -0700

    Video controls

--- static/js/components/drag.js
+++ static/js/components/drag.js
@@ -1,5 +1,3 @@
-
-
 // Make an element draggable:
 import {logger} from "../logger";
 
@@ -68,63 +66,5 @@ export function dragElement(elmnt, parent = null) {
         document.onmouseup = onMouseUp;
 
     }
-
-
-        /*
-        // get the mouse cursor position at startup:
-        const posX = e.clientX,
-              posY = e.clientY;
-
-        // calculate dX and dY
-        if (parent !== null) {
-
-            const offsets = parent.getBoundingClientRect();
-            parentTop = offsets.top;
-            parentLeft = offsets.left;
-
-            boundX = parent.offsetWidth - elmnt.offsetWidth;
-            boundY = parent.offsetHeight - elmnt.offsetHeight;
-
-        }
-
-        dX = posX - parentLeft - elmnt.style.left.replace('px','');
-        dY = posY - parentTop - elmnt.style.top.replace('px','');
-
-        logger.log('debug', 'dX = %d, dY = %d, parentTop = %d, parentLeft = %d, boundX = %d, boundY = %d',
-            dX, dY, parentTop, parentLeft, boundX, boundY);
-
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        // e = e || window.event;
-        e.preventDefault();
-
-        // calculate the new cursor position:
-        const posX = e.clientX,
-              posY = e.clientY,
-
-              aX = posX - dX,
-              aY = posY - dY;
-
-        logger.log('debug', 'aX = %d, aY = %d', aX, aY);
-
-        if (parent === null ||
-            ((aX>parentLeft) && (aX<parentLeft+boundX) &&
-                (aY>parentTop) && (aY<parentTop+boundY))) {
-            // set the element's new position:
-            elmnt.style.left = aX + "px";
-            elmnt.style.top = aY + "px";
-        }
-    }
-
-    function closeDragElement() {
-        // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-
-         */
 
 }
