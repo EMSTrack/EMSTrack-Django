964ca24386b6210292764902a1d7a700ed0382ce
Mauricio de Oliveira <mauricio@emstrack.org>
2021-10-20 21:13:52 -0700

    Attempt to fix the webpack_loader issue

--- emstrack/webpack.py
+++ emstrack/webpack.py
@@ -5,11 +5,11 @@
 from webpack_loader.loader import WebpackLoader
 
 class CustomWebpackLoader(WebpackLoader):
-    def get_chunk_url(self, chunk):
-        asset = self.get_assets()['assets'][chunk['name']]
-        return super().get_chunk_url(asset)
+    # def get_chunk_url(self, chunk):
+    #     asset = self.get_assets()['assets'][chunk['name']]
+    #     return super().get_chunk_url(asset)
 
-    # def filter_chunks(self, chunks):
-    #     chunks = [chunk if isinstance(chunk, dict) else {'name': chunk} for chunk in chunks]
-    #     return super().filter_chunks(chunks)
+    def filter_chunks(self, chunks):
+        chunks = [chunk['name'] if isinstance(chunk, dict) else chunk for chunk in chunks]
+        return super().filter_chunks(chunks)
 
