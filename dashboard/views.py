10a1faba875de10bdea53c5dcdac0ef3399ba2b5
koumorikun <turbohuynh@gmail.com>
2021-11-17 16:57:54 -0800

    dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,20 @@
 import logging
-
 from django.views.generic.base import TemplateView
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 logger = logging.getLogger(__name__)
 
 class DashboardView(TemplateView):
     template_name = 'dashboard/index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # for user in User.objects.all():
+        #     Token.objects.get_or_create(user=user)
+        
+        context['token'] = None
+        logger.error(request.user)
+
+        return context
