bb93c390066c090659f4776d0597161946a8db07
koumorikun <turbohuynh@gmail.com>
2021-11-29 17:28:00 -0800

    dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,6 @@ import logging
 from django.views.generic.base import TemplateView
 from rest_framework.authtoken.models import Token
 import requests
-import os
 
 
 logger = logging.getLogger(__name__)
@@ -12,19 +11,24 @@ class DashboardView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        SERVER = "UCSD"
-        # param = {
-        #     'username': os.environ["DASHBOARD_USERNAME_" + SERVER],
-        #     'password': os.environ["DASHBOARD_PASSWORD_" + SERVER],
-        # }
-        url = os.environ["DASHBOARD_AUTHURL_" + SERVER]
-        # res = requests.post(url, data=param)
-        # res.raise_for_status()
-        # token = res.json()['token']
-        # token = self.get_token()
-        token = url
+        token = self.get_token()
         # token, _ = Token.objects.get_or_create(user=self.request.user)
         context['token'] = token
         context['user'] = self.request.user
 
         return context
+
+    def get_token(self):
+        """
+        Sets the token for accessing the API of EMSTrack.
+        """
+        SERVER = "UCSD"
+        param = {
+            'username': os.environ["DASHBOARD_USERNAME_" + SERVER],
+            'password': os.environ["DASHBOARD_PASSWORD_" + SERVER],
+        }
+        url = os.environ["DASHBOARD_AUTHURL_" + SERVER]
+        res = requests.post(url, data=param)
+        res.raise_for_status()
+        token = res.json()['token']
+        return token
