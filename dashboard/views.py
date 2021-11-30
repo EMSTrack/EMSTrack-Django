705072e08c7d221f6a73c9ebe7a2706afb12be95
koumorikun <turbohuynh@gmail.com>
2021-11-29 17:41:40 -0800

    dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,24 +11,17 @@ class DashboardView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        token = self.get_token()
-        # token, _ = Token.objects.get_or_create(user=self.request.user)
-        context['token'] = token
-        context['user'] = self.request.user
-
-        return context
-
-    def get_token(self):
-        """
-        Sets the token for accessing the API of EMSTrack.
-        """
         SERVER = "UCSD"
         param = {
             'username': os.environ["DASHBOARD_USERNAME_" + SERVER],
             'password': os.environ["DASHBOARD_PASSWORD_" + SERVER],
         }
         url = os.environ["DASHBOARD_AUTHURL_" + SERVER]
+        token = url
         # res = requests.post(url, data=param)
         # res.raise_for_status()
         # token = res.json()['token']
-        return url
+        # token, _ = Token.objects.get_or_create(user=self.request.user)
+        context['token'] = token
+        context['user'] = self.request.user
+        return context
