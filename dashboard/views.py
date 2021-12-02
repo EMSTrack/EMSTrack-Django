c13c9b341e3e014acf67992b9c90e9f3adf48087
koumorikun <turbohuynh@gmail.com>
2021-12-01 16:53:08 -0800

    dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -23,5 +23,7 @@ class DashboardView(TemplateView):
         token = res.json()['token']
         token_2, _ = Token.objects.get_or_create(user=self.request.user)
         context['token'] = token
+        context['username1'] = os.environ["DASHBOARD_USERNAME_" + SERVER]
         context['token_2'] = token_2
+        context['username2'] = self.request.user
         return context
