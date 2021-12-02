4d459a98624ad2801a23590a8cd79b08d4cfac4d
koumorikun <turbohuynh@gmail.com>
2021-12-01 20:36:49 -0800

    dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -27,11 +27,11 @@ class DashboardView(TemplateView):
             list_of_users.append(user)
             token_temp, created = Token.objects.get_or_create(user=user)
             list_of_tokens.append(token_temp.key)
-        token_2 = Token.objects.get(id=self.request.user.id).key
         logger.error(list_of_users)
         logger.error(list_of_tokens)
         logger.error(self.request.user)
         logger.error(list_of_users[1])
+        token_2 = Token.objects.get(id=self.request.user.id).key
         context['token'] = self.request.user.id
         context['username1'] = token_2
         context['token_2'] = self.request.user.is_authenticated
