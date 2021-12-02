1d54450d23383b79ffb29c5abeae24ce10e4bb74
koumorikun <turbohuynh@gmail.com>
2021-12-01 17:53:14 -0800

    dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -28,7 +28,9 @@ class DashboardView(TemplateView):
             list_of_tokens.append(Token.objects.get_or_create(user=user).key)
         token_2, created = Token.objects.get_or_create(user=self.request.user)
         context['token'] = token_2.key
-        context['username1'] = list_of_tokens
+        context['username1'] = list_of_tokens[0]
         context['token_2'] = token # print the key
-        context['username2'] = list_of_users
+        context['username2'] = list_of_users[0]
+        print(list_of_users)
+        print(list_of_tokens)
         return context
