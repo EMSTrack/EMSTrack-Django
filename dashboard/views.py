6b540f098597488616adbea304252cc34324cc62
koumorikun <turbohuynh@gmail.com>
2021-12-18 00:20:36 -0800

    localhost

--- dashboard/views.py
+++ dashboard/views.py
@@ -13,33 +13,19 @@ class DashboardView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # SERVER = "UCSD"
-        # param = {
-        #     'username': os.environ["DASHBOARD_USERNAME_" + SERVER],
-        #     'password': os.environ["DASHBOARD_PASSWORD_" + SERVER],
-        # }
-        # url = os.environ["DASHBOARD_AUTHURL_" + SERVER]
-        # res = requests.post(url, data=param)
-        # res.raise_for_status()
-        # token = res.json()['token']
-        # list_of_tokens = []
-        # list_of_users = []
-        # for user in User.objects.all():
-        #     list_of_users.append(user)
-        #     token_temp, created = Token.objects.get_or_create(user=user)
-        #     list_of_tokens.append(token_temp.key)
-        # logger.error(list_of_users)
-        # logger.error(list_of_tokens)
-        # logger.error(self.request.user)
-        # logger.error(list_of_users[1])
-        # user = User.objects.get(username="admin")
-        # logger.error(user)
-        # user = authenticate(username='admin', password='cruzrojaadmin')
-        token, created = Token.objects.get_or_create(user=self.request.user)
-        context['token'] = token.key
-        # context['token'] = self.request.META[]
-        # context['username1'] = token_2.key
-        # context['token_2'] = self.request.user.is_authenticated
-        # context['username2'] = self.request.user.is_anonymous
+        mode = "production"
+        if mode == "dev":
+            SERVER = "UCSD"
+            param = {
+                'username': os.environ["DASHBOARD_USERNAME_" + SERVER],
+                'password': os.environ["DASHBOARD_PASSWORD_" + SERVER],
+            }
+            url = os.environ["DASHBOARD_AUTHURL_" + SERVER]
+            res = requests.post(url, data=param)
+            res.raise_for_status()
+            token = res.json()['token']
+        elif mode == "production": # if running on your own computer, has empty data
+            token, created = Token.objects.get_or_create(user=self.request.user)
+            context['token'] = token.key
 
         return context
